182cc92b4e25ffc0907675ec471b8cc2ad5e8d5d
wibosco <wboles1986@gmail.com>
2016-05-07 10:56:46 +0100

    Implemented a few summing tasks

---
+++ WhiteBoardCodingChallenges/Challenges/Summing/Summing.swift
@@ -0,0 +1,58 @@
+
+//
+//  Summing.swift
+//  WhiteBoardCodingChallenges
+//
+//  Created by Home on 07/05/2016.
+//  Copyright © 2016 Boles. All rights reserved.
+//
+
+import UIKit
+
+class Summing: NSObject {
+    
+    class func sum(array: [Int]) -> Int {
+        
+        var sum = 0
+        
+        for value in array {
+            
+            sum = sum + value
+        }
+        
+        return sum
+    }
+    
+    class func sumLargeValue(array: [Double]) -> Double {
+        
+        var sum: Double = 0
+        
+        for value in array {
+            
+            sum = sum + value
+        }
+        
+        return sum
+    }
+    
+    class func sumMatrixDiagonals(matrix: [[Int]]) -> Int {
+        
+        var sumOfRightDiagonal = 0
+        var sumOfLeftDiagonal = 0
+        
+        let finalElementIndex = (matrix.count - 1)
+        
+        for index in 0..<matrix.count {
+            
+            let row = matrix[index]
+            
+            let rightDiagonalElement = row[index]
+            sumOfRightDiagonal = sumOfRightDiagonal + rightDiagonalElement
+            
+            let leftDiagonalelement = row[(finalElementIndex - index)]
+            sumOfLeftDiagonal = sumOfLeftDiagonal + leftDiagonalelement
+        }
+        
+        return abs(sumOfRightDiagonal - sumOfLeftDiagonal)
+    }
+}
---
+++ WhiteBoardCodingChallengesTests/SummingTests.swift
@@ -0,0 +1,54 @@
+//
+//  SummingTests.swift
+//  WhiteBoardCodingChallenges
+//
+//  Created by Home on 07/05/2016.
+//  Copyright © 2016 Boles. All rights reserved.
+//
+
+import XCTest
+
+class SummingTests: XCTestCase {
+    
+    // MARK: TestLifecycle
+    
+    override func setUp() {
+        super.setUp()
+        
+    }
+    
+    override func tearDown() {
+        
+        super.tearDown()
+    }
+    
+    // MARK: Tests
+    
+    func test_sum() {
+        
+        let sum = Summing.sum([7, 8, 67, 5, 23, 9])
+        
+        XCTAssertEqual(119, sum, "Summing up has failed to produce the expected answer")
+    }
+    
+    func test_sumLargeValues() {
+        
+        let sum = Summing.sumLargeValue([1000000000000001, 1000000000000002, 1000000000000003, 1000000000000004, 1000000000000005])
+        
+        XCTAssertEqual(5000000000000015, sum, "Summing up has failed to produce the expected answer")
+    }
+    
+    func test_sumMatrixDiagonals() {
+        
+        let row1 = [11, 2, 4]
+        let row2 = [4, 5, 6]
+        let row3 = [10, 8, -12]
+        
+        let matrix = [row1, row2, row3]
+        
+        let sum = Summing.sumMatrixDiagonals(matrix)
+        
+        XCTAssertEqual(15, sum, "Summing up has failed to produce the expected answer")
+        
+    }
+}
