fa86d5749d93bcdca34691677b533965b11e464c
William Boles <william.boles@unii.com>
2016-05-10 19:45:35 +0100

    Implemented Manasa and Stones

---
+++ WhiteBoardCodingChallenges/Challenges/ManasaAndStones/ManasaAndStones.swift
@@ -0,0 +1,32 @@
+//
+//  ManasaAndStones.swift
+//  WhiteBoardCodingChallenges
+//
+//  Created by William Boles on 10/05/2016.
+//  Copyright © 2016 Boles. All rights reserved.
+//
+
+import UIKit
+
+class ManasaAndStones: NSObject {
+
+    class func possibleValuesForFinalStone(numberOfStones: Int, gapValueA: Int, gapValueB: Int) -> Set<Int> {
+
+        var multiplesOfA = (numberOfStones-1)
+        var multiplesOfB = 0
+        
+        var possibleValues = Set<Int>()
+        
+        while multiplesOfA >= 0 {
+            
+            let total = (multiplesOfA * gapValueA) + (multiplesOfB * gapValueB)
+            
+            possibleValues.insert(total)
+            
+            multiplesOfA -= 1
+            multiplesOfB += 1
+        }
+    
+        return possibleValues
+    }
+}
