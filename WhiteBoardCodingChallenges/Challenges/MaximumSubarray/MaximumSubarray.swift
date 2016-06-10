a0ed3d26c08c06629dac840041d5a202c0f8fd02
William Boles <wboles1986@gmail.com>
2016-06-10 17:12:57 +0100

    Implemented maximum subarray

---
+++ WhiteBoardCodingChallenges/Challenges/MaximumSubarray/MaximumSubarray.swift
@@ -0,0 +1,61 @@
+//
+//  MaximumSubarray.swift
+//  WhiteBoardCodingChallenges
+//
+//  Created by William Boles on 10/06/2016.
+//  Copyright © 2016 Boles. All rights reserved.
+//
+
+import Foundation
+
+class MaximumSubarray: NSObject {
+
+    class func maximumTotals(array: [Int]) -> (contiguousTotal: Int, nonContiguousTotal: Int) {
+    
+        let contiguousTotal = MaximumSubarray.maximumContiguous(array)
+        let nonContiguousTotal = MaximumSubarray.maximumNonContiguousTotal(array)
+        
+        return (contiguousTotal, nonContiguousTotal)
+    }
+    
+    class func maximumNonContiguousTotal(array: [Int]) -> Int {
+        
+        var nonContiguousTotal = array[0]
+        
+        if array.count > 1 {
+         
+            for value in array[1..<array.count] {
+                
+                if nonContiguousTotal > 0 {
+                    
+                    nonContiguousTotal = max(nonContiguousTotal, (nonContiguousTotal + value))
+                }
+                else {
+                    
+                    nonContiguousTotal = max(nonContiguousTotal, value)
+                }
+            }
+        }
+        
+        return nonContiguousTotal
+    }
+    
+    class func maximumContiguous(array: [Int]) -> Int {
+        
+        var contiguousTotal = array[0]
+        
+        if array.count > 1 {
+            
+            var currentTotal = array[0]
+            
+            for value in array[1..<array.count] {
+                
+                currentTotal = max(value, (currentTotal + value))
+                contiguousTotal = max(contiguousTotal, currentTotal)
+            }
+        }
+
+        return contiguousTotal
+    }
+    
+}
---
+++ WhiteBoardCodingChallengesTests/MaximumSubarrayTests.swift
@@ -0,0 +1,105 @@
+//
+//  MaximumSubarrayTests.swift
+//  WhiteBoardCodingChallenges
+//
+//  Created by William Boles on 10/06/2016.
+//  Copyright © 2016 Boles. All rights reserved.
+//
+
+import XCTest
+
+class MaximumSubarrayTests: XCTestCase {
+    
+    // MARK: Tests
+    
+    func test_maximumSubarrayA() {
+        
+        let array = [1,2,3,4]
+        
+        let maximums = MaximumSubarray.maximumTotals(array)
+        
+        XCTAssertEqual(maximums.contiguousTotal, 10)
+        XCTAssertEqual(maximums.nonContiguousTotal, 10)
+    }
+    
+    func test_maximumSubarrayB() {
+        
+        let array = [2,-1,2,3,4,-5]
+        
+        let maximums = MaximumSubarray.maximumTotals(array)
+        
+        XCTAssertEqual(maximums.contiguousTotal, 10)
+        XCTAssertEqual(maximums.nonContiguousTotal, 11)
+    }
+    
+    func test_maximumSubarrayC() {
+        
+        let array = [1]
+        
+        let maximums = MaximumSubarray.maximumTotals(array)
+        
+        XCTAssertEqual(maximums.contiguousTotal, 1)
+        XCTAssertEqual(maximums.nonContiguousTotal, 1)
+    }
+    
+    func test_maximumSubarrayD() {
+        
+        let array = [-1,-2,-3,-4,-5,-6]
+        
+        let maximums = MaximumSubarray.maximumTotals(array)
+        
+        XCTAssertEqual(maximums.contiguousTotal, -1)
+        XCTAssertEqual(maximums.nonContiguousTotal, -1)
+    }
+    
+    func test_maximumSubarrayE() {
+        
+        let array = [1, -2]
+        
+        let maximums = MaximumSubarray.maximumTotals(array)
+        
+        XCTAssertEqual(maximums.contiguousTotal, 1)
+        XCTAssertEqual(maximums.nonContiguousTotal, 1)
+    }
+    
+    func test_maximumSubarrayF() {
+        
+        let array = [1, 2, 3]
+        
+        let maximums = MaximumSubarray.maximumTotals(array)
+        
+        XCTAssertEqual(maximums.contiguousTotal, 6)
+        XCTAssertEqual(maximums.nonContiguousTotal, 6)
+    }
+    
+    func test_maximumSubarrayG() {
+        
+        let array = [-10]
+        
+        let maximums = MaximumSubarray.maximumTotals(array)
+        
+        XCTAssertEqual(maximums.contiguousTotal, -10)
+        XCTAssertEqual(maximums.nonContiguousTotal, -10)
+    }
+    
+    func test_maximumSubarrayH() {
+        
+        let array = [1, -1, -1, -1, -1, 5]
+        
+        let maximums = MaximumSubarray.maximumTotals(array)
+        
+        XCTAssertEqual(maximums.contiguousTotal, 5)
+        XCTAssertEqual(maximums.nonContiguousTotal, 6)
+    }
+    
+    func test_maximumSubarrayI() {
+        
+        let array = [-2, -3, 4, -1, -2, 1, 5, -3]
+        
+        let maximums = MaximumSubarray.maximumTotals(array)
+        
+        XCTAssertEqual(maximums.contiguousTotal, 7)
+        XCTAssertEqual(maximums.nonContiguousTotal, 10)
+    }
+    
+}
