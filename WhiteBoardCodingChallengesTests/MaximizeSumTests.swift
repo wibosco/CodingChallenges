f93b01658b620a35e8de7d4a389a504da856e0d8
William Boles <wboles1986@gmail.com>
2016-07-12 17:06:43 +0100

    Implemented maximise sum

---
+++ WhiteBoardCodingChallengesTests/MaximizeSumTests.swift
@@ -0,0 +1,24 @@
+//
+//  MaximizeSumTests.swift
+//  WhiteBoardCodingChallenges
+//
+//  Created by William Boles on 11/07/2016.
+//  Copyright © 2016 Boles. All rights reserved.
+//
+
+import XCTest
+
+class MaximizeSumTests: XCTestCase {
+    
+    // MARK: Tests
+    
+    func test_maximizeSumA() {
+        let values = [3,3,9,9,5]
+        let modulo = 7
+        
+        let maximum = MaximizeSum.maximizeSum(values, modulo: modulo)
+        
+        XCTAssertEqual(maximum, 6)
+    }
+    
+}
