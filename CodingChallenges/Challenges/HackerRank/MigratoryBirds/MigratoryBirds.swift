97e1bbac54ed98e3887671dd49d23e563416b00d
William Boles <wibosco@users.noreply.github.com>
2021-10-29 15:14:33 +0100

    Implemented migratory birds challenge

---
+++ CodingChallenges/Challenges/HackerRank/MigratoryBirds/MigratoryBirds.swift
@@ -0,0 +1,28 @@
+//
+//  MigratoryBirds.swift
+//  CodingChallenges
+//
+//  Created by William Boles on 29/10/2021.
+//  Copyright © 2021 Boles. All rights reserved.
+//
+
+import Foundation
+
+struct MigratoryBirds {
+    static func mostFrequentBird(from arr: [Int]) -> Int {
+        var countedBirds = [arr[0]: 1]
+        var mostFrequentBird = arr[0]
+        
+        for bird in arr.suffix(from: 1) {
+            countedBirds[bird, default: 0] += 1
+            
+            if (countedBirds[bird]! > countedBirds[mostFrequentBird]!) {
+                mostFrequentBird = bird
+            } else if (countedBirds[bird]! == countedBirds[mostFrequentBird]!) && (bird < mostFrequentBird) {
+                mostFrequentBird = bird
+            }
+        }
+    
+        return mostFrequentBird
+    }
+}
