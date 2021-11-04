4174c3fefd887a35ec729baece75623f733c19a4
William Boles <wibosco@users.noreply.github.com>
2021-11-04 13:04:36 +0000

    Implemented Longest Substring Without Repeating Characters challenge

---
+++ CodingChallenges/Challenges/LeetCode/LongestSubstring/LongestSubstring.swift
@@ -0,0 +1,51 @@
+//
+//  LongestSubstring.swift
+//  CodingChallenges
+//
+//  Created by William Boles on 04/11/2021.
+//  Copyright © 2021 Boles. All rights reserved.
+//
+
+import Foundation
+
+//https://leetcode.com/problems/longest-substring-without-repeating-characters/
+struct LongestSubstring {
+    static func lengthOfLongestSubstring(_ s: String) -> Int {
+        var visited = [Character: Int]()
+        var longestNonRepeatingWindow = 0
+        var currentWindowStartIndex = 0
+        
+        for (currentWindowEndIndex, char) in s.enumerated() {
+            if let vistedIndex = visited[char], vistedIndex >= currentWindowStartIndex {
+                currentWindowStartIndex = (vistedIndex + 1)
+            }
+            
+            let currentWindowSize = (currentWindowEndIndex + 1) - currentWindowStartIndex
+            longestNonRepeatingWindow = max(longestNonRepeatingWindow, currentWindowSize)
+            
+            visited[char] = currentWindowEndIndex
+        }
+    
+        return longestNonRepeatingWindow
+    }
+    
+    static func lengthOfLongestSubstringSlow(_ s: String) -> Int {
+        var visited = [Character: Int]()
+        var longestNonRepeatingWindow = 0
+        var currentWindowStartIndex = 0
+        
+        for index in 0..<s.count {
+            let char = s[s.index(s.startIndex, offsetBy: index)]
+            if let vistedIndex = visited[char], vistedIndex >= currentWindowStartIndex {
+                currentWindowStartIndex = (vistedIndex + 1)
+            }
+            
+            let currentWindowSize = (index + 1) - currentWindowStartIndex
+            longestNonRepeatingWindow = max(longestNonRepeatingWindow, currentWindowSize)
+            
+            visited[char] = index
+        }
+    
+        return longestNonRepeatingWindow
+    }
+}
