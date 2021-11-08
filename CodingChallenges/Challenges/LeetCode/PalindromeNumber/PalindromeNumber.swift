3bc09ca1702df65365de28a27a25f96c3237515b
William Boles <wibosco@users.noreply.github.com>
2021-11-08 12:16:54 +0000

    Implemented Palindrome Number challenge

---
+++ CodingChallenges/Challenges/LeetCode/PalindromeNumber/PalindromeNumber.swift
@@ -0,0 +1,33 @@
+//
+//  PalindromeNumber.swift
+//  CodingChallenges
+//
+//  Created by William Boles on 08/11/2021.
+//  Copyright © 2021 Boles. All rights reserved.
+//
+
+import Foundation
+
+//https://leetcode.com/problems/palindrome-number/
+struct PalindromeNumber {
+    static func isPalindrome(_ x: Int) -> Bool {
+        guard x >= 0 else {
+            return false
+        }
+
+        guard x > 9 else {
+            return true
+        }
+
+        var reversedX = 0
+        var tmp = x
+        while tmp > 0 {
+            let pop = tmp % 10
+            reversedX *= 10 // make room for the the pop to be added e.g. 123 becomes 1230
+            reversedX += pop
+            tmp /= 10
+        }
+
+        return reversedX == x
+    }
+}
