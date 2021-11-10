a1465e150c039ca32b768f2cd875c59a89e7be29
William Boles <wibosco@users.noreply.github.com>
2021-11-10 08:56:17 +0000

    Implemented Merge Two Sorted Lists challenge

---
+++ CodingChallenges/Challenges/LeetCode/MergeTwoSortedLists.swift
@@ -0,0 +1,48 @@
+//
+//  MergeTwoSortedLists.swift
+//  CodingChallenges
+//
+//  Created by William Boles on 10/11/2021.
+//  Copyright © 2021 Boles. All rights reserved.
+//
+
+import Foundation
+
+//https://leetcode.com/problems/merge-two-sorted-lists/
+struct MergeTwoSortedLists {
+    
+    //O(n)
+    static func mergeTwoLists(_ l1: ListNode?, _ l2: ListNode?) -> ListNode? {
+        guard l1 != nil else {
+            return l2
+        }
+        
+        guard l2 != nil else {
+            return l1
+        }
+        
+        var l1 = l1
+        var l2 = l2
+        
+        let dummy = ListNode(0)
+        var tail = dummy
+
+        while let l1N = l1, let l2N = l2 { // only loop until you exhaust one of the lists
+            if l1N.val < l2N.val {
+                tail.next = l1N
+                tail = l1N
+                
+                l1 = l1N.next
+            } else {
+                tail.next = l2N
+                tail = l2N
+                
+                l2 = l2N.next
+            }
+        }
+        
+        tail.next = (l1 == nil) ? l2 : l1
+        
+        return dummy.next
+    }
+}
---
+++ CodingChallengesTests/Tests/MergeTwoSortedListsTests.swift
@@ -0,0 +1,46 @@
+//
+//  MergeTwoSortedListsTests.swift
+//  CodingChallengesTests
+//
+//  Created by William Boles on 10/11/2021.
+//  Copyright © 2021 Boles. All rights reserved.
+//
+
+import XCTest
+
+@testable import CodingChallenges
+
+class MergeTwoSortedListsTests: XCTestCase {
+
+    // MARK: - Tests
+    
+    func test_A() {
+        let l1 = ListNode.createList(fromArray: [1, 2, 4])
+        let l2 = ListNode.createList(fromArray: [1, 3, 4])
+        
+        let mergedList = MergeTwoSortedLists.mergeTwoLists(l1, l2)
+        let values = ListNode.extractValues(fromList: mergedList)
+        
+        XCTAssertEqual(values, [1, 1, 2, 3, 4, 4])
+    }
+    
+    func test_B() {
+        let l1 = ListNode.createList(fromArray: [])
+        let l2 = ListNode.createList(fromArray: [])
+        
+        let mergedList = MergeTwoSortedLists.mergeTwoLists(l1, l2)
+        let values = ListNode.extractValues(fromList: mergedList)
+        
+        XCTAssertEqual(values, [])
+    }
+    
+    func test_C() {
+        let l1 = ListNode.createList(fromArray: [])
+        let l2 = ListNode.createList(fromArray: [0])
+        
+        let mergedList = MergeTwoSortedLists.mergeTwoLists(l1, l2)
+        let values = ListNode.extractValues(fromList: mergedList)
+        
+        XCTAssertEqual(values, [0])
+    }
+}
