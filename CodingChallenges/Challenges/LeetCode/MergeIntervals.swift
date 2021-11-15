4c700ba070e722184c4722bf1ed016b47ec4244a
William Boles <wibosco@users.noreply.github.com>
2021-11-15 16:54:39 +0000

    Implemented 'Merge Intervals' challenge

---
+++ CodingChallenges/Challenges/LeetCode/MergeIntervals.swift
@@ -0,0 +1,39 @@
+//
+//  MergeIntervals.swift
+//  CodingChallenges
+//
+//  Created by William Boles on 15/11/2021.
+//  Copyright © 2021 Boles. All rights reserved.
+//
+
+import Foundation
+
+//https://leetcode.com/problems/merge-intervals/
+//sorting
+struct MergeIntervals {
+    
+    //0(nlogn)
+    static func merge(_ intervals: [[Int]]) -> [[Int]] {
+        guard intervals.count > 1 else {
+            return intervals
+        }
+        
+        var intervals = intervals
+        intervals.sort { $0[0] < $1[0] }
+        
+        var mergedIntervals = [intervals[0]]
+        
+        for i in 1..<intervals.count {
+            let comparisonInterval = intervals[i]
+            let mergedInterval = mergedIntervals.last!
+            if mergedInterval[1] >= comparisonInterval[0] {
+                //Only the max can change as we already sorted on the on the min value
+                mergedIntervals[(mergedIntervals.count - 1)][1] = max(comparisonInterval[1], mergedInterval[1])
+            } else {
+                mergedIntervals.append(comparisonInterval)
+            }
+        }
+        
+       return mergedIntervals
+    }
+}
