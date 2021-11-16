c24f683aed1c8955df3ccf70595f88568028c31a
William Boles <wibosco@users.noreply.github.com>
2021-11-16 21:39:17 +0000

    Implemented binary search

---
+++ CodingChallenges/Challenges/Other/BinarySearch.swift
@@ -0,0 +1,40 @@
+//
+//  BinarySearch.swift
+//  CodingChallenges
+//
+//  Created by William Boles on 16/11/2021.
+//  Copyright © 2021 Boles. All rights reserved.
+//
+
+import Foundation
+
+//binary search
+//See: https://medium.com/swlh/binary-search-find-upper-and-lower-bound-3f07867d81fb
+struct BinarySearch {
+    //O(log n)
+    static func find(_ target: Int, in nums: [Int]) -> Int {
+        guard !nums.isEmpty else {
+            return -1
+        }
+        
+        guard nums.count > 1 else {
+            return (nums[0] == target) ? 0 : -1
+        }
+        
+        var left = 0
+        var right = nums.count - 1
+        
+        while right > left {
+            let mid = left + (right - left) / 2
+            if nums[mid] == target {
+                return mid
+            } else if nums[mid] > target {
+                right = mid
+            } else {
+                left = mid + 1
+            }
+        }
+        
+        return -1
+    }
+}
