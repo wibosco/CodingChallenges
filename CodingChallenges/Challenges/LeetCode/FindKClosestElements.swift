f767d992ee149fe1c97f0c68da38a5507e636309
William Boles <wibosco@users.noreply.github.com>
2021-11-19 13:46:17 +0000

    Implemented 'Find K Closest Elements' challenge

---
+++ CodingChallenges/Challenges/LeetCode/FindKClosestElements.swift
@@ -0,0 +1,83 @@
+//
+//  FindKClosestElements.swift
+//  CodingChallenges
+//
+//  Created by William Boles on 18/11/2021.
+//  Copyright © 2021 Boles. All rights reserved.
+//
+
+import Foundation
+
+//https://leetcode.com/problems/find-k-closest-elements/
+//binary search
+//two pointers
+//sliding window
+struct FindKClosestElements {
+    //O((log n) + k)
+    static func findClosestElements(_ arr: [Int], _ k: Int, _ x: Int) -> [Int] {
+        guard arr.count > k else {
+            return arr
+        }
+        
+        var left = 0
+        var right = arr.count - 1
+        
+        while (left + 1) < right {
+            let mid = left + (right - left) / 2
+            
+            if arr[mid] == x {
+                left = mid
+                break
+            } else if arr[mid] > x {
+                right = mid
+            } else {
+                left = mid
+            }
+        }
+        
+        let startingIndex = nearestIndex(to: x, in: arr, from: left, to: right)
+        
+        left = startingIndex
+        right = startingIndex
+        
+        for i in 0..<(k - 1) {
+            let previous = left - 1
+            let next = right + 1
+            
+            if previous < 0 {
+                right += ((k - 1) - i)
+                break
+            } else if next >= arr.count {
+                left -= ((k - 1) - i)
+                break
+            }
+            
+            let nearestIndex = nearestIndex(to: x, in: arr, from: previous, to: next)
+            
+            if nearestIndex < left {
+                left -= 1
+            } else {
+                right += 1
+            }
+        }
+        
+        return Array(arr[left...right])
+    }
+    
+    private static func nearestIndex(to x: Int, in arr: [Int], from left: Int, to right: Int) -> Int {
+        let leftDelta = abs(arr[left] - x)
+        let rightDelta = abs(arr[right] - x)
+        
+        if rightDelta > leftDelta {
+            return left
+        } else if leftDelta > rightDelta {
+            return right
+        } else { //same difference
+            if arr[right] > arr[left] {
+                return left
+            } else {
+                return right
+            }
+        }
+    }
+}
---
+++ CodingChallengesTests/Tests/FindKClosestElementsTests.swift
@@ -0,0 +1,66 @@
+//
+//  FindKClosestElementsTests.swift
+//  CodingChallengesTests
+//
+//  Created by William Boles on 18/11/2021.
+//  Copyright © 2021 Boles. All rights reserved.
+//
+
+import XCTest
+
+@testable import CodingChallenges
+
+class FindKClosestElementsTests: XCTestCase {
+
+    // MARK: - Tests
+    
+    func test_A() {
+        let arr = [1, 2, 3, 4, 5]
+        let k = 4
+        let x = 3
+        
+        let closet = FindKClosestElements.findClosestElements(arr, k, x)
+        
+        XCTAssertEqual(closet, [1, 2, 3, 4])
+    }
+    
+    func test_B() {
+        let arr = [1, 2, 3, 4, 5]
+        let k = 4
+        let x = -1
+        
+        let closet = FindKClosestElements.findClosestElements(arr, k, x)
+        
+        XCTAssertEqual(closet, [1, 2, 3, 4])
+    }
+    
+    func test_C() {
+        let arr = [1, 1, 1, 10, 10, 10]
+        let k = 1
+        let x = 9
+        
+        let closet = FindKClosestElements.findClosestElements(arr, k, x)
+        
+        XCTAssertEqual(closet, [10])
+    }
+    
+    func test_D() {
+        let arr = [0, 0, 0, 1, 3, 5, 6, 7, 8, 8]
+        let k = 2
+        let x = 2
+        
+        let closet = FindKClosestElements.findClosestElements(arr, k, x)
+        
+        XCTAssertEqual(closet, [1, 3])
+    }
+    
+    func test_E() {
+        let arr = [0, 0, 1, 2, 3, 3, 4, 7, 7, 8]
+        let k = 3
+        let x = 5
+        
+        let closet = FindKClosestElements.findClosestElements(arr, k, x)
+        
+        XCTAssertEqual(closet, [3, 3, 4])
+    }
+}
