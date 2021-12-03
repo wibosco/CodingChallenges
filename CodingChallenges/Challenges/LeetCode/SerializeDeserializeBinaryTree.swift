0b414d17f6b2b3cb17bc463b9fcaf1d4a30a5f03
William Boles <wibosco@users.noreply.github.com>
2021-12-03 14:54:16 +0000

    Implemented 'Serialize and Deserialize Binary Tree' challenge

---
+++ CodingChallenges/Challenges/LeetCode/SerializeDeserializeBinaryTree.swift
@@ -0,0 +1,106 @@
+//
+//  SerializeDeserializeBinaryTree.swift
+//  CodingChallenges
+//
+//  Created by William Boles on 03/12/2021.
+//  Copyright © 2021 Boles. All rights reserved.
+//
+
+import Foundation
+
+//https://leetcode.com/problems/serialize-and-deserialize-binary-tree/
+//binary tree
+struct SerializeDeserializeBinaryTree {
+    
+    //Time: O(n) where n is the number of elements in data
+    //Space: O(n) where n is the nodes in the tree
+    //BFS
+    //level traversal
+    //
+    //Solution Description:
+    //Traverse the tree level-by-level (BFS) adding the value (or lack of value)
+    //of each node encountered to an array. At the end of the traveral remove
+    //any trailing `nil` values.
+    static func serialize(_ root: TreeNode?) -> [Int?] {
+        guard let root = root else {
+            return [Int?]()
+        }
+        
+        var queue = [TreeNode?]()
+        queue.append(root)
+        var values = [Int?]()
+        
+        while !queue.isEmpty {
+            let levelCount = queue.count
+            
+            for _ in 0..<levelCount {
+                guard let node = queue.removeFirst() else {
+                    values.append(nil)
+                    continue
+                }
+                
+                values.append(node.val)
+                
+                queue.append(node.left)
+                queue.append(node.right)
+            }
+        }
+        
+        //trim `nil` from end
+        var i = (values.count - 1)
+        while i > 0 {
+            if values[i] != nil {
+                break
+            }
+            i -= 1
+        }
+        
+        return Array(values[0...i])
+    }
+    
+    //Time: O(n) where n is the number of elements in data
+    //Space: O(n) where n is the nodes in the tree
+    //BFS
+    //level traversal
+    //
+    //Solution Description:
+    //Iterate through the array using BFS, taking the elements in pairs as
+    //the left and right of the first tree node of the queue
+    static func deserialize(_ data: [Int?]) -> TreeNode? {
+        guard !data.isEmpty else {
+            return nil
+        }
+        
+        let root = TreeNode(data[0]!) //safe to do this?
+        var queue = [root]
+        var i = 1
+        
+        while i < data.count {
+            let node = queue.removeFirst()
+            
+            if let leftValue = data[i] {
+                let left = TreeNode(leftValue)
+                node.left = left
+                
+                queue.append(left)
+            }
+            
+            i += 1
+            
+            guard i < data.count else {
+                continue
+            }
+            
+            if let rightValue = data[i] {
+                let right = TreeNode(rightValue)
+                node.right = right
+                
+                queue.append(right)
+            }
+            
+            i += 1
+        }
+        
+        return root
+    }
+}
