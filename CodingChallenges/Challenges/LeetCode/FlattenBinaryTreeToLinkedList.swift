ef1cb5792d97bb052fde6cb4ce9ea6def1ad696a
William Boles <wibosco@users.noreply.github.com>
2021-12-08 23:29:15 +0000

    Implemented 'Flatten Binary Tree to Linked List' challenge

---
+++ CodingChallenges/Challenges/LeetCode/FlattenBinaryTreeToLinkedList.swift
@@ -0,0 +1,55 @@
+//
+//  FlattenBinaryTreeToLinkedList.swift
+//  CodingChallenges
+//
+//  Created by William Boles on 08/12/2021.
+//  Copyright © 2021 Boles. All rights reserved.
+//
+
+import Foundation
+
+//
+//binary tree
+//linked list
+struct FlattenBinaryTreeToLinkedList {
+
+    //Time: O(n) where n is the number of nodes
+    //Space: O(n) where n is the number of nodes
+    //array
+    //dfs
+    //
+    //Solution Description:
+    //Using dfs perform a preorder traversal of the tree adding the nodes that
+    //we come across to an array. After the array is populated we iterate
+    //through, setting the current nodes right node to the next node in the
+    //array. We also remove any existing left and right associations
+    static func flatten(_ root: BinaryTreeNode?) {
+        guard let root = root else {
+            return
+        }
+        
+        var ordering = [BinaryTreeNode]()
+        dfs(root, &ordering)
+        
+        for i in 0..<(ordering.count - 1) {
+            let curr = ordering[i]
+            let next = ordering[(i + 1)]
+            
+            curr.left = nil
+            curr.right = next
+            
+            next.right = nil
+            next.left = nil
+        }
+    }
+    
+    private static func dfs(_ root: BinaryTreeNode?, _ ordering: inout [BinaryTreeNode]) {
+        guard let root = root else {
+            return
+        }
+        
+        ordering.append(root)
+        dfs(root.left, &ordering)
+        dfs(root.right, &ordering)
+    }
+}
---
+++ CodingChallengesTests/Tests/LeetCode/FlattenBinaryTreeToLinkedListTests.swift
@@ -0,0 +1,52 @@
+//
+//  FlattenBinaryTreeToLinkedListTests.swift
+//  CodingChallengesTests
+//
+//  Created by William Boles on 08/12/2021.
+//  Copyright © 2021 Boles. All rights reserved.
+//
+
+import XCTest
+
+@testable import CodingChallenges
+
+class FlattenBinaryTreeToLinkedListTests: XCTestCase {
+
+    // MARK: - Tests
+    
+    func test_A() {
+        let data = [1, 2, 5, 3, 4, nil, 6]
+        
+        let root = BinaryTreeNode.createBinaryTree(fromLevelOrderArray: data)
+        
+        FlattenBinaryTreeToLinkedList.flatten(root)
+        
+        let values =  BinaryTreeNode.extractValuesInLevelOrder(fromBinaryTree: root)
+        
+        XCTAssertEqual(values, [1, nil, 2, nil, 3, nil, 4, nil, 5, nil, 6])
+    }
+    
+    func test_B() {
+        let data = [Int]()
+        
+        let root = BinaryTreeNode.createBinaryTree(fromLevelOrderArray: data)
+        
+        FlattenBinaryTreeToLinkedList.flatten(root)
+        
+        let values =  BinaryTreeNode.extractValuesInLevelOrder(fromBinaryTree: root)
+        
+        XCTAssertEqual(values, [])
+    }
+    
+    func test_C() {
+        let data = [0]
+        
+        let root = BinaryTreeNode.createBinaryTree(fromLevelOrderArray: data)
+        
+        FlattenBinaryTreeToLinkedList.flatten(root)
+        
+        let values =  BinaryTreeNode.extractValuesInLevelOrder(fromBinaryTree: root)
+        
+        XCTAssertEqual(values, [0])
+    }
+}
