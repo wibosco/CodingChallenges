2344cc05d413b7781a4ad5c4e5a6c3a75cd03e82
William Boles <wibosco@users.noreply.github.com>
2022-01-22 10:49:14 +0000

    Implemented 'N-Repeated Element in Size 2N Array' challenge

---
+++ CodingChallenges/Challenges/LeetCode/NRepeatedElementInSize2NArray.swift
@@ -0,0 +1,40 @@
+//
+//  N-RepeatedElementInSize2NArray.swift
+//  CodingChallenges
+//
+//  Created by William Boles on 22/01/2022.
+//  Copyright © 2022 Boles. All rights reserved.
+//
+
+import Foundation
+
+//https://leetcode.com/problems/n-repeated-element-in-size-2n-array/
+//array
+struct NRepeatedElementInSize2NArray {
+    
+    //Time: O(n) where `n` is the number of elements in `nums`
+    //Space: O(1)
+    //dictionary
+    //
+    //Solution Description:
+    //
+    static func repeatedNTimes(_ nums: [Int]) -> Int {
+        var count = [Int: Int]()
+        
+        for num in nums {
+            count[num, default: 0] += 1
+        }
+        
+        let target = nums.count / 2
+        
+        for key in count.keys {
+            guard count[key] == target else {
+                continue
+            }
+            
+            return key
+        }
+        
+        return -1
+    }
+}
