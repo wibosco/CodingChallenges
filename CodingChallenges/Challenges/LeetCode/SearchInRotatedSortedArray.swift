a8cc9c9db234f8bd9bf93a1c0b1e3c80afb1d542
William Boles <wibosco@users.noreply.github.com>
2022-01-22 10:36:01 +0000

    Implemented 'Search in Rotated Sorted Array II' challenge

--- CodingChallenges/Challenges/LeetCode/SearchInRotatedSortedArray.swift
+++ CodingChallenges/Challenges/LeetCode/SearchInRotatedSortedArray.swift
@@ -10,11 +10,67 @@ import Foundation
 
 //https://leetcode.com/problems/search-in-rotated-sorted-array/
 //binary search
-//array
 struct SearchInRotatedSortedArray {
     
-    //Time: O(log n)
-    static func searchAlt(_ nums: [Int], _ target: Int) -> Int {
+    //Time: O(log n) where `n` is the number of elements in `nums`
+    //Space: O(1)
+    //array
+    //
+    //Solution Description:
+    //In a sorted array, index 0 contains a smaller value than index n. We can use this property to know where to search in this
+    //array using binary search by treating it as effectively two sorted arrays - 0..<pivot and pivot..n. First we determine our
+    //mid point and compare it against the target - if mid is the target then we return that index else we compare mid with our
+    //right most index (remember if this is the second or later iterations right wouldn't be nums.count - 1). If mid is greater
+    //than right then we know that pivot is in right part of the subarray i.e. right part isn't sorted, so we compare the target
+    //against the sorted left array and adjust boundaries appropriately - reducing the search space by half; if mid is less then
+    //we do the opposite. Eventually we either find the target in the array or after we have exhausted the search space we return
+    //-1
+    static func search1(_ nums: [Int], _ target: Int) -> Int {
+        var left = 0
+        var right = nums.count - 1
+        
+        //find pivot and perform search
+        while left <= right {
+            let mid = left + (right - left) / 2
+            
+            if target == nums[mid] {
+                return mid
+            } else if nums[mid] > nums[right] {
+                //pivot is somewhere to the right of mid
+                if target >= nums[left] && target <= nums[mid] {
+                    //search left
+                    right = mid - 1
+                } else {
+                    //search right
+                    left = mid + 1
+                }
+            } else {
+                //pivot is somewhere to the left of mid
+                if target >= nums[mid] && target <= nums[right] {
+                    //search right
+                    left = mid + 1
+                } else {
+                    //search left
+                    right = mid - 1
+                }
+            }
+        }
+        
+        return -1
+    }
+    
+    //Time: O(log n) where `n` is the number of elements in `nums`
+    //Space: O(1)
+    //array
+    //
+    //Solution Description:
+    //First we need to find the pivot index. To do this we can perform an altered binary search that rather than attempting to
+    //find a target is attempting to find where the current index is greater than the next index e.g. [4, 5, 6, 0, 1, 2 ,3] -
+    //here the pivot is index 3 as index 2 is greater. Once we find the pivot i.e. the original start of the sorted array, we
+    //need to determine if the target is before or after that pivot (provided the pivot isn't actually the target) e.g. in
+    //[4, 5, 6, 0, 1, 2 ,3] is the target in [4, 5, 6] or [1, 2, 3] - effectively performing a one step binary search. Once we
+    //have our sorted subarray we can then perform a standard binary search to find the target
+    static func search(_ nums: [Int], _ target: Int) -> Int {
         var left = 0
         var right = nums.count - 1
         
@@ -58,42 +114,6 @@ struct SearchInRotatedSortedArray {
                 left = mid + 1
             } else {
                 right = mid - 1
-            }
-        }
-        
-        return -1
-    }
-    
-    //More complex but only one pass
-    //Time: O(log n)
-    static func search(_ nums: [Int], _ target: Int) -> Int {
-        var left = 0
-        var right = nums.count - 1
-        
-        //find pivot and peform search
-        while left <= right {
-            let mid = left + (right - left) / 2
-            
-            if target == nums[mid] {
-                return mid
-            } else if nums[mid] > nums[right] {
-                //pivot is somewhere to the right of mid i.e right is unsorted
-                if target >= nums[left] && target <= nums[mid] {
-                    //search left
-                    right = mid - 1
-                } else {
-                    //search right
-                    left = mid + 1
-                }
-            } else {
-                //pivot is somewhere to the left of mid i.e left is unsorted
-                if target >= nums[mid] && target <= nums[right] {
-                    //search right
-                    left = mid + 1
-                } else {
-                    //search left
-                    right = mid - 1
-                }
             }
         }
         
---
+++ CodingChallenges/Challenges/LeetCode/SearchInRotatedSortedArrayII.swift
@@ -0,0 +1,65 @@
+//
+//  SearchInRotatedSortedArrayII.swift
+//  CodingChallenges
+//
+//  Created by William Boles on 21/01/2022.
+//  Copyright © 2022 Boles. All rights reserved.
+//
+
+import Foundation
+
+//https://leetcode.com/problems/search-in-rotated-sorted-array-ii/
+//binary search
+struct SearchInRotatedSortedArrayII {
+    
+    //Average Time: O(log n) where `n` is the number of elements in `nums`
+    //Worse Time: O(n) where `n` is the number of elements in `nums`
+    //Space: O(1)
+    //array
+    //
+    //Solution Description:
+    //In a sorted array, index 0 contains a smaller value than index n. We can use this property to know where to search in this
+    //array using binary search by treating it as effectively two sorted arrays - 0..<pivot and pivot..n. Like a standard
+    //binary search we take the mid point of our search space and use it to see if we have found the target. If so we can return
+    //true and exit; if not then we first compare if the subarray before `mid` is sorted in ascending order i.e. left is smaller
+    //than mid. If so we can safely check if `target` is within that range and move the left or right boundary appropriately. If
+    //left is not less than mid we check if it is greater than mid (remember this array can contain duplicates). If so then we
+    //know that the right subarray is sorted and we check if `target` would be in that subarray and move the left and right
+    //boundary appropriately. Finally as this array can contain duplicates if `left` and `mid` are equal we increment left to
+    //create a slightly smaller search space next iteration. 
+    static func search(_ nums: [Int], _ target: Int) -> Bool {
+        guard nums.count > 0 else {
+            return false
+        }
+        
+        var left = 0
+        var right = nums.count - 1
+        
+        while left < right {
+            let mid = left + (right - left) / 2
+            
+            if nums[mid] == target {
+                return true
+            } else if nums[left] < nums[mid] {
+                //left subarray is sorted
+                if target >= nums[left] && target <= nums[mid] {
+                    right = mid - 1
+                } else {
+                    left = mid + 1
+                }
+            } else if nums[left] > nums[mid] {
+                //right subarray is sorted
+                if target >= nums[mid] && target <= nums[right] {
+                    left = mid + 1
+                } else {
+                    right = mid - 1
+                }
+            } else {
+                //left and mid are equal
+                left += 1
+            }
+        }
+        
+        return nums[left] == target
+    }
+}
