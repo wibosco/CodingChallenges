bfd91351c7a0f6a69c37626e1b756010c492d370
William Boles <wibosco@users.noreply.github.com>
2022-01-28 16:38:40 +0000

    Implemented 'Second Minimum Node In a Binary Tree' challenge

---
+++ CodingChallenges/Challenges/LeetCode/SecondMinimumNodeInBinaryTree.swift
@@ -0,0 +1,47 @@
+//
+//  SecondMinimumNodeInBinaryTree.swift
+//  CodingChallenges
+//
+//  Created by William Boles on 28/01/2022.
+//  Copyright © 2022 Boles. All rights reserved.
+//
+
+import Foundation
+
+//https://leetcode.com/problems/second-minimum-node-in-a-binary-tree/
+//binary tree
+struct SecondMinimumNodeInBinaryTree {
+    
+    //Time: O(n log n) where `n` is the number of nodes in the tree
+    //Space: O(n) where `n` is the number of nodes in the tree
+    //DFS
+    //recursion
+    //sorted
+    //
+    //Solution Description:
+    //Traverse the tree using DFS and insert every node into the `values` set, then sort that set and return the
+    //second element (if it exists else return -1)
+    static func findSecondMinimumValue(_ root: BinaryTreeNode?) -> Int {
+        var values = Set<Int>()
+        
+        dfs(root, &values)
+        
+        guard values.count > 1 else {
+            return -1
+        }
+        
+        let sorted = Array(values).sorted { $0 < $1 } // O(n log n)
+        
+        return sorted[1]
+    }
+    
+    private static func dfs(_ root: BinaryTreeNode?, _ values: inout Set<Int>) {
+        guard let root = root else {
+            return
+        }
+        
+        dfs(root.left, &values)
+        values.insert(root.val)
+        dfs(root.right, &values)
+    }
+}
---
+++ CodingChallengesTests/Tests/LeetCode/SecondMinimumNodeInBinaryTreeTests.swift
@@ -0,0 +1,34 @@
+//
+//  SecondMinimumNodeInBinaryTreeTests.swift
+//  CodingChallengesTests
+//
+//  Created by William Boles on 28/01/2022.
+//  Copyright © 2022 Boles. All rights reserved.
+//
+
+import XCTest
+
+@testable import CodingChallenges
+
+class SecondMinimumNodeInBinaryTreeTests: XCTestCase {
+
+    // MARK: - Tests
+    
+    func test_A() {
+        let data = [2, 2, 5, nil, nil, 5, 7]
+        let root = BinaryTreeNode.deserialize(data)
+        
+        let secondMin = SecondMinimumNodeInBinaryTree.findSecondMinimumValue(root)
+        
+        XCTAssertEqual(secondMin, 5)
+    }
+    
+    func test_B() {
+        let data = [2, 2, 2]
+        let root = BinaryTreeNode.deserialize(data)
+        
+        let secondMin = SecondMinimumNodeInBinaryTree.findSecondMinimumValue(root)
+        
+        XCTAssertEqual(secondMin, -1)
+    }
+}
