c37826f6a5a2f140929ba524f61ed671443c1fc4
William Boles <wibosco@users.noreply.github.com>
2022-01-31 15:39:54 +0000

    Implemented 'Maximum Subarray' challenge

--- LeetCode/LeetCode/Challenges/MaximumSubarray.swift
+++ LeetCode/LeetCode/Challenges/MaximumSubarray.swift
@@ -9,15 +9,33 @@
 import Foundation
 
 //https://leetcode.com/problems/maximum-subarray/
-//
+//array
 struct MaximumSubarray {
     
-    //Time: O(
-    //Space: O(
+    //Time: O(n) where `n` is the number of elements in `nums`
+    //Space: O(1)
+    //counting
+    //greedy
     //
     //Solution Description:
-    //
+    //As this array contains both positive and negative values it be possible that as add values together we can end up
+    //with a lower total value that what we began with i.e. [4, -1] would give us 3 which is lower than just 4, however
+    //it is also possible that we have e.g. [4, -1, 2] - here by going through the negative we actually end up with a
+    //higher positive value, 5. So we need to know when to discard negative i.e start a new subarray sum and when to
+    //accept them into the current subarray sum. To do we need to be "greedy" - if adding a value to the current subarray
+    //sum takes us below the value that was added then that value was more valuable on its own and we need to start a
+    //new subbarray sum; if adding a value to the current subarray sum takes us above the value that was added then we
+    //continue with that subarray sum. We then compare that new subarray sum with the maximum/greatest subarray sum that
+    //we have seen so far and keep the larger of the two.
     static func maxSubArray(_ nums: [Int]) -> Int {
-        return 0
+        var currentSum = nums[0]
+        var maxSum = nums[0]
+        
+        for num in nums[1...] {
+            currentSum = max(num, (currentSum + num))
+            maxSum = max(currentSum, maxSum)
+        }
+        
+        return maxSum
     }
 }
--- LeetCode/LeetCodeTests/Tests/MaximumSubarrayTests.swift
+++ LeetCode/LeetCodeTests/Tests/MaximumSubarrayTests.swift
@@ -15,10 +15,26 @@ class MaximumSubarrayTests: XCTestCase {
     // MARK: - Tests
     
     func test_A() {
+        let nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
         
+        let sum = MaximumSubarray.maxSubArray(nums)
+        
+        XCTAssertEqual(sum, 6)
     }
     
     func test_B() {
+        let nums = [1]
         
+        let sum = MaximumSubarray.maxSubArray(nums)
+        
+        XCTAssertEqual(sum, 1)
+    }
+    
+    func test_C() {
+        let nums = [5, 4, -1, 7, 8]
+        
+        let sum = MaximumSubarray.maxSubArray(nums)
+        
+        XCTAssertEqual(sum, 23)
     }
 }
