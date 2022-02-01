33c6dd2cf5a6643dc9b3aac346ade39b545adfdf
William Boles <wibosco@users.noreply.github.com>
2022-02-01 20:41:56 +0000

    Implemented 'Best Time to Buy and Sell Stock II' challenge

---
+++ LeetCode/LeetCode/Challenges/BestTimeToBuyAndSellStockII.swift
@@ -0,0 +1,37 @@
+//
+//  BestTimeToBuyAndSellStockII.swift
+//  LeetCode
+//
+//  Created by William Boles on 01/02/2022.
+//
+
+import Foundation
+
+//https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/
+//array
+struct BestTimeToBuyAndSellStockII {
+    
+    //Time: O(n) where `n` is the number of elements in `prices`
+    //Space: O(1)
+    //counting
+    //greedy
+    //Kadanes algorithm
+    //
+    //Solution Description:
+    //To generate profit we need to buy low and sell high. We can do this by always comparing the current price with
+    //the next price. If the next price is higher we don't buy and if it lower we do buy and sell that next day. The
+    //profit is then banked and we continuing on looking for the next profitable buy/sell combination. We are not
+    //trying to find the greatest positive difference instead we are using a greedy approach and taking any positive
+    //difference
+    static func maxProfit(_ prices: [Int]) -> Int {
+        var profit = 0
+        
+        for i in 0..<(prices.count - 1) {
+            if prices[i] < prices[(i + 1)] {
+                profit += prices[(i + 1)] - prices[i]
+            }
+        }
+        
+        return max(profit, 0)
+    }
+}
---
+++ LeetCode/LeetCodeTests/Tests/BestTimeToBuyAndSellStockIITests.swift
@@ -0,0 +1,39 @@
+//
+//  BestTimeToBuyAndSellStockIITests.swift
+//  LeetCodeTests
+//
+//  Created by William Boles on 01/02/2022.
+//
+
+import XCTest
+
+@testable import LeetCode
+
+class BestTimeToBuyAndSellStockIITests: XCTestCase {
+    
+    // MARK: - Tests
+    
+    func test_A() {
+        let prices = [7, 1, 5, 3, 6, 4]
+        
+        let profit = BestTimeToBuyAndSellStockII.maxProfit(prices)
+        
+        XCTAssertEqual(profit, 7)
+    }
+    
+    func test_B() {
+        let prices = [1, 2, 3, 4, 5]
+        
+        let profit = BestTimeToBuyAndSellStockII.maxProfit(prices)
+        
+        XCTAssertEqual(profit, 4)
+    }
+    
+    func test_C() {
+        let prices = [7, 6, 4, 3, 1]
+        
+        let profit = BestTimeToBuyAndSellStockII.maxProfit(prices)
+        
+        XCTAssertEqual(profit, 0)
+    }
+}
