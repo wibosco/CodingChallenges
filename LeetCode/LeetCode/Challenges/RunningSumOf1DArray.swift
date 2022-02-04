c5892a210fd63fde6a2b18fb494682b18985717b
William Boles <wibosco@users.noreply.github.com>
2022-02-04 14:38:16 +0000

    Implemented 'Running Sum of 1d Array' challenge

---
+++ LeetCode/LeetCode/Challenges/RunningSumOf1DArray.swift
@@ -0,0 +1,32 @@
+//
+//  RunningSumOf1DArray.swift
+//  LeetCode
+//
+//  Created by William Boles on 04/02/2022.
+//
+
+import Foundation
+
+//https://leetcode.com/problems/running-sum-of-1d-array/
+//prefix sum
+struct RunningSumOf1DArray {
+    
+    //Time: O(n) where `n` is the number of elements in `nums`
+    //Space: O(n)
+    //array
+    //
+    //Solution Description:
+    //Using the prefix sum technique we iterate through the `nums` array and adding nums[i] to running total of all the
+    //previous values of `nums` which we can get from the last elements in `prefixSum`
+    static func runningSum(_ nums: [Int]) -> [Int] {
+        var prefixSum = [Int]()
+        prefixSum.append(nums[0])
+        
+        for num in nums[1...] {
+            let value = prefixSum.last! + num
+            prefixSum.append(value)
+        }
+        
+        return prefixSum
+    }
+}
---
+++ LeetCode/LeetCodeTests/Tests/RunningSumOf1DArrayTests.swift
@@ -0,0 +1,39 @@
+//
+//  RunningSumOf1DArrayTests.swift
+//  LeetCodeTests
+//
+//  Created by William Boles on 04/02/2022.
+//
+
+import XCTest
+
+@testable import LeetCode
+
+class RunningSumOf1DArrayTests: XCTestCase {
+
+    // MARK: - Tests
+    
+    func test_A() {
+        let nums = [1, 2, 3, 4]
+        
+        let array = RunningSumOf1DArray.runningSum(nums)
+        
+        XCTAssertEqual(array, [1, 3, 6, 10])
+    }
+    
+    func test_B() {
+        let nums = [1, 1, 1, 1, 1]
+        
+        let array = RunningSumOf1DArray.runningSum(nums)
+        
+        XCTAssertEqual(array, [1, 2, 3, 4, 5])
+    }
+    
+    func test_C() {
+        let nums = [3, 1, 2, 10, 1]
+        
+        let array = RunningSumOf1DArray.runningSum(nums)
+        
+        XCTAssertEqual(array, [3, 4, 6, 16, 17])
+    }
+}
