f02f2d5784347a57b78c348a106a4e84b75e7802
William Boles <wibosco@users.noreply.github.com>
2022-02-28 18:17:18 +0000

    Implemented 'Moving Average' challenge

---
+++ LeetCode/LeetCode/Challenges/MovingAverage.swift
@@ -0,0 +1,33 @@
+//
+//  MovingAverage.swift
+//  LeetCode
+//
+//  Created by William Boles on 28/02/2022.
+//
+
+import Foundation
+
+//https://leetcode.com/problems/moving-average-from-data-stream/
+//design
+//
+//Time: O(n) where `n` is the window size
+//Space: O(1)
+class MovingAverage {
+    private let size: Int
+    private var values = [Int]()
+    private var sum = 0
+    
+    init(_ size: Int) {
+        self.size = size
+    }
+    
+    func next(_ val: Int) -> Double {
+        values.append(val)
+        
+        let sub = values.count > size ? values.removeFirst() : 0
+        sum = sum - sub + val
+        let divider = min(values.count, size)
+        
+        return (Double(sum) / Double(divider))
+    }
+}
---
+++ LeetCode/LeetCodeTests/Tests/MovingAverageTests.swift
@@ -0,0 +1,23 @@
+//
+//  MovingAverageTests.swift
+//  LeetCodeTests
+//
+//  Created by William Boles on 28/02/2022.
+//
+
+import XCTest
+
+@testable import LeetCode
+
+class MovingAverageTests: XCTestCase {
+    
+    // MARK: - Tests
+    
+    func test_A() {
+        let sut = MovingAverage(3)
+        XCTAssertEqual(sut.next(1), 1.0, accuracy: 0.1)
+        XCTAssertEqual(sut.next(10), 5.5, accuracy: 0.1)
+        XCTAssertEqual(sut.next(3), 4.6, accuracy: 0.1)
+        XCTAssertEqual(sut.next(5), 6.0, accuracy: 0.1)
+    }
+}
