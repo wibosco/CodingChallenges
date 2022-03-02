17d17981de545926c56aa2e15377ef1610607d13
William Boles <wibosco@users.noreply.github.com>
2022-03-02 10:05:08 +0000

    Implemented 'Exclusive Time of Functions' challenge

---
+++ LeetCode/LeetCode/Challenges/ExclusiveTimeOfFunctions.swift
@@ -0,0 +1,87 @@
+//
+//  ExclusiveTimeOfFunctions.swift
+//  LeetCode
+//
+//  Created by William Boles on 01/03/2022.
+//
+
+import Foundation
+
+//https://leetcode.com/problems/exclusive-time-of-functions/
+//array
+struct ExclusiveTimeOfFunctions {
+    
+    //Time: O(n) where `n` is the number of elements in `logs`
+    //Space: O(n) where `n` is the number of elements in `logs` (actually O(n/2))
+    //stack
+    //
+    //Solution Description:
+    //In order to calculate how long a process has been running we to stitch together all the times that process has had sole
+    //use of the CPU. There are two scenarios where a process isn't running:
+    //
+    //1. It has been paused (because another process is now running)
+    //2. It has been ended
+    //
+    //For both these scenarios we need to calculate the difference between the previous timestamp and current timestamp to
+    //determine that run time. So we are effectively uses the present/current process to determine the runtime of the previous
+    //process. As a process can be paused and/or have multiple instances we can stitch it's running time together adding to a
+    //running total associated with that process - `times`, with the process ID being the index. We can use a stack to hold any
+    //running/paused processes. When a new process starts we peek at the previous process on the stack to update that previous
+    //processes running time value. When a process ends we pop from the stack to and update that ended processes running time
+    //value. As we know that every start has a end we don't need to handle any cases around non-ended processes.
+    static func exclusiveTime(_ n: Int, _ logs: [String]) -> [Int] {
+        var stack = [Log]()
+        var times = Array(repeating: 0, count: n)
+        
+        var time = 0 //timestamp of last work - start or end
+        
+        for i in 0..<logs.count {
+            let currentLog = parse(logs[i])
+            
+            switch currentLog.type {
+            case .start:
+                if let previousLog = stack.last {
+                    let timeElapsed = currentLog.timestamp - time
+                    times[previousLog.functionID] += timeElapsed
+                }
+                stack.append(currentLog)
+            case .end:
+                //every start has an end so last element in the stack should be the start log of this current end log
+                let previousLog = stack.removeLast()
+                let timeElapsed = currentLog.timestamp - time
+                times[previousLog.functionID] += timeElapsed
+            }
+
+            time = currentLog.timestamp
+        }
+        
+        return times
+    }
+    
+    private static func parse(_ log: String) -> Log {
+        let components = log.split(separator: ":")
+        
+        let functionID = Int(components[0])!
+        let type = components[1] == "start" ? Log.LogType.start : Log.LogType.end
+        //when a process ends it does at the end of the timestamp i.e. the start of the next second
+        //when a process starts it does so at the start of the timestamp i.e. the start of the current second
+        //so we need to offset on end but not start
+        let timestampOffset = type == .end ? 1 : 0
+        let timestamp =  Int(components[2])! + timestampOffset
+        
+        let log = Log(functionID: functionID, type: type, timestamp: timestamp)
+        
+        return log
+    }
+}
+
+private struct Log {
+    enum LogType {
+        case start
+        case end
+    }
+    
+    let functionID: Int
+    let type: LogType
+    let timestamp: Int
+}
---
+++ LeetCode/LeetCodeTests/Tests/ExclusiveTimeOfFunctionsTests.swift
@@ -0,0 +1,42 @@
+//
+//  ExclusiveTimeOfFunctionsTests.swift
+//  LeetCodeTests
+//
+//  Created by William Boles on 01/03/2022.
+//
+
+import XCTest
+
+@testable import LeetCode
+
+class ExclusiveTimeOfFunctionsTests: XCTestCase {
+
+    // MARK: - Tests
+    
+    func test_A() {
+        let n = 2
+        let logs = ["0:start:0", "1:start:2", "1:end:5", "0:end:6"]
+        
+        let times = ExclusiveTimeOfFunctions.exclusiveTime(n, logs)
+        
+        XCTAssertEqual(times, [3, 4])
+    }
+    
+    func test_B() {
+        let n = 1
+        let logs = ["0:start:0", "0:start:2", "0:end:5", "0:start:6", "0:end:6", "0:end:7"]
+        
+        let times = ExclusiveTimeOfFunctions.exclusiveTime(n, logs)
+        
+        XCTAssertEqual(times, [8])
+    }
+    
+    func test_C() {
+        let n = 2
+        let logs = ["0:start:0", "0:start:2", "0:end:5", "1:start:6", "1:end:6", "0:end:7"]
+        
+        let times = ExclusiveTimeOfFunctions.exclusiveTime(n, logs)
+        
+        XCTAssertEqual(times, [7, 1])
+    }
+}
