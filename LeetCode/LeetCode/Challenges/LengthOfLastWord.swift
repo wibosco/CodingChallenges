9b8b8d062f5dc97eefa5a9dd2da093ad1e706420
William Boles <wibosco@users.noreply.github.com>
2022-03-14 22:29:59 +0000

    Implemented 'Length of Last Word' challenge

---
+++ LeetCode/LeetCode/Challenges/LengthOfLastWord.swift
@@ -0,0 +1,37 @@
+//
+//  LengthOfLastWord.swift
+//  LeetCode
+//
+//  Created by William Boles on 14/03/2022.
+//
+
+import Foundation
+
+//https://leetcode.com/problems/length-of-last-word/
+//string
+struct LengthOfLastWord {
+    
+    //Time: O(n) where `n` is the number of characters in `s`
+    //Space: O(1)
+    //
+    //Solution Description:
+    //Iterating `s` in reverse we first filter out any space characters (" ") that we encounter until we hit our first
+    //non-space character at which point we start to count those characters. When continue counting until we either
+    //reach the start of `s` or we encounter another space character at which point we our loop as the first word is
+    //over.
+    static func lengthOfLastWord(_ s: String) -> Int {
+        var wordCount = 0
+        
+        for c in s.reversed() {
+            if c == " " && wordCount > 0 {
+              break
+            }
+            
+            if c != " " {
+                wordCount += 1
+            }
+        }
+        
+        return wordCount
+    }
+}
---
+++ LeetCode/LeetCodeTests/Tests/LengthOfLastWordTests.swift
@@ -0,0 +1,39 @@
+//
+//  LengthOfLastWordTests.swift
+//  LeetCodeTests
+//
+//  Created by William Boles on 14/03/2022.
+//
+
+import XCTest
+
+@testable import LeetCode
+
+class LengthOfLastWordTests: XCTestCase {
+
+    // MARK: - Tests
+    
+    func test_A() {
+        let s = "Hello World"
+        
+        let length = LengthOfLastWord.lengthOfLastWord(s)
+        
+        XCTAssertEqual(length, 5)
+    }
+    
+    func test_B() {
+        let s = "   fly me   to   the moon  "
+        
+        let length = LengthOfLastWord.lengthOfLastWord(s)
+        
+        XCTAssertEqual(length, 4)
+    }
+    
+    func test_C() {
+        let s = "luffy is still joyboy"
+        
+        let length = LengthOfLastWord.lengthOfLastWord(s)
+        
+        XCTAssertEqual(length, 6)
+    }
+}
