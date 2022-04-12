d64f4fcb0c8bbdcbdbdb3229451dcd06ddf3c700
William Boles <wibosco@users.noreply.github.com>
2022-04-12 21:44:22 +0100

    Implemented 'Valid Number' challenge

---
+++ LeetCode/LeetCode/Challenges/ValidNumber.swift
@@ -0,0 +1,61 @@
+//
+//  ValidNumber.swift
+//  LeetCode
+//
+//  Created by William Boles on 12/04/2022.
+//
+
+import Foundation
+
+//
+//
+struct ValidNumber {
+    
+    //Time: O(
+    //Space: O(
+    //
+    //Solution Description:
+    //
+    static func isNumber(_ s: String) -> Bool {
+        let characters = Array(s)
+        
+        var i = 0
+        var foundSign = false
+        var foundDot = false
+        var foundExponent = false
+        var foundNumber = false
+        
+        if characters[0] == "+" || characters[0] == "-" {
+            i += 1
+        }
+        
+        for c in characters[i...] {
+            if c == "." {
+                if foundDot || foundExponent {
+                    return false
+                }
+                
+                foundDot = true
+            } else if c.isWholeNumber {
+                foundNumber = true
+            } else if c == "e" || c == "E" {
+                if foundExponent || !foundNumber {
+                    return false
+                }
+                
+                foundNumber = false
+                foundExponent = true
+            } else if c == "+" || c == "-" {
+                if foundExponent && !foundSign && !foundNumber {
+                    foundSign = true
+                }  else {
+                    return false
+                }
+            } else {
+                return false
+            }
+        }
+        
+        return foundNumber
+    }
+}
---
+++ LeetCode/LeetCodeTests/Tests/ValidNumberTests.swift
@@ -0,0 +1,151 @@
+//
+//  ValidNumberTests.swift
+//  LeetCodeTests
+//
+//  Created by William Boles on 12/04/2022.
+//
+
+import XCTest
+
+@testable import LeetCode
+
+class ValidNumberTests: XCTestCase {
+
+    // MARK: - Tests
+    
+    func test_A() {
+        let s = "0"
+        
+        let isValid = ValidNumber.isNumber(s)
+        
+        XCTAssertTrue(isValid)
+    }
+    
+    func test_B() {
+        let s = "e"
+        
+        let isValid = ValidNumber.isNumber(s)
+        
+        XCTAssertFalse(isValid)
+    }
+    
+    func test_C() {
+        let s = "."
+        
+        let isValid = ValidNumber.isNumber(s)
+        
+        XCTAssertFalse(isValid)
+    }
+    
+    func test_D() {
+        let s = "2.0e6"
+        
+        let isValid = ValidNumber.isNumber(s)
+        
+        XCTAssertTrue(isValid)
+    }
+    
+    func test_E() {
+        let s = "2.0E6"
+        
+        let isValid = ValidNumber.isNumber(s)
+        
+        XCTAssertTrue(isValid)
+    }
+    
+    func test_F() {
+        let s = "G2.0E6"
+        
+        let isValid = ValidNumber.isNumber(s)
+        
+        XCTAssertFalse(isValid)
+    }
+    
+    func test_G() {
+        let s = "2E6.9"
+        
+        let isValid = ValidNumber.isNumber(s)
+        
+        XCTAssertFalse(isValid)
+    }
+    
+    func test_H() {
+        let s = "2Ee6"
+        
+        let isValid = ValidNumber.isNumber(s)
+        
+        XCTAssertFalse(isValid)
+    }
+    
+    func test_I() {
+        let s = "-2E6"
+        
+        let isValid = ValidNumber.isNumber(s)
+        
+        XCTAssertTrue(isValid)
+    }
+    
+    func test_J() {
+        let s = "+2E6"
+        
+        let isValid = ValidNumber.isNumber(s)
+        
+        XCTAssertTrue(isValid)
+    }
+    
+    func test_K() {
+        let s = "2E-6"
+        
+        let isValid = ValidNumber.isNumber(s)
+        
+        XCTAssertTrue(isValid)
+    }
+    
+    func test_L() {
+        let s = "2E+6"
+        
+        let isValid = ValidNumber.isNumber(s)
+        
+        XCTAssertTrue(isValid)
+    }
+    
+    func test_M() {
+        let s = "2E++6"
+        
+        let isValid = ValidNumber.isNumber(s)
+        
+        XCTAssertFalse(isValid)
+    }
+    
+    func test_N() {
+        let s = "--2E6"
+        
+        let isValid = ValidNumber.isNumber(s)
+        
+        XCTAssertFalse(isValid)
+    }
+    
+    func test_O() {
+        let s = "E6"
+        
+        let isValid = ValidNumber.isNumber(s)
+        
+        XCTAssertFalse(isValid)
+    }
+    
+    func test_P() {
+        let s = "6."
+        
+        let isValid = ValidNumber.isNumber(s)
+        
+        XCTAssertTrue(isValid)
+    }
+    
+    func test_Q() {
+        let s = "459277e38+"
+        
+        let isValid = ValidNumber.isNumber(s)
+        
+        XCTAssertFalse(isValid)
+    }
+}
