9ccf7d9f0aeba2a10e84df649fd9f4cc904c5abe
William Boles <wibosco@users.noreply.github.com>
2022-04-13 08:53:29 +0100

    Implemented 'Game Of Life' challenge

---
+++ LeetCode/LeetCode/Challenges/GameOfLife.swift
@@ -0,0 +1,80 @@
+//
+//  GameOfLife.swift
+//  LeetCode
+//
+//  Created by William Boles on 13/04/2022.
+//
+
+import Foundation
+
+//https://leetcode.com/problems/game-of-life/
+//matrix
+struct GameOfLife {
+    
+    //Time: O(n * m) where n is the number of rows, m is the the number of columns
+    //Space: O(n * m)
+    //relative indexing
+    //multi-source
+    //
+    //Solution Description:
+    //Iterate through `board` and using relative indexing determine the state of cells surrounding each cell. We then
+    //record the changes to that cell in a new board `nextBoard` so that the changes that we make don't affect any
+    //unprocessed cells on the original board - `board`.
+    static func gameOfLife(_ board: inout [[Int]]) {
+        var nextBoard = Array(repeating: Array(repeating: 0, count: board[0].count), count: board.count)
+        
+        let relativeIndexing = [[-1, -1], [-1, 0], [-1, 1],
+                                [0, -1],           [0, 1],
+                                [1, -1],  [1, 0],  [1, 1]]
+        
+        for row in 0..<board.count {
+            for column in 0..<board[row].count {
+                let outcome = update(board, row, column, relativeIndexing)
+                
+                nextBoard[row][column] = outcome
+            }
+        }
+        
+        board = nextBoard
+    }
+    
+    private static func update(_ board: [[Int]], _ row: Int, _ column: Int, _ relativeIndexing: [[Int]]) -> Int {
+        let isAlive = board[row][column] == 1
+        var liveNeighbors = 0
+        
+        for relativeIndex in relativeIndexing {
+            let newRow = row + relativeIndex[0]
+            let newColumn = column + relativeIndex[1]
+            
+            guard newRow >= 0, newRow < board.count else {
+                continue
+            }
+            
+            guard newColumn >= 0, newColumn < board[newRow].count else {
+                continue
+            }
+            
+            if board[newRow][newColumn] == 1 {
+                liveNeighbors += 1
+            }
+        }
+        
+        var outcome = 0
+        
+        if isAlive {
+            if liveNeighbors < 2 {
+                outcome = 0
+            } else if liveNeighbors < 4 {
+                outcome = 1
+            } else {
+                outcome = 0
+            }
+        } else {
+            if liveNeighbors == 3 {
+                outcome = 1
+            }
+        }
+        
+        return outcome
+    }
+}
---
+++ LeetCode/LeetCodeTests/Tests/GameOfLifeTests.swift
@@ -0,0 +1,35 @@
+//
+//  GameOfLifeTests.swift
+//  LeetCodeTests
+//
+//  Created by William Boles on 13/04/2022.
+//
+
+import XCTest
+
+@testable import LeetCode
+
+class GameOfLifeTests: XCTestCase {
+
+    // MARK: - Tests
+    
+    func test_A() {
+        var board = [[0, 1, 0],
+                     [0, 0, 1],
+                     [1, 1, 1],
+                     [0, 0, 0]]
+        
+        GameOfLife.gameOfLife(&board)
+        
+        XCTAssertEqual(board, [[0, 0, 0], [1, 0, 1], [0, 1, 1], [0, 1, 0]])
+    }
+    
+    func test_B() {
+        var board = [[1, 1],
+                     [1, 0]]
+        
+        GameOfLife.gameOfLife(&board)
+        
+        XCTAssertEqual(board, [[1, 1], [1, 1]])
+    }
+}
