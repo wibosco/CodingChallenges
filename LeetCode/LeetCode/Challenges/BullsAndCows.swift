df339c6ad4c1c99bf1b450ade1454c8ca2702c61
William Boles <wibosco@users.noreply.github.com>
2023-04-01 23:51:25 +0100

    Implemented 'Bulls And Cows' challenge

---
+++ LeetCode/LeetCode/Challenges/BullsAndCows.swift
@@ -0,0 +1,48 @@
+//
+//  BullsAndCows.swift
+//  LeetCode
+//
+//  Created by William Boles on 01/04/2023.
+//
+
+import Foundation
+
+//https://leetcode.com/problems/bulls-and-cows/
+//string
+struct BullsAndCows {
+    
+    //Time: O(n) where `n` is the number of elements in `secret`/`guess` (both should match)
+    //Space: O(n)
+    //dictionary
+    //
+    //Solution Description:
+    //First we iterate through both `secret` and `guess` and compare each character at the same position against each other. If
+    //the characters match then we have found a `bull` and can increment `bullCount`; if they don't match we have potentnally
+    //found `cows` and we add each to `nonMatchingSecret` and `nonMatchingGuess`. Once all bulls have been found, we use the
+    //keys of `nonMatchingGuess` to find at `cow` characters. As each `cow` character must be paired between `nonMatchingSecret`
+    //and `nonMatchingGuess` we take the min of the counts that we collected as this will only include the pairing reguardless
+    //of which dictionary holds the smaller count. Finally we format and return the counts.
+    static func getHint(_ secret: String, _ guess: String) -> String {
+        var bullCount = 0
+        var cowCount = 0
+
+        var nonMatchingSecret = [Character: Int]()
+        var nonMatchingGuess = [Character: Int]()
+
+        for (sChar, gChar) in zip(secret, guess) {
+            guard sChar != gChar else {
+                bullCount += 1
+                continue
+            }
+
+            nonMatchingSecret[sChar, default: 0] += 1
+            nonMatchingGuess[gChar, default: 0] += 1
+        }
+
+        for (key, val) in nonMatchingGuess {
+            cowCount += min(nonMatchingSecret[key, default: 0], val)
+        }
+
+        return "\(bullCount)A\(cowCount)B"
+    }
+}
---
+++ LeetCode/LeetCodeTests/Tests/BullsAndCowsTest.swift
@@ -0,0 +1,33 @@
+//
+//  BullsAndCowsTest.swift
+//  LeetCodeTests
+//
+//  Created by William Boles on 01/04/2023.
+//
+
+import XCTest
+
+@testable import LeetCode
+
+final class BullsAndCowsTest: XCTestCase {
+    
+    //MARK: - Tests
+    
+    func testA() {
+        let secret = "1807"
+        let guess = "7810"
+        
+        let hint = BullsAndCows.getHint(secret, guess)
+        
+        XCTAssertEqual(hint, "1A3B")
+    }
+    
+    func testB() {
+        let secret = "1123"
+        let guess = "0111"
+        
+        let hint = BullsAndCows.getHint(secret, guess)
+        
+        XCTAssertEqual(hint, "1A1B")
+    }
+}
