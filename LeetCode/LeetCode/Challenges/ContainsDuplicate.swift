90579ce87944501bd8f8497fb8894cc21d2495ae
William Boles <wibosco@users.noreply.github.com>
2023-04-08 22:52:10 +0100

    Implemented 'Contains Duplicate' challenge

---
+++ LeetCode/LeetCode/Challenges/ContainsDuplicate.swift
@@ -0,0 +1,36 @@
+//
+//  ContainsDuplicate.swift
+//  LeetCode
+//
+//  Created by William Boles on 08/04/2023.
+//
+
+import Foundation
+
+//https://leetcode.com/problems/contains-duplicate/
+//array
+struct ContainsDuplicate {
+    
+    //Time: O(n) where `n` is the number of elements in `nums`
+    //Space: O(n)
+    //set
+    //
+    //Solution Description:
+    //As we iterate through `nums` we store each element we come across in `seen`. As `seen` is it `Set` it allows O(1) access so
+    //whenever we encounter a new element we check if it already in `seen`. If it is in `seen` we have a duplicate and can
+    //immediately exit and return true; if it isn't in `seen` we add to `seen` and continue onto the next element. If we iterate
+    //through all the elements in `nums` we return false as it contains no duplicates.
+    static func containsDuplicate(_ nums: [Int]) -> Bool {
+        var seen = Set<Int>()
+        
+        for num in nums {
+            guard !seen.contains(num) else {
+                return true
+            }
+            
+            seen.insert(num)
+        }
+        
+        return false
+    }
+}
---
+++ LeetCode/LeetCodeTests/Tests/ContainsDuplicateTests.swift
@@ -0,0 +1,39 @@
+//
+//  ContainsDuplicateTests.swift
+//  LeetCodeTests
+//
+//  Created by William Boles on 08/04/2023.
+//
+
+import XCTest
+
+@testable import LeetCode
+
+final class ContainsDuplicateTests: XCTestCase {
+
+    //MARK: - Tests
+    
+    func test_A() {
+        let nums = [1, 2, 3, 1]
+        
+        let result = ContainsDuplicate.containsDuplicate(nums)
+        
+        XCTAssertTrue(result)
+    }
+    
+    func test_B() {
+        let nums = [1, 2, 3, 4]
+        
+        let result = ContainsDuplicate.containsDuplicate(nums)
+        
+        XCTAssertFalse(result)
+    }
+    
+    func test_C() {
+        let nums = [1, 1, 1, 3, 3, 4, 3, 2, 4, 2]
+        
+        let result = ContainsDuplicate.containsDuplicate(nums)
+        
+        XCTAssertTrue(result)
+    }
+}
