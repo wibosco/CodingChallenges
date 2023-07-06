072d30ada5e86bdf6e41a194856cc688601d9171
William Boles <wibosco@users.noreply.github.com>
2023-07-06 12:29:45 +0100

    Implemented 'Replace Elements with Greatest Element on Right Side' challenge

---
+++ LeetCode/LeetCode/Challenges/ReplaceElementsWithGreatestElementOnRightSide.swift
@@ -0,0 +1,33 @@
+//
+//  ReplaceElementsWithGreatestElementOnRightSide.swift
+//  LeetCode
+//
+//  Created by William Boles on 06/07/2023.
+//
+
+import Foundation
+
+//https://leetcode.com/problems/replace-elements-with-greatest-element-on-right-side/
+//array
+struct ReplaceElementsWithGreatestElementOnRightSide {
+    
+    //Time: O(n) where n is the number of elements in `arr`
+    //Space: O(n)
+    //
+    //Solution Description:
+    //Going right-to-left we iterate through `arr` and hold onto the highest/biggest value that we have seen so far - `rightMax`.
+    //This value is used to set the element at the same index as we are iterating at in `result`, we then comapre the current
+    //value against `rightMax` and take the larger of the two as the next `rightMax`. We repeat this process until all elements
+    //have been seen.
+    static func replaceElements(_ arr: [Int]) -> [Int] {
+        var result = Array(repeating: -1, count: arr.count)
+        var rightMax = -1
+        
+        for (i, val) in arr.enumerated().reversed() {
+            result[i] = rightMax
+            rightMax = max(rightMax, val)
+        }
+        
+        return result
+    }
+}
---
+++ LeetCode/LeetCodeTests/Tests/ReplaceElementsWithGreatestElementOnRightSideTests.swift
@@ -0,0 +1,31 @@
+//
+//  ReplaceElementsWithGreatestElementOnRightSideTests.swift
+//  LeetCodeTests
+//
+//  Created by William Boles on 06/07/2023.
+//
+
+import XCTest
+
+@testable import LeetCode
+
+final class ReplaceElementsWithGreatestElementOnRightSideTests: XCTestCase {
+
+    //MARK: - Tests
+    
+    func test_A() {
+        let arr = [17, 18, 5, 4, 6, 1]
+            
+        let result = ReplaceElementsWithGreatestElementOnRightSide.replaceElements(arr)
+        
+        XCTAssertEqual(result, [18, 6, 6, 6, 1, -1])
+    }
+    
+    func test_B() {
+        let arr = [400]
+            
+        let result = ReplaceElementsWithGreatestElementOnRightSide.replaceElements(arr)
+        
+        XCTAssertEqual(result, [-1])
+    }
+}
