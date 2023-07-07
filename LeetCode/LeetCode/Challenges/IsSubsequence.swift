9163b599e2cdb7de08c1436e86caa57b956de6a8
William Boles <wibosco@users.noreply.github.com>
2023-07-07 13:34:31 +0100

    Implemented 'Is Subsequence' challenge

---
+++ LeetCode/LeetCode/Challenges/IsSubsequence.swift
@@ -0,0 +1,44 @@
+//
+//  IsSubsequence.swift
+//  LeetCode
+//
+//  Created by William Boles on 07/07/2023.
+//
+
+import Foundation
+
+//https://leetcode.com/problems/is-subsequence/
+//array
+struct IsSubsequence {
+    
+    //Time: O(n) where n is the larger of `s` or `t`
+    //Space: O(n)
+    //two pointers
+    //
+    //Solution Description:
+    //Using two pointers we iterate through `s` and `t` comparing the element at `sP` against the that at `tP`. If they match
+    //then we advance/increment the `sP` to search for the next character in the sequence. Regardless of matching or not we
+    //always advance/increment the `tP`. By iterating linearly through `t` we ensure that `s` is matched in the correct
+    //order. At the end we know if all of `s` was found in `t`, if `sP` has went beyond the elements of `s`.
+    static func isSubsequence(_ s: String, _ t: String) -> Bool {
+        guard t.count >= s.count else {
+            return false
+        }
+        
+        var sP = 0
+        var tP = 0
+        
+        let sCharacters = Array(s)
+        let tCharacters = Array(t)
+        
+        while sP < sCharacters.count && tP < tCharacters.count {
+            if sCharacters[sP] == tCharacters[tP] {
+                sP += 1
+            }
+            
+            tP += 1
+        }
+        
+        return sP == sCharacters.count
+    }
+}
---
+++ LeetCode/LeetCodeTests/Tests/IsSubsequenceTests.swift
@@ -0,0 +1,42 @@
+//
+//  IsSubsequence.swift
+//  LeetCodeTests
+//
+//  Created by William Boles on 07/07/2023.
+//
+
+import XCTest
+
+@testable import LeetCode
+
+final class IsSubsequenceTests: XCTestCase {
+
+    //MARK: - Tests
+    
+    func test_A() {
+        let s = "abc"
+        let t = "ahbgdc"
+        
+        let result = IsSubsequence.isSubsequence(s, t)
+        
+        XCTAssertTrue(result)
+    }
+    
+    func test_B() {
+        let s = "axc"
+        let t = "ahbgdc"
+        
+        let result = IsSubsequence.isSubsequence(s, t)
+        
+        XCTAssertFalse(result)
+    }
+    
+    func test_C() {
+        let s = "abc"
+        let t = "ahbgdcz"
+        
+        let result = IsSubsequence.isSubsequence(s, t)
+        
+        XCTAssertTrue(result)
+    }
+}
