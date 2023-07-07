6bcd150672be14422bc18109a4a97208aaf4180c
William Boles <wibosco@users.noreply.github.com>
2023-07-07 15:49:28 +0100

    Implemented 'Unique Email Addresses' challenge

---
+++ LeetCode/LeetCode/Challenges/UniqueEmailAddresses.swift
@@ -0,0 +1,104 @@
+//
+//  UniqueEmailAddresses.swift
+//  LeetCode
+//
+//  Created by William Boles on 07/07/2023.
+//
+
+import Foundation
+
+//https://leetcode.com/problems/unique-email-addresses/
+//array
+struct UniqueEmailAddresses {
+    
+    //Time: O(n * m) where n is the elements in `emails`, m is the number of characters in each email
+    //Space: O(n)
+    //nested loops
+    //set
+    //
+    //Solution Description:
+    //Iterating through each email we prune out the undesired characters `.` and `+` from before the `@`. With `.` characters
+    //we simply remove them from the string and with `+` we remove them and all characters that follow until we get to an `@`
+    //To do this we first split the email around the `@` and then prune the local section. If we encounter a `.` we skip it
+    //and if we encounter a `+` we break the loop, for any other characters we add it to `pruneLocal`. Once we iterate
+    //through all characters in the email address (removing those characters we don't want) we add the sanitised email to a
+    //set so that any duplicate entries are automatically merged/removed. Once all emails are parsed we return the count of
+    //that set.
+    static func numUniqueEmails(_ emails: [String]) -> Int {
+        var uniqueEmails = Set<String>()
+        
+        for email in emails {
+            let elements = email.split(separator: "@")
+            let local = Array(elements[0])
+            let domain = elements[1]
+            
+            var prunedLocal = [Character]()
+            for c in local {
+                if c == "+" {
+                    break
+                }
+                
+                if c == "." {
+                    continue
+                }
+                
+                prunedLocal.append(c)
+            }
+            
+            let prunedEmail = "\(String(prunedLocal))@\(domain)"
+            uniqueEmails.insert(prunedEmail)
+        }
+        
+        return uniqueEmails.count
+    }
+    
+    //Time: O(n * m) where n is the elements in `emails`, m is the number of characters in each email
+    //Space: O(n)
+    //nested loops
+    //set
+    //
+    //Solution Description:
+    //Iterating through each email we prune out the undesired characters `.` and `+` from before the `@`. With `.` characters
+    //we simply remove them from the string and with `+` we remove them and all characters that follow until we get to an `@`
+    //To control this we have two local variable `prune` and `ignore` - `prune` controls whether we care about the characters
+    //in this section of the email address, `ignore` controls whether we skip the current character. Once we iterate through
+    //all characters in the email address (removing those characters we don't want) we add the sanitised email to a set so
+    //that any duplicate entries are automatically merged/removed. Once all emails are parsed we return the count of that set.
+    static func numUniqueEmailsCharacter(_ emails: [String]) -> Int {
+        var uniqueEmails = Set<String>()
+        
+        for email in emails {
+            let characters = Array(email)
+            
+            var prune = true
+            var ignore = false
+            
+            var prunedEmail = [Character]()
+            for c in characters {
+                if c == "@" {
+                    prune = false
+                    ignore = false
+                }
+                
+                if ignore {
+                    continue
+                }
+                
+                if (c == "+") && prune {
+                    ignore = true
+                    continue
+                }
+                
+                if (c == ".") && prune {
+                    continue
+                }
+                
+                prunedEmail.append(c)
+            }
+            
+            uniqueEmails.insert(String(prunedEmail))
+        }
+        
+        return uniqueEmails.count
+    }
+}
---
+++ LeetCode/LeetCodeTests/Tests/UniqueEmailAddressesTests.swift
@@ -0,0 +1,31 @@
+//
+//  UniqueEmailAddressesTests.swift
+//  LeetCodeTests
+//
+//  Created by William Boles on 07/07/2023.
+//
+
+import XCTest
+
+@testable import LeetCode
+
+final class UniqueEmailAddressesTests: XCTestCase {
+
+    //MARK: - Tests
+    
+    func test_A() {
+        let emails = ["test.email+alex@leetcode.com", "test.e.mail+bob.cathy@leetcode.com", "testemail+david@lee.tcode.com"]
+        
+        let result = UniqueEmailAddresses.numUniqueEmails(emails)
+        
+        XCTAssertEqual(result, 2)
+    }
+    
+    func test_B() {
+        let emails = ["a@leetcode.com", "b@leetcode.com", "c@leetcode.com"]
+        
+        let result = UniqueEmailAddresses.numUniqueEmails(emails)
+        
+        XCTAssertEqual(result, 3)
+    }
+}
