1d9789cbcd9a680a25f622300bb71d515929895f
William Boles <wibosco@users.noreply.github.com>
2023-07-07 16:59:45 +0100

    Implemented 'Find All Numbers Disappeared in an Array' challenge

---
+++ LeetCode/LeetCode/Challenges/FindAllNumbersDisappearedInAnArray.swift
@@ -0,0 +1,69 @@
+//
+//  FindAllNumbersDisappearedInAnArray.swift
+//  LeetCode
+//
+//  Created by William Boles on 07/07/2023.
+//
+
+import Foundation
+
+//https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/
+//array
+struct FindAllNumbersDisappearedInAnArray {
+    
+    //Time: O(n) where n is the number of elements in `nums`
+    //Space: O(1)
+    //marking
+    //
+    //Solution Description:
+    //To avoid using extra storage we can update `nums` to indicate that an index is included by setting that value of
+    //that index to be negative. We first iterate through `nums` and use the absolute value of the current index as the
+    //index of the element that we want to "mark". We use the absolute value as it's possible the value at any given
+    //index has already been marked so we need to be careful not to undo that. After the first iteration we then iterate
+    //again and add any indexes that have a positive value to `disappearedNumbers` as that index isn't included as a
+    //value of `nums`. Please note that as our output is expected to start at 1 we need to add or subtrate 1 when
+    //required.
+    static func findDisappearedNumbers(_ nums: [Int]) -> [Int] {
+        var disappearedNumbers = [Int]()
+        var nums = nums
+        
+        for num in nums {
+            //use the absolute value incase this index has already been "marked"
+            let absNum = (abs(num) - 1) //as indexing starts at 1 to get the "true" index we subtrate 1
+            if absNum < nums.count {
+                nums[absNum] = abs(nums[absNum]) * -1
+            }
+        }
+        
+        for (i, num) in nums.enumerated() {
+            if num >= 0 {
+                disappearedNumbers.append((i + 1)) //as indexing starts at 1 to get the output index we 1
+            }
+        }
+        
+        return disappearedNumbers
+    }
+    
+    //Time: O(n) where n is the number of elements in `nums`
+    //Space: O(n)
+    //set
+    //
+    //Solution Description:
+    //First we convert the `nums` array into a set to allow for O(1) retrieval time. We then iterate through the range 1...n
+    //and check if that index is in the set `numsSet`. If that index is not in the set then we append it to
+    //`disappearedNumbers`; if that index is in the set we skip over it.
+    static func findDisappearedNumbersExtraStorage(_ nums: [Int]) -> [Int] {
+        var disappearedNumbers = [Int]()
+        let numsSet = Set(nums)
+        
+        for i in 1...nums.count { //indexing starts at 1 so we do here
+            guard !numsSet.contains(i) else {
+                continue
+            }
+            
+            disappearedNumbers.append(i)
+        }
+        
+        return disappearedNumbers
+    }
+}
---
+++ LeetCode/LeetCodeTests/Tests/FindAllNumbersDisappearedInAnArrayTests.swift
@@ -0,0 +1,31 @@
+//
+//  FindAllNumbersDisappearedInAnArrayTests.swift
+//  LeetCodeTests
+//
+//  Created by William Boles on 07/07/2023.
+//
+
+import XCTest
+
+@testable import LeetCode
+
+final class FindAllNumbersDisappearedInAnArrayTests: XCTestCase {
+
+    //MARK: - Tests
+    
+    func test_A() {
+        let nums = [4, 3, 2, 7, 8, 2, 3, 1]
+        
+        let result = FindAllNumbersDisappearedInAnArray.findDisappearedNumbers(nums)
+        
+        XCTAssertEqual(result, [5, 6])
+    }
+    
+    func test_B() {
+        let nums = [1, 1]
+        
+        let result = FindAllNumbersDisappearedInAnArray.findDisappearedNumbers(nums)
+        
+        XCTAssertEqual(result, [2])
+    }
+}
