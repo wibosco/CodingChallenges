ff677be1b623f82f3f4748a5b84742c31ede6969
William Boles <wibosco@users.noreply.github.com>
2023-07-11 23:20:19 +0100

    Implemented 'All Nodes Distance K in Binary Tree' challenge

---
+++ LeetCode/LeetCode/Challenges/AllNodesDistanceKInBinaryTree.swift
@@ -0,0 +1,100 @@
+//
+//  AllNodesDistanceKInBinaryTree.swift
+//  LeetCode
+//
+//  Created by William Boles on 11/07/2023.
+//
+
+import Foundation
+
+//https://leetcode.com/problems/all-nodes-distance-k-in-binary-tree/
+//binary tree
+struct AllNodesDistanceKInBinaryTree {
+    
+    //Time: O(n) where n is the number of nodes in the tree
+    //Space: O(n)
+    //DFS
+    //recursive
+    //BFS
+    //iterative
+    //dictionary
+    //visited
+    //
+    //Solution Description:
+    //First we build a parent->child dictionary between the `root` and `target` nodes using DFS - this will allow us to traverse
+    //both down and up the tree and find those nodes that are `k` distance away. First we traverse down the tree from the
+    //`target` node using BFS and add any nodes at `k` away from `target` to the `values` array. Next using the `parents`
+    //dictionary we move up a level and search downwards from there. Every time we move up we reduce the possible `k` downward
+    //movements by 1. We need to be careful to avoid re-searching the branch we just came from so we use a blocker node to cut
+    //that branch off - blocker acts like a single entry `visited` set. We repeat the upwards movement and then downwards
+    //searching until `k` is 0.
+    static func distanceK(_ root: BinaryTreeNode?, _ target: BinaryTreeNode?, _ k: Int) -> [Int] {
+        guard let root = root, let target = target else {
+            return []
+        }
+        
+        var parents = [Int: BinaryTreeNode]()
+        
+        buildParentRelationships(root, target, &parents)
+        
+        var values = [Int]()
+        
+        bfs(target, target, k, &values)
+        
+        var child = target
+        var remainingK = k
+        
+        while let parent = parents[child.val], remainingK > 0 {
+            remainingK -= 1
+            
+            bfs(parent, child, remainingK, &values)
+            
+            child = parent
+        }
+        
+        return values
+    }
+    
+    private static func buildParentRelationships(_ node: BinaryTreeNode, _ target: BinaryTreeNode, _ parents: inout [Int: BinaryTreeNode]) {
+        guard node.val != target.val else {
+            return
+        }
+        
+        if let left = node.left {
+            parents[left.val] = node
+            buildParentRelationships(left, target, &parents)
+        }
+        
+        if let right = node.right {
+            parents[right.val] = node
+            buildParentRelationships(right, target, &parents)
+        }
+    }
+    
+    private static func bfs(_ node: BinaryTreeNode, _ blocker: BinaryTreeNode, _ k: Int, _ values: inout [Int]) {
+        var queue = [node]
+        var k = k
+        
+        while !queue.isEmpty && k >= 0 {
+            var newQueue = [BinaryTreeNode]()
+            
+            for node in queue {
+                guard k > 0 else {
+                    values.append(node.val)
+                    continue
+                }
+                
+                if let left = node.left, left.val != blocker.val {
+                    newQueue.append(left)
+                }
+                
+                if let right = node.right, right.val != blocker.val {
+                    newQueue.append(right)
+                }
+            }
+            
+            k -= 1
+            queue = newQueue
+        }
+    }
+}
---
+++ LeetCode/LeetCodeTests/Tests/AllNodesDistanceKInBinaryTreeTests.swift
@@ -0,0 +1,75 @@
+//
+//  AllNodesDistanceKInBinaryTreeTests.swift
+//  LeetCodeTests
+//
+//  Created by William Boles on 11/07/2023.
+//
+
+import XCTest
+
+@testable import LeetCode
+
+final class AllNodesDistanceKInBinaryTreeTests: XCTestCase {
+
+    //MARK: - Tests
+    
+    func test_A() {
+        let data = [3, 5, 1, 6, 2, 0, 8, nil, nil, 7, 4]
+        let root = BinaryTreeNode.deserialize(data)!
+        let targetValue = 5
+        let target = dfs(root, targetValue)
+        let k = 2
+        
+        let result = AllNodesDistanceKInBinaryTree.distanceK(root, target, k)
+        
+        XCTAssertEqual(result.count, 3)
+        XCTAssertEqual(result, [7, 4, 1])
+    }
+    
+    func test_B() {
+        let data = [1]
+        let root = BinaryTreeNode.deserialize(data)!
+        let targetValue = 1
+        let target = dfs(root, targetValue)
+        let k = 3
+        
+        let result = AllNodesDistanceKInBinaryTree.distanceK(root, target, k)
+        
+        XCTAssertEqual(result.count, 0)
+        XCTAssertEqual(result, [])
+    }
+    
+    func test_C() {
+        let data = [0, 2, 1, nil, nil, 3]
+        let root = BinaryTreeNode.deserialize(data)!
+        let targetValue = 3
+        let target = dfs(root, targetValue)
+        let k = 3
+        
+        let result = AllNodesDistanceKInBinaryTree.distanceK(root, target, k)
+        
+        XCTAssertEqual(result.count, 1)
+        XCTAssertEqual(result, [2])
+    }
+}
+
+extension AllNodesDistanceKInBinaryTreeTests {
+    
+    func dfs(_ node: BinaryTreeNode?, _ value: Int) -> BinaryTreeNode? {
+        guard let node = node else {
+            return nil
+        }
+        
+        guard node.val != value else {
+            return node
+        }
+        
+        var target = dfs(node.left, value)
+        
+        if target == nil {
+            target = dfs(node.right, value)
+        }
+        
+        return target
+    }
+}
