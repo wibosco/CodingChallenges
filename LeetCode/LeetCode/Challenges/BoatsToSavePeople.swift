61390a44aed3d12c81d3525967fb1c3e3871c600
William Boles <wibosco@users.noreply.github.com>
2023-07-19 17:00:48 +0100

    Implemented 'Boats to Save People' challenge

---
+++ LeetCode/LeetCode/Challenges/BoatsToSavePeople.swift
@@ -0,0 +1,43 @@
+//
+//  BoatsToSavePeople.swift
+//  LeetCode
+//
+//  Created by William Boles on 18/07/2023.
+//
+
+import Foundation
+
+//https://leetcode.com/problems/boats-to-save-people/
+//array
+struct BoatsToSavePeople {
+    
+    //Time: O(n log n) where n is the number of elements in `people`
+    //Space: O(n)
+    //sorting
+    //greedy
+    //two pointers
+    //
+    //Solution Description:
+    //Sorting `people` in ascending order we can take a greedy two pointer approach to filling the boats. As we fill
+    //the boats we prioritise the larger of the elements in `people` and attempt to add a smaller element in as well.
+    //As each element is added we adjust the pointers inwards.
+    static func numRescueBoats(_ people: [Int], _ limit: Int) -> Int {
+        let sorted = people.sorted()
+        
+        var boats = 0
+        
+        var left = 0
+        var right = sorted.count - 1
+        
+        while left <= right { //needs to be less than or equal otherwise we would skip one element
+            if (sorted[left] + sorted[right]) <= limit { //can two people get on the boat?
+                left += 1
+            }
+            
+            right -= 1 //heavier person always makes the boat
+            boats += 1
+        }
+        
+        return boats
+    }
+}
---
+++ LeetCode/LeetCodeTests/Tests/BoatsToSavePeopleTests.swift
@@ -0,0 +1,51 @@
+//
+//  BoatsToSavePeopleTests.swift
+//  LeetCodeTests
+//
+//  Created by William Boles on 18/07/2023.
+//
+
+import XCTest
+
+@testable import LeetCode
+
+final class BoatsToSavePeopleTests: XCTestCase {
+
+    //MARK: - Tests
+    
+    func test_A() {
+        let people = [1, 2]
+        let limit = 3
+        
+        let result = BoatsToSavePeople.numRescueBoats(people, limit)
+        
+        XCTAssertEqual(result, 1)
+    }
+    
+    func test_B() {
+        let people = [3, 2, 2, 1]
+        let limit = 3
+        
+        let result = BoatsToSavePeople.numRescueBoats(people, limit)
+        
+        XCTAssertEqual(result, 3)
+    }
+    
+    func test_C() {
+        let people = [3, 5, 3, 4]
+        let limit = 5
+        
+        let result = BoatsToSavePeople.numRescueBoats(people, limit)
+        
+        XCTAssertEqual(result, 4)
+    }
+    
+    func test_D() {
+        let people = [5, 1, 4, 2]
+        let limit = 6
+        
+        let result = BoatsToSavePeople.numRescueBoats(people, limit)
+        
+        XCTAssertEqual(result, 2)
+    }
+}
