0e9c3d5ccdb874b8efcca439bec8d4c662445777
William Boles <wibosco@users.noreply.github.com>
2023-07-31 22:07:09 +0100

    Implemented 'Single Element in a Sorted Array' challenge

---
+++ LeetCode/LeetCode/Challenges/SingleElementInASortedArray.swift
@@ -0,0 +1,56 @@
+//
+//  SingleElementInASortedArray.swift
+//  LeetCode
+//
+//  Created by William Boles on 31/07/2023.
+//
+
+import Foundation
+
+//https://leetcode.com/problems/single-element-in-a-sorted-array/
+struct SingleElementInASortedArray {
+    
+    //Time: O(log n) where n is number of elements in `nums`
+    //Space: O(1)
+    //array
+    //binary search
+    //sorted
+    //
+    //Solution Description:
+    //As we have a sorted array, we can find the duplicate by using binary search. The trick here is to spot that an array
+    //containing only doubles will always have an even number of elements. So using binary search to narrow down the search
+    //space, we need to focus on the side of the `nums` that has an odd number of elements. So we can compare our `mid`
+    //element against it's neighbors to determine if `mid` is the left or right of that pair and determine if it should be
+    //on an even or odd index. If there is a mismatch between expectation and reality then we know which way to shift the
+    //search. Of course is possible that `mid` falls on the non-duplicate element in which case we return it immediately.
+    static func singleNonDuplicate(_ nums: [Int]) -> Int {
+        var left = 0
+        var right = (nums.count - 1)
+        
+        while left <= right {
+            let mid = left + (right - left) / 2 //to avoid overflow
+            let midLeft = mid - 1
+            let midRight = mid + 1
+        
+            if midLeft >= 0 && nums[mid] == nums[midLeft] {
+                //mid is the right side of the pair so check if its partner is on an even index
+                if midLeft % 2 == 0 {
+                    left = mid + 1
+                } else {
+                    right = mid
+                }
+            } else if midRight < nums.count && nums[mid] == nums[midRight] {
+                //mid is the left side of the pair so check if it is on an even index
+                if mid % 2 == 0 {
+                    left = mid + 1
+                } else {
+                    right = mid
+                }
+            } else {
+                return nums[mid]
+            }
+        }
+        
+        return -1 //should never get here
+    }
+}
---
+++ LeetCode/LeetCodeTests/Tests/SingleElementInASortedArrayTests.swift
@@ -0,0 +1,63 @@
+//
+//  SingleElementInASortedArrayTests.swift
+//  LeetCodeTests
+//
+//  Created by William Boles on 31/07/2023.
+//
+
+import XCTest
+
+@testable import LeetCode
+
+final class SingleElementInASortedArrayTests: XCTestCase {
+
+    //MARK: - Tests
+    
+    func test_A() {
+        let nums = [1, 1, 2, 3, 3, 4, 4, 8, 8]
+        
+        let result = SingleElementInASortedArray.singleNonDuplicate(nums)
+        
+        XCTAssertEqual(result, 2)
+    }
+    
+    func test_B() {
+        let nums = [3, 3, 7, 7, 10, 11, 11]
+        
+        let result = SingleElementInASortedArray.singleNonDuplicate(nums)
+        
+        XCTAssertEqual(result, 10)
+    }
+    
+    func test_C() {
+        let nums = [1]
+        
+        let result = SingleElementInASortedArray.singleNonDuplicate(nums)
+        
+        XCTAssertEqual(result, 1)
+    }
+    
+    func test_D() {
+        let nums = [1, 1, 2]
+        
+        let result = SingleElementInASortedArray.singleNonDuplicate(nums)
+        
+        XCTAssertEqual(result, 2)
+    }
+    
+    func test_E() {
+        let nums = [1, 2, 2, 3, 3]
+        
+        let result = SingleElementInASortedArray.singleNonDuplicate(nums)
+        
+        XCTAssertEqual(result, 1)
+    }
+    
+    func test_F() {
+        let nums = [2, 2, 3, 3, 4]
+        
+        let result = SingleElementInASortedArray.singleNonDuplicate(nums)
+        
+        XCTAssertEqual(result, 4)
+    }
+}
