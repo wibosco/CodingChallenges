c89814bda40ae24f63458ad0bafa48b10bfb2ad6
William Boles <wibosco@users.noreply.github.com>
2023-08-02 16:36:05 +0100

    Implemented 'Search Suggestions System' challenge

---
+++ LeetCode/LeetCode/Challenges/SearchSuggestionsSystem.swift
@@ -0,0 +1,51 @@
+//
+//  SearchSuggestionsSystem.swift
+//  LeetCode
+//
+//  Created by William Boles on 02/08/2023.
+//
+
+import Foundation
+
+//https://leetcode.com/problems/search-suggestions-system/
+struct SearchSuggestionsSystem {
+    
+    //Time: O(n log n + m * n) where n is the numeber of elements in `products`
+    //                         where m is the number of characters in `searchWord`
+    //Space: O(n + m)
+    //array
+    //string
+    //sorting
+    //nested loops
+    //
+    //Solution Description:
+    //Suggestions are expected to be in lexicographical order so first we sort `products` to ensure that is always the case. Then
+    //we iterate through each character in `searchWord` and find matches for that character at that index in `products`, any
+    //elements that don't match we discard so each iteration of through `searchWord` potentally results in fewer `products`
+    //iterations. if more than 3 products match that iterations criteria is only add the first 3 to `suggestions`.
+    static func suggestedProducts(_ products: [String], _ searchWord: String) -> [[String]] {
+        var products = products.sorted()
+        let searchWord = Array(searchWord)
+        
+        var suggestions = [[String]]()
+        
+        for i in 0..<searchWord.count {
+            var localProducts = [String]()
+            
+            for product in products {
+                let product = Array(product)
+                
+                if i < product.count {
+                    if product[i] == searchWord[i] {
+                        localProducts.append(String(product))
+                    }
+                }
+            }
+                
+            products = localProducts //reduce products to only those that matched
+            suggestions.append(Array(localProducts.prefix(3)))
+        }
+        
+        return suggestions
+    }
+}
---
+++ LeetCode/LeetCodeTests/Tests/SearchSuggestionsSystemTests.swift
@@ -0,0 +1,54 @@
+//
+//  SearchSuggestionsSystemTests.swift
+//  LeetCodeTests
+//
+//  Created by William Boles on 02/08/2023.
+//
+
+import XCTest
+
+@testable import LeetCode
+
+final class SearchSuggestionsSystemTests: XCTestCase {
+
+    //MARK: - Tests
+    
+    func test_A() {
+        let products = ["mobile", "mouse", "moneypot", "monitor", "mousepad"]
+        let searchWord = "mouse"
+        
+        let result = SearchSuggestionsSystem.suggestedProducts(products, searchWord)
+        
+        XCTAssertEqual(result, [["mobile", "moneypot", "monitor"],
+                                ["mobile", "moneypot", "monitor"],
+                                ["mouse", "mousepad"],
+                                ["mouse", "mousepad"],
+                                ["mouse", "mousepad"]])
+    }
+    
+    func test_B() {
+        let products = ["havana"]
+        let searchWord = "havana"
+        
+        let result = SearchSuggestionsSystem.suggestedProducts(products, searchWord)
+        
+        XCTAssertEqual(result, [["havana"],
+                                ["havana"],
+                                ["havana"],
+                                ["havana"],
+                                ["havana"],
+                                ["havana"]])
+    }
+    
+    func test_C() {
+        let products = ["bags", "baggage", "banner", "box", "cloths"]
+        let searchWord = "bags"
+        
+        let result = SearchSuggestionsSystem.suggestedProducts(products, searchWord)
+        
+        XCTAssertEqual(result, [["baggage", "bags", "banner"],
+                                ["baggage", "bags", "banner"],
+                                ["baggage", "bags"],
+                                ["bags"]])
+    }
+}
