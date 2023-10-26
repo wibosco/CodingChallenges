c2a75dba2448db6f54569b9f9f46729e6882fd75
William Boles <wibosco@users.noreply.github.com>
2023-10-26 14:16:27 +0100

    Implemented 'Reorder Routes to Make All Paths Lead to the City Zero' challenge

---
+++ LeetCode/LeetCode/Challenges/ReorderRoutesToMakeAllPathsLeadToTheCityZero.swift
@@ -0,0 +1,83 @@
+//
+//  ReorderRoutesToMakeAllPathsLeadToTheCityZero.swift
+//  LeetCode
+//
+//  Created by William Boles on 25/10/2023.
+//
+
+import Foundation
+
+//https://leetcode.com/problems/reorder-routes-to-make-all-paths-lead-to-the-city-zero/
+struct ReorderRoutesToMakeAllPathsLeadToTheCityZero {
+    
+    //Time: O(n) where n is the number of nodes in the graph
+    //Space: O(n)
+    //BFS
+    //graph theory
+    //n-ary tree
+    //unweighted graph
+    //adjacency list
+    //visited
+    //queue
+    //iterative
+    //directed graph
+    //undirected graph
+    //
+    //Solution Description:
+    //We need to treat this graph as both an undirected graph and a directed graph. Undirected for the purpose of finding
+    //which nodes are connected to a given node (regardless of who is the source) - `neighbors`; directed for the purpose
+    //of determining which connection needs to reordered - `adjList`. As this graph is actually an n-ary tree we know that
+    //it won't contain any loops and that the graph won't be disconnected so don't need to handle those scenarios. First
+    //we build both `neighbors` and `adjList` and using BFS we move through `neighbors`, as we know all nodes must have
+    //path back to node `0` we seed our queue with the neighbors of node `0`. We then iterate through the queue adding
+    //each node we encountered to the `visited` set so as not to search not search again. As we know all previous
+    //iterations offer a path to node `0` if we discover that `node` is pointing away from it's predecessor (by checking
+    //the `adjList`) we know that that path would need to change and we increment `changes`. After iterating through all
+    //nodes in the graph we return `changes` as the result.
+     static func minReorder(_ n: Int, _ connections: [[Int]]) -> Int {
+        var adjList = Array(repeating: Set<Int>(), count: n)
+        var neighbors = Array(repeating: Set<Int>(), count: n)
+    
+        for connection in connections {
+            let src = connection[0]
+            let dst = connection[1]
+            
+            adjList[src].insert(dst)
+            
+            neighbors[src].insert(dst)
+            neighbors[dst].insert(src)
+        }
+        
+        var queue = [(Int, Int)]() //(from, node)
+        for neighbor in neighbors[0] {
+            queue.append((0, neighbor))
+        }
+        
+        var visited = Set<Int>()
+        visited.insert(0)
+        
+        var changes = 0
+        
+        while !queue.isEmpty {
+            var newQueue = [(Int, Int)]()
+            
+            for (from, node) in queue {
+                visited.insert(node)
+                
+                if !adjList[node].contains(from) {
+                    changes += 1
+                }
+                
+                for neighbor in neighbors[node] {
+                    if !visited.contains(neighbor) {
+                        newQueue.append((node, neighbor))
+                    }
+                }
+            }
+            
+            queue = newQueue
+        }
+        
+        return changes
+    }
+}
---
+++ LeetCode/LeetCodeTests/Tests/ReorderRoutesToMakeAllPathsLeadToTheCityZeroTests.swift
@@ -0,0 +1,42 @@
+//
+//  ReorderRoutesToMakeAllPathsLeadToTheCityZeroTests.swift
+//  LeetCodeTests
+//
+//  Created by William Boles on 25/10/2023.
+//
+
+import XCTest
+
+@testable import LeetCode
+
+final class ReorderRoutesToMakeAllPathsLeadToTheCityZeroTests: XCTestCase {
+
+    //MARK: - Tests
+    
+    func test_A() {
+        let n = 6
+        let connections = [[0, 1], [1, 3], [2, 3], [4, 0], [4, 5]]
+        
+        let result = ReorderRoutesToMakeAllPathsLeadToTheCityZero.minReorder(n, connections)
+        
+        XCTAssertEqual(result, 3)
+    }
+    
+    func test_B() {
+        let n = 5
+        let connections = [[1, 0], [1, 2], [3, 2], [3, 4]]
+        
+        let result = ReorderRoutesToMakeAllPathsLeadToTheCityZero.minReorder(n, connections)
+        
+        XCTAssertEqual(result, 2)
+    }
+    
+    func test_C() {
+        let n = 3
+        let connections = [[1, 0], [2, 0]]
+        
+        let result = ReorderRoutesToMakeAllPathsLeadToTheCityZero.minReorder(n, connections)
+        
+        XCTAssertEqual(result, 0)
+    }
+}
