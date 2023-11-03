4c19b21eb2774b3fd71de23e6438c60ed54dd3dd
William Boles <wibosco@users.noreply.github.com>
2023-11-03 11:40:23 +0000

    Implemented 'Minimum Number of Vertices to Reach All Nodes' challenge

---
+++ LeetCode/LeetCode/Challenges/MinimumNumberOfVerticesToReachAllNodes.swift
@@ -0,0 +1,47 @@
+//
+//  MinimumNumberOfVerticesToReachAllNodes.swift
+//  LeetCode
+//
+//  Created by William Boles on 03/11/2023.
+//
+
+import Foundation
+
+//https://leetcode.com/problems/minimum-number-of-vertices-to-reach-all-nodes/
+struct MinimumNumberOfVerticesToReachAllNodes {
+    
+    //Time: O(n + m) where n is the number of nodes
+    //               where m is the number of edges
+    //Space: O(n)
+    //graph theory
+    //indegree
+    //DAG
+    //
+    //Solution Description:
+    //As this is a DAG we know that the graph will not contain cycles, all nodes will be connected and that edges are one way.
+    //In order to find the minimum nodes required to full traversal this graph we need to think not about searching algorithms
+    //but rather about how travel between nodes works. In order for a node to be reachable from another node it needs to have
+    //an in-degree value that is greater than 0 which means that that node can not be part of other minimum node set. Only
+    //nodes with an in-degree of 0 can be part of our minimum node set as otherwise they would be unreachable. So first we
+    //iterate through `edges` and store the in-degree value for each destination node that we encounter in `inDegreeCount`. We
+    //then iterate through `inDegreeCount` and add any nodes with a count of 0 to `result`.
+    static func findSmallestSetOfVertices(_ n: Int, _ edges: [[Int]]) -> [Int] {
+        var inDegreeCount = Array(repeating: 0, count: n)
+        
+        for edge in edges {
+            let dst = edge[1]
+            
+            inDegreeCount[dst] += 1
+        }
+        
+        var result = [Int]()
+        
+        for (vertice, count) in inDegreeCount.enumerated() {
+            if count == 0 {
+                result.append(vertice)
+            }
+        }
+        
+        return result
+    }
+}
---
+++ LeetCode/LeetCodeTests/Tests/MinimumNumberOfVerticesToReachAllNodesTests.swift
@@ -0,0 +1,41 @@
+//
+//  MinimumNumberOfVerticesToReachAllNodesTests.swift
+//  LeetCodeTests
+//
+//  Created by William Boles on 03/11/2023.
+//
+
+import XCTest
+
+@testable import LeetCode
+
+final class MinimumNumberOfVerticesToReachAllNodesTests: XCTestCase {
+
+    //MARK: - Tests
+    
+    func test_A() {
+        let n = 6
+        let edges = [[0,1],
+                     [0,2],
+                     [2,5],
+                     [3,4],
+                     [4,2]]
+        
+        let result = MinimumNumberOfVerticesToReachAllNodes.findSmallestSetOfVertices(n, edges)
+        
+        XCTAssertEqual(result, [0, 3])
+    }
+    
+    func test_B() {
+        let n = 5
+        let edges = [[0,1],
+                     [2,1],
+                     [3,1],
+                     [1,4],
+                     [2,4]]
+        
+        let result = MinimumNumberOfVerticesToReachAllNodes.findSmallestSetOfVertices(n, edges)
+        
+        XCTAssertEqual(result, [0, 2, 3])
+    }
+}
