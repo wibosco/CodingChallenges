5d0304f2fd967a55ab617171df3edc36afb41a8c
William Boles <wibosco@users.noreply.github.com>
2023-11-03 15:38:23 +0000

    Implemented 'Detonate the Maximum Bombs' challenge

---
+++ LeetCode/LeetCode/Challenges/DetonateTheMaximumBombs.swift
@@ -0,0 +1,85 @@
+//
+//  DetonateTheMaximumBombs.swift
+//  LeetCode
+//
+//  Created by William Boles on 03/11/2023.
+//
+
+import Foundation
+
+//https://leetcode.com/problems/detonate-the-maximum-bombs/
+struct DetonateTheMaximumBombs {
+    
+    //Time: O(n^2) where n is the number of elements in `bombs`
+    //Space: O(n)
+    //graph theory
+    //DFS
+    //recursive
+    //visited
+    //set
+    //multi-source
+    //directed graph
+    //weighted graph
+    //adjacency list
+    //maths
+    //
+    //Solution Description:
+    //If we treat the bombs as nodes on a graph, we can treat the blast radius of each bomb as a potential weight edge between
+    //those nodes. Determing where the blast radius reaches isn't as simple as comparing `x` and `y` as the blast radius is a
+    //circle instead we need to use `distance = √((x1 - x2)^2 + (y1 - y2)^2)` to determine how fall apart two bombs are. So
+    //using that forumla we can build an adjacency list of graph - if a bomb falls inside the blast radius of a another bomb
+    //they are connected, this relationship needs to be check both ways as different bombs have different blast radius i.e. our
+    //graph is a directed graph. While we can think of our graph as a weighted graph once we have our edges in place we don't
+    //need to care about the weighting of the edge so can drop that detail. Once we have our adjacency list we perform a DFS
+    //from each node and track the nodes visited during that search. We then compare the nodes visited from each node and keep
+    //track of the largest which is returned once all DFSs are complete.
+    static func maximumDetonation(_ bombs: [[Int]]) -> Int {
+        var adjList = Array(repeating: Set<Int>(), count: bombs.count)
+        
+        for i in 0..<bombs.count {
+            let b1 = bombs[i]
+            
+            for j in i..<bombs.count {
+                let b2 = bombs[j]
+                
+                //working out the distance between between the two bombs using `d = √((x1 - x2)^2 + (y1 - y2)^2)`
+                let xSquared = ((Double(b1[0]) - Double(b2[0])) * (Double(b1[0]) - Double(b2[0])))
+                let ySquared = ((Double(b1[1]) - Double(b2[1])) * (Double(b1[1]) - Double(b2[1])))
+                
+                let distance = sqrt((xSquared + ySquared))
+                
+                //does the other bomb fall inside the blast radius
+                if distance <= Double(b1[2]) {
+                    adjList[i].insert(j)
+                }
+                
+                //does the other bomb fall inside the blast radius
+                if distance <= Double(b2[2]) {
+                    adjList[j].insert(i)
+                }
+            }
+        }
+        
+        var maxBombs = 0
+        for node in 0..<adjList.count {
+            var visited = Set<Int>()
+            dfs(adjList, node, &visited)
+            
+            maxBombs = max(maxBombs, visited.count)
+        }
+        
+        return maxBombs
+    }
+    
+    private static func dfs(_ adjList: [Set<Int>], _ node: Int, _ visited: inout Set<Int>) {
+        visited.insert(node)
+        
+        for neighbor in adjList[node] {
+            guard !visited.contains(neighbor) else {
+                continue
+            }
+            
+            dfs(adjList, neighbor, &visited)
+        }
+    }
+}
---
+++ LeetCode/LeetCodeTests/Tests/DetonateTheMaximumBombsTests.swift
@@ -0,0 +1,45 @@
+//
+//  DetonateTheMaximumBombsTests.swift
+//  LeetCodeTests
+//
+//  Created by William Boles on 03/11/2023.
+//
+
+import XCTest
+
+@testable import LeetCode
+
+final class DetonateTheMaximumBombsTests: XCTestCase {
+
+    //MARK: - Tests
+    
+    func test_A() {
+        let bombs = [[2,1,3],
+                     [6,1,4]]
+        
+        let result = DetonateTheMaximumBombs.maximumDetonation(bombs)
+        
+        XCTAssertEqual(result, 2)
+    }
+    
+    func test_B() {
+        let bombs = [[1,1,5],
+                     [10,10,5]]
+        
+        let result = DetonateTheMaximumBombs.maximumDetonation(bombs)
+        
+        XCTAssertEqual(result, 1)
+    }
+    
+    func test_C() {
+        let bombs = [[1,2,3],
+                     [2,3,1],
+                     [3,4,2],
+                     [4,5,3],
+                     [5,6,4]]
+        
+        let result = DetonateTheMaximumBombs.maximumDetonation(bombs)
+        
+        XCTAssertEqual(result, 5)
+    }
+}
