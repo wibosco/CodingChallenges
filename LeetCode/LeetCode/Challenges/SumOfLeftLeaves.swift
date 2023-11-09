89007d620c8801a26874c44bcfac0fd45c4e5329
William Boles <wibosco@users.noreply.github.com>
2023-11-09 15:06:36 +0000

    Implemented 'Sum of Left Leaves' challenge

---
+++ LeetCode/LeetCode/Challenges/SumOfLeftLeaves.swift
@@ -0,0 +1,40 @@
+//
+//  SumOfLeftLeaves.swift
+//  LeetCode
+//
+//  Created by William Boles on 09/11/2023.
+//
+
+import Foundation
+
+//https://leetcode.com/problems/sum-of-left-leaves/
+struct SumOfLeftLeaves {
+    
+    //Time: O(n) where n is the number of nodes in the tree
+    //Space: O(h) where h is the height of the tree
+    //binary tree
+    //DFS
+    //recursive
+    //
+    //Solution Description:
+    //Performing a DFS traversal of the tree, we attempt ot find all left leaf nodes. As `BinaryTreeNode` does not contain
+    //a parent property, during the DFS we pass if that node is a left chld of it's parent or not. Once we find a left child
+    //leaf node we return its value and add that value to other left child leaf nodes as the the callstack unwinds. Once all
+    //nodes have been checked we return the sum.
+    static func sumOfLeftLeaves(_ root: BinaryTreeNode?) -> Int {
+        return dfs(root, false)
+    }
+    
+    private static func  dfs(_ node: BinaryTreeNode?, _ leftChild: Bool) -> Int {
+        guard let node = node else {
+            return 0
+        }
+        
+        if node.left == nil && node.right == nil {
+            let val = leftChild ? node.val : 0
+            return val
+        }
+        
+        return dfs(node.left, true) + dfs(node.right, false)
+    }
+}
---
+++ LeetCode/LeetCodeTests/Tests/SumOfLeftLeavesTests.swift
@@ -0,0 +1,42 @@
+//
+//  SumOfLeftLeavesTests.swift
+//  LeetCodeTests
+//
+//  Created by William Boles on 09/11/2023.
+//
+
+import XCTest
+
+@testable import LeetCode
+
+final class SumOfLeftLeavesTests: XCTestCase {
+
+    //MARK: - Tests
+    
+    func test_A() {
+        let data = [3,9,20,nil,nil,15,7]
+        let root = BinaryTreeNode.deserialize(data)
+        
+        let result = SumOfLeftLeaves.sumOfLeftLeaves(root)
+        
+        XCTAssertEqual(result, 24)
+    }
+    
+    func test_B() {
+        let data = [1]
+        let root = BinaryTreeNode.deserialize(data)
+        
+        let result = SumOfLeftLeaves.sumOfLeftLeaves(root)
+        
+        XCTAssertEqual(result, 0)
+    }
+    
+    func test_C() {
+        let data = [1,2,3,4,5]
+        let root = BinaryTreeNode.deserialize(data)
+        
+        let result = SumOfLeftLeaves.sumOfLeftLeaves(root)
+        
+        XCTAssertEqual(result, 4)
+    }
+}
