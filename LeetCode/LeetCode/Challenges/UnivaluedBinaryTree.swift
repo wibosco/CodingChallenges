ee6f3a2032bd27d9e630f7168ad5f0cf847fe235
William Boles <wibosco@users.noreply.github.com>
2023-11-10 14:54:11 +0000

    Implemented 'Univalued Binary Tree' challenge

---
+++ LeetCode/LeetCode/Challenges/UnivaluedBinaryTree.swift
@@ -0,0 +1,41 @@
+//
+//  UnivaluedBinaryTree.swift
+//  LeetCode
+//
+//  Created by William Boles on 10/11/2023.
+//
+
+import Foundation
+
+//https://leetcode.com/problems/univalued-binary-tree/
+struct UnivaluedBinaryTree {
+    
+    //Time: O(n) where n is the number of nodes in the tree.
+    //Space: O(h) where h is the height of the tree
+    //binary tree
+    //DFS
+    //recursive
+    //
+    //Solution Description:
+    //Using DFS, we traverse through the tree comparing the nodes we encounter with the value of the root node. If the current
+    //nodes value and the root nodes value don't match we return false else we continue on the check the next nodes down.
+    static func isUnivalTree(_ root: BinaryTreeNode?) -> Bool {
+        guard let root = root else {
+            return false
+        }
+        
+        return dfs(root, root.val)
+    }
+    
+    private static func dfs(_ node: BinaryTreeNode?, _ matcher: Int) -> Bool {
+        guard let node = node else {
+            return true
+        }
+        
+        guard node.val == matcher else {
+            return false
+        }
+        
+        return dfs(node.left, matcher) && dfs(node.right, matcher)
+    }
+}
---
+++ LeetCode/LeetCodeTests/Tests/UnivaluedBinaryTreeTests.swift
@@ -0,0 +1,33 @@
+//
+//  UnivaluedBinaryTreeTests.swift
+//  LeetCodeTests
+//
+//  Created by William Boles on 10/11/2023.
+//
+
+import XCTest
+
+@testable import LeetCode
+
+final class UnivaluedBinaryTreeTests: XCTestCase {
+
+    //MARK: - Tests
+    
+    func test_A() {
+        let data = [1,1,1,1,1,nil,1]
+        let root = BinaryTreeNode.deserialize(data)
+        
+        let result = UnivaluedBinaryTree.isUnivalTree(root)
+        
+        XCTAssertTrue(result)
+    }
+    
+    func test_B() {
+        let data = [2,2,2,5,2]
+        let root = BinaryTreeNode.deserialize(data)
+        
+        let result = UnivaluedBinaryTree.isUnivalTree(root)
+        
+        XCTAssertFalse(result)
+    }
+}
