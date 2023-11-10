c2973f71c328fbd895c1af2af7c8513493cebc33
William Boles <wibosco@users.noreply.github.com>
2023-11-10 14:42:17 +0000

    Implemented 'Sum of Root To Leaf Binary Numbers' challenge

---
+++ LeetCode/LeetCode/Challenges/SumOfRootToLeafBinaryNumbers.swift
@@ -0,0 +1,53 @@
+//
+//  SumOfRootToLeafBinaryNumbers.swift
+//  LeetCode
+//
+//  Created by William Boles on 10/11/2023.
+//
+
+import Foundation
+
+//https://leetcode.com/problems/sum-of-root-to-leaf-binary-numbers/
+struct SumOfRootToLeafBinaryNumbers {
+    
+    //Time: O(n) where n is the number of nodes in the tree
+    //Space: O(n)
+    //binary tree
+    //DFS
+    //recursive
+    //bit shifting
+    //bit manipulation
+    //
+    //Solution Description:
+    //Using DFS we gradually build up the value of the nodes as we descend each level. Once we get to the leaf nodes we 
+    //return the completed value. As the call stack unwinds we sum the values of the left and right child together and pass
+    //that summed value up to the next level. We repeat this process until all we are back at the root at which point we
+    //that final summed value.
+    static func sumRootToLeaf(_ root: BinaryTreeNode?) -> Int {
+        guard let root = root else {
+            return 0
+        }
+        
+        return dfs(root, 0)
+    }
+    
+    private static func dfs(_ node: BinaryTreeNode, _ value: Int) -> Int {
+        let nextValue = value << 1 + node.val //bit shift
+        
+        if node.left == nil && node.right == nil { //leaf node
+            return nextValue
+        }
+        
+        var leftValue = 0
+        if let left = node.left {
+            leftValue = dfs(left, nextValue)
+        }
+        
+        var rightValue = 0
+        if let right = node.right {
+            rightValue = dfs(right, nextValue)
+        }
+        
+        return leftValue + rightValue
+    }
+}
---
+++ LeetCode/LeetCodeTests/Tests/SumOfRootToLeafBinaryNumbersTests.swift
@@ -0,0 +1,33 @@
+//
+//  SumOfRootToLeafBinaryNumbersTests.swift
+//  LeetCodeTests
+//
+//  Created by William Boles on 10/11/2023.
+//
+
+import XCTest
+
+@testable import LeetCode
+
+final class SumOfRootToLeafBinaryNumbersTests: XCTestCase {
+
+    //MARK: - Tests
+    
+    func test_A() {
+        let data = [1,0,1,0,1,0,1]
+        let root = BinaryTreeNode.deserialize(data)
+        
+        let result = SumOfRootToLeafBinaryNumbers.sumRootToLeaf(root)
+        
+        XCTAssertEqual(result, 22)
+    }
+    
+    func test_B() {
+        let data = [0]
+        let root = BinaryTreeNode.deserialize(data)
+        
+        let result = SumOfRootToLeafBinaryNumbers.sumRootToLeaf(root)
+        
+        XCTAssertEqual(result, 0)
+    }
+}
