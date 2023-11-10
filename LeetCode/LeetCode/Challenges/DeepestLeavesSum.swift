7e299916692723dd567658783c82b08ae9070a54
William Boles <wibosco@users.noreply.github.com>
2023-11-10 21:58:25 +0000

    Implemented 'Deepest Leaves Sum' challenge

---
+++ LeetCode/LeetCode/Challenges/DeepestLeavesSum.swift
@@ -0,0 +1,56 @@
+//
+//  DeepestLeavesSum.swift
+//  LeetCode
+//
+//  Created by William Boles on 10/11/2023.
+//
+
+import Foundation
+
+//https://leetcode.com/problems/deepest-leaves-sum/
+struct DeepestLeavesSum {
+    
+    //Time: O(n) where n is the number of nodes in the tree
+    //Space: O(m) is the maximum number of nodes at any given level
+    //binary tree
+    //BFS
+    //level traversal
+    //
+    //Solution Description:
+    //Using BFS we traverse the tree level-by-level and calculate the sum of all the nodes at that level, this level sum is then
+    //store outside the BFS loop in `lastSum`. Using `lastSum` means tjaty we don't need any special loop to know when we are on the
+    //final level as each level will override `lastSum` so when the queue is empty `lastSum` will be the last sum.
+    static func deepestLeavesSum(_ root: BinaryTreeNode?) -> Int {
+        guard let root = root else {
+            return 0
+        }
+        
+        var lastSum = 0
+        
+        var queue = [BinaryTreeNode]()
+        queue.append(root)
+        
+        while !queue.isEmpty {
+            var nextIteration = [BinaryTreeNode]()
+            
+            var sum = 0
+            
+            for node in queue {
+                sum += node.val
+                
+                if let left = node.left {
+                    nextIteration.append(left)
+                }
+                
+                if let right = node.right {
+                    nextIteration.append(right)
+                }
+            }
+            
+            lastSum = sum
+            queue = nextIteration
+        }
+        
+        return lastSum
+    }
+}
---
+++ LeetCode/LeetCodeTests/Tests/DeepestLeavesSumTests.swift
@@ -0,0 +1,33 @@
+//
+//  DeepestLeavesSumTests.swift
+//  LeetCodeTests
+//
+//  Created by William Boles on 10/11/2023.
+//
+
+import XCTest
+
+@testable import LeetCode
+
+final class DeepestLeavesSumTests: XCTestCase {
+
+    //MARK: - Tests
+    
+    func test_A() {
+        let data = [1,2,3,4,5,nil,6,7,nil,nil,nil,nil,8]
+        let root = BinaryTreeNode.deserialize(data)
+        
+        let result = DeepestLeavesSum.deepestLeavesSum(root)
+        
+        XCTAssertEqual(result, 15)
+    }
+    
+    func test_B() {
+        let data = [6,7,8,2,7,1,3,9,nil,1,4,nil,nil,nil,5]
+        let root = BinaryTreeNode.deserialize(data)
+        
+        let result = DeepestLeavesSum.deepestLeavesSum(root)
+        
+        XCTAssertEqual(result, 19)
+    }
+}
