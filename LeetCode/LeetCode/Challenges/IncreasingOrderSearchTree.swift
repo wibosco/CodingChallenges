9ba9467a1d181a26300d23d7c6845986b1f2d8f3
William Boles <wibosco@users.noreply.github.com>
2023-11-10 13:04:28 +0000

    Implemented 'Increasing Order Search Tree' challenge

---
+++ LeetCode/LeetCode/Challenges/IncreasingOrderSearchTree.swift
@@ -0,0 +1,57 @@
+//
+//  IncreasingOrderSearchTree.swift
+//  LeetCode
+//
+//  Created by William Boles on 10/11/2023.
+//
+
+import Foundation
+
+//https://leetcode.com/problems/increasing-order-search-tree/
+struct IncreasingOrderSearchTree {
+    
+    //Time: O(n) where n is the number of nodes in the tree
+    //Space: O(n)
+    //binary tree
+    //binary search tree
+    //DFS
+    //recursive
+    //inout
+    //in-order
+    //
+    //Solution Description:
+    //Using CFS we perform an in-order traversal of the tree, this will order the nodes into an ascending order array - ``order.
+    //We then iterate through `order` and associated the current node with the previous node. Once all nodes have been reordered
+    //we return the new root.
+    static func increasingBST(_ root: BinaryTreeNode?) -> BinaryTreeNode? {
+        var order = [BinaryTreeNode]()
+        inorder(root, &order)
+        
+        let newRoot = order[0]
+        newRoot.left = nil
+        newRoot.right = nil
+        
+        var parent = newRoot
+        
+        for node in order {
+            parent.right = node
+            
+            node.left = nil
+            node.right = nil
+            
+            parent = node
+        }
+        
+        return newRoot
+    }
+    
+    private static func inorder(_ node: BinaryTreeNode?, _ order: inout [BinaryTreeNode]) {
+        guard let node = node else {
+            return
+        }
+        
+        inorder(node.left, &order)
+        order.append(node)
+        inorder(node.right, &order)
+    }
+}
---
+++ LeetCode/LeetCodeTests/Tests/IncreasingOrderSearchTreeTests.swift
@@ -0,0 +1,37 @@
+//
+//  IncreasingOrderSearchTreeTests.swift
+//  LeetCodeTests
+//
+//  Created by William Boles on 10/11/2023.
+//
+
+import XCTest
+
+@testable import LeetCode
+
+final class IncreasingOrderSearchTreeTests: XCTestCase {
+
+    //MARK: - Tests
+    
+    func test_A() {
+        let data = [5,3,6,2,4,nil,8,1,nil,nil,nil,7,9]
+        let root = BinaryTreeNode.deserialize(data)
+        
+        let result = IncreasingOrderSearchTree.increasingBST(root)
+        
+        let values = BinaryTreeNode.serialize(result)
+        
+        XCTAssertEqual(values, [1,nil,2,nil,3,nil,4,nil,5,nil,6,nil,7,nil,8,nil,9])
+    }
+    
+    func test_B() {
+        let data = [5,1,7]
+        let root = BinaryTreeNode.deserialize(data)
+        
+        let result = IncreasingOrderSearchTree.increasingBST(root)
+        
+        let values = BinaryTreeNode.serialize(result)
+        
+        XCTAssertEqual(values, [1,nil,5,nil,7])
+    }
+}
