3cc6caf8349a63b7a18a1928a9a4baab8aed7748
William Boles <wibosco@users.noreply.github.com>
2023-11-13 16:48:59 +0000

    Implemented 'Binary Search Tree to Greater Sum Tree' challenge

---
+++ LeetCode/LeetCode/Challenges/BinarySearchTreeToGreaterSumTree.swift
@@ -0,0 +1,45 @@
+//
+//  BinarySearchTreeToGreaterSumTree.swift
+//  LeetCode
+//
+//  Created by William Boles on 13/11/2023.
+//
+
+import Foundation
+
+//https://leetcode.com/problems/binary-search-tree-to-greater-sum-tree/
+struct BinarySearchTreeToGreaterSumTree {
+    
+    //Time: O(n) where n is the number of nodes in the tree
+    //Space: O(h) where h is the height of the tree
+    //binary tree
+    //binary search tree
+    //DFS
+    //recursive
+    //inout
+    //
+    //Solution Description:
+    //As we have a BST we know that all nodes to the right of the `root` are greater than it in value and all nodes to the
+    //left are less than it in value. So using DFS, we can traverse the tree in such a manner so that we visit all `right`
+    //nodes then `root` then `left` nodes. By doing so we can keep a sum of all the nodes we have encountered so far which
+    //we can then use to get the `root` new GST value. As this is tree we can perform this in recursive manner as each
+    //subtree is itself a full tree. We repeat this process until all nodes have been updated, at which point we can
+    //return the `root` node.
+    static func bstToGst(_ root: BinaryTreeNode?) -> BinaryTreeNode? {
+        var total = 0
+        dfs(root, &total)
+        
+        return root
+    }
+    
+    private static func dfs(_ node: BinaryTreeNode?, _ total: inout Int) {
+        guard let node = node else {
+            return
+        }
+        
+        dfs(node.right, &total)
+        total += node.val
+        node.val = total
+        dfs(node.left, &total)
+    }
+}
---
+++ LeetCode/LeetCodeTests/Tests/BinarySearchTreeToGreaterSumTreeTests.swift
@@ -0,0 +1,35 @@
+//
+//  BinarySearchTreeToGreaterSumTreeTests.swift
+//  LeetCodeTests
+//
+//  Created by William Boles on 13/11/2023.
+//
+
+import XCTest
+
+@testable import LeetCode
+
+final class BinarySearchTreeToGreaterSumTreeTests: XCTestCase {
+
+    //MARK: - Tests
+    
+    func test_A() {
+        let data = [4,1,6,0,2,5,7,nil,nil,nil,3,nil,nil,nil,8]
+        let root = BinaryTreeNode.deserialize(data)
+        
+        let result = BinarySearchTreeToGreaterSumTree.bstToGst(root)
+        let values = BinaryTreeNode.serialize(result)
+        
+        XCTAssertEqual(values, [30,36,21,36,35,26,15,nil,nil,nil,33,nil,nil,nil,8])
+    }
+    
+    func test_B() {
+        let data = [0,nil,1]
+        let root = BinaryTreeNode.deserialize(data)
+        
+        let result = BinarySearchTreeToGreaterSumTree.bstToGst(root)
+        let values = BinaryTreeNode.serialize(result)
+        
+        XCTAssertEqual(values, [1,nil,1])
+    }
+}
