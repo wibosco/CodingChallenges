ee8ceaaeedea744b828454a7a4dad902a59e1e44
William Boles <wibosco@users.noreply.github.com>
2023-11-14 21:48:31 +0000

    Implemented 'All Elements in Two Binary Search Trees' challenge

---
+++ LeetCode/LeetCode/Challenges/AllElementsInTwoBinarySearchTrees.swift
@@ -0,0 +1,73 @@
+//
+//  AllElementsInTwoBinarySearchTrees.swift
+//  LeetCode
+//
+//  Created by William Boles on 14/11/2023.
+//
+
+import Foundation
+
+//https://leetcode.com/problems/all-elements-in-two-binary-search-trees/
+struct AllElementsInTwoBinarySearchTrees {
+    
+    //Time: O(n+m) where n is the number of nodes in the `root1` tree
+    //             where m is the number of nodes in the `root2` tree
+    //Space: O(n+m)
+    //binary tree
+    //DFS
+    //recursive
+    //sorting
+    //inout
+    //in-order
+    //two pointers
+    //
+    //Solution Description:
+    //First we perform an in-order traversal of each tree which results in two sorted arrays. We then merge the sorted arrays in
+    //ascending order by comparing and then selecting the smaller of two elements in the two array. We use two pointers to iterate
+    //through each array independently so ensuring that the smallest element of each array is being compared. Once we have
+    //exhausted one array we stop merging and instead take the remaining elements from the non-exhausted array and merge them onto
+    //the end of `mergedValues`. 
+    static func getAllElements(_ root1: TreeNode?, _ root2: TreeNode?) -> [Int] {
+        var values1 = [Int]()
+        var values2 = [Int]()
+        
+        inorder(root1, &values1)
+        inorder(root2, &values2)
+        
+        var mergedValues = [Int]()
+        
+        var p1 = 0
+        var p2 = 0
+        
+        while p1 < values1.count && p2 < values2.count {
+            let v1 = values1[p1]
+            let v2 = values2[p2]
+            
+            if v1 < v2 {
+                mergedValues.append(v1)
+                p1 += 1
+            } else {
+                mergedValues.append(v2)
+                p2 += 1
+            }
+        }
+        
+        if p1 < values1.count {
+            mergedValues.append(contentsOf: values1[p1...])
+        } else {
+            mergedValues.append(contentsOf: values2[p2...])
+        }
+        
+        return mergedValues
+    }
+    
+    private static func inorder(_ node: TreeNode?, _ values: inout [Int]) {
+        guard let node = node else {
+            return
+        }
+        
+        inorder(node.left, &values)
+        values.append(node.val)
+        inorder(node.right, &values)
+    }
+}
---
+++ LeetCode/LeetCodeTests/Tests/AllElementsInTwoBinarySearchTreesTests.swift
@@ -0,0 +1,39 @@
+//
+//  AllElementsInTwoBinarySearchTreesTests.swift
+//  LeetCodeTests
+//
+//  Created by William Boles on 14/11/2023.
+//
+
+import XCTest
+
+@testable import LeetCode
+
+final class AllElementsInTwoBinarySearchTreesTests: XCTestCase {
+
+    //MARK: - Tests
+    
+    func test_A() {
+        let data1 = [2,1,4]
+        let data2 = [1,0,3]
+            
+        let root1 = TreeNode.deserialize(data1)
+        let root2 = TreeNode.deserialize(data2)
+        
+        let result = AllElementsInTwoBinarySearchTrees.getAllElements(root1, root2)
+        
+        XCTAssertEqual(result, [0,1,1,2,3,4])
+    }
+    
+    func test_B() {
+        let data1 = [1,nil,8]
+        let data2 = [8,1]
+            
+        let root1 = TreeNode.deserialize(data1)
+        let root2 = TreeNode.deserialize(data2)
+        
+        let result = AllElementsInTwoBinarySearchTrees.getAllElements(root1, root2)
+        
+        XCTAssertEqual(result, [1,1,8,8])
+    }
+}
