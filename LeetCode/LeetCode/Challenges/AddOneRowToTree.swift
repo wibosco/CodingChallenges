96c26174cf585bc30e4d7e6744249e0dc1cee515
William Boles <wibosco@users.noreply.github.com>
2023-11-15 14:53:08 +0000

    Implemented 'Add One Row to Tree' challenge

---
+++ LeetCode/LeetCode/Challenges/AddOneRowToTree.swift
@@ -0,0 +1,73 @@
+//
+//  AddOneRowToTree.swift
+//  LeetCode
+//
+//  Created by William Boles on 15/11/2023.
+//
+
+import Foundation
+
+//https://leetcode.com/problems/add-one-row-to-tree/
+struct AddOneRowToTree {
+    
+    //Time: O(n) where n is the number of nodes in the tree
+    //Space: O(m) where m is the widest level of the tree
+    //binary tree
+    //DFS
+    //level traversal
+    //
+    //Solution Description:
+    //Using BFS we traverse the tree level-by-level until we get to the `depth - 1` level i.e. the parents of the nodes to be added,
+    //and then insert new nodes into that level by transplanting the existing left and right nodes as the child of the new nodes and
+    //then setting the left and right child of the parent node to those new nodes.
+    static func addOneRow(_ root: TreeNode?, _ val: Int, _ depth: Int) -> TreeNode? {
+        guard let root = root else {
+            return nil
+        }
+        
+        guard depth > 1 else {
+            let newRoot = TreeNode(val)
+            newRoot.left = root
+            
+            return newRoot
+        }
+        
+        var currentDepth = 2
+        var queue = [TreeNode]()
+        queue.append(root)
+        
+        while !queue.isEmpty {
+            var nextIteration = [TreeNode]()
+            
+            for node in queue {
+                if currentDepth == depth {
+                    let newLeftNode = TreeNode(val)
+                    let newRightNode = TreeNode(val)
+                    
+                    newLeftNode.left = node.left
+                    newRightNode.right = node.right
+                    
+                    node.left = newLeftNode
+                    node.right = newRightNode
+                } else {
+                    if let left = node.left {
+                        nextIteration.append(left)
+                    }
+                    
+                    if let right = node.right {
+                        nextIteration.append(right)
+                    }
+                }
+            }
+            
+            guard currentDepth < depth else {
+                break
+            }
+            
+            currentDepth += 1
+            queue = nextIteration
+        }
+        
+        return root
+    }
+}
---
+++ LeetCode/LeetCodeTests/Tests/AddOneRowToTreeTests.swift
@@ -0,0 +1,39 @@
+//
+//  AddOneRowToTreeTests.swift
+//  LeetCodeTests
+//
+//  Created by William Boles on 15/11/2023.
+//
+
+import XCTest
+
+@testable import LeetCode
+
+final class AddOneRowToTreeTests: XCTestCase {
+
+    //MARK: - Tests
+    
+    func test_A() {
+        let val = 1
+        let depth = 2
+        let data = [4,2,6,3,1,5]
+        let root = TreeNode.deserialize(data)
+        
+        let result = AddOneRowToTree.addOneRow(root, val, depth)
+        let values = TreeNode.serialize(result)
+        
+        XCTAssertEqual(values, [4,1,1,2,nil,nil,6,3,1,5])
+    }
+    
+    func test_B() {
+        let val = 1
+        let depth = 3
+        let data = [4,2,nil,3,1]
+        let root = TreeNode.deserialize(data)
+        
+        let result = AddOneRowToTree.addOneRow(root, val, depth)
+        let values = TreeNode.serialize(result)
+        
+        XCTAssertEqual(values, [4,2,nil,1,1,3,nil,nil,1])
+    }
+}
