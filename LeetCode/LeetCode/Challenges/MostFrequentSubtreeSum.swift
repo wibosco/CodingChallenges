fda5e6c4fbadacc54a9dff9f1a0238f6996c91b3
William Boles <wibosco@users.noreply.github.com>
2023-11-15 10:55:49 +0000

    Implemented 'Most Frequent Subtree Sum' challenge

---
+++ LeetCode/LeetCode/Challenges/MostFrequentSubtreeSum.swift
@@ -0,0 +1,65 @@
+//
+//  MostFrequentSubtreeSum.swift
+//  LeetCode
+//
+//  Created by William Boles on 15/11/2023.
+//
+
+import Foundation
+
+//https://leetcode.com/problems/most-frequent-subtree-sum/
+struct MostFrequentSubtreeSum {
+ 
+    //Time: O(n) where n is the number of nodes in the tree
+    //Space: O(n)
+    //binary tree
+    //DFS
+    //recursive
+    //post-order
+    //bottom-up
+    //inout
+    //dictionary
+    //
+    //Solution Description:
+    //Using a recursive bottom-up approach we first calculate the sum of the bottom-most subtrees and then combine these
+    //subtree sums to calculate the higher sums. At each stage we store the sum we have calculated in the `sums`
+    //dictionary alongside the count of how many times that sum has been produced. To simplify retrieval of the most
+    //frequent sums we keep track of which count is the most seen. Once we have fully traversed the tree and produced all
+    //sums we iterate through `sums` and add any sum that has a count of `maxCount` to `mostFrequentSums`.
+    static func findFrequentTreeSum(_ root: TreeNode?) -> [Int] {
+        var maxCount = 0
+        var sums = [Int: Int]() //[sum, count]
+        
+        _ = dfs(root, &sums, &maxCount)
+        
+        var mostFrequentSums = [Int]()
+        
+        for (sum, count) in sums {
+            guard count == maxCount else {
+                continue
+            }
+            
+            mostFrequentSums.append(sum)
+        }
+        
+        return mostFrequentSums
+    }
+    
+    private static func dfs(_ node: TreeNode?, _ sums: inout [Int: Int], _ maxCount: inout Int) -> Int {
+        guard let node = node else {
+            return 0
+        }
+        
+        let leftSum = dfs(node.left, &sums, &maxCount)
+        let rightSum = dfs(node.right, &sums, &maxCount)
+        
+        let sum = leftSum + rightSum + node.val
+        
+        let count = (sums[sum] ?? 0) + 1
+        
+        maxCount = max(maxCount, count)
+        sums[sum] = count
+        
+        return sum
+    }
+}
---
+++ LeetCode/LeetCodeTests/Tests/MostFrequentSubtreeSumTests.swift
@@ -0,0 +1,47 @@
+//
+//  MostFrequentSubtreeSumTests.swift
+//  LeetCodeTests
+//
+//  Created by William Boles on 15/11/2023.
+//
+
+import XCTest
+
+@testable import LeetCode
+
+final class MostFrequentSubtreeSumTests: XCTestCase {
+
+    //MARK: - Tests
+    
+    func test_A() {
+        let data = [5,2,-3]
+        let root = TreeNode.deserialize(data)
+        
+        let result = MostFrequentSubtreeSum.findFrequentTreeSum(root)
+        
+        let expectedResult = Set([2,-3,4])
+        
+        XCTAssertEqual(result.count, expectedResult.count)
+        
+        //order isn't important
+        for sum in result {
+            XCTAssertTrue(expectedResult.contains(sum))
+        }
+    }
+    
+    func test_B() {
+        let data = [5,2,-5]
+        let root = TreeNode.deserialize(data)
+        
+        let result = MostFrequentSubtreeSum.findFrequentTreeSum(root)
+        
+        let expectedResult = Set([2])
+        
+        XCTAssertEqual(result.count, expectedResult.count)
+        
+        //order isn't important
+        for sum in result {
+            XCTAssertTrue(expectedResult.contains(sum))
+        }
+    }
+}
