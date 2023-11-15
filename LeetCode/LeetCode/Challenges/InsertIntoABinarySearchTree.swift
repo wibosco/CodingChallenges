872b09fc2c7aaba0121ee6de5a8c8a738c71c92c
William Boles <wibosco@users.noreply.github.com>
2023-11-15 21:58:54 +0000

    Implemented 'Insert into a Binary Search Tree' challenge

---
+++ LeetCode/LeetCode/Challenges/InsertIntoABinarySearchTree.swift
@@ -0,0 +1,53 @@
+//
+//  InsertIntoABinarySearchTree.swift
+//  LeetCode
+//
+//  Created by William Boles on 15/11/2023.
+//
+
+import Foundation
+
+//https://leetcode.com/problems/insert-into-a-binary-search-tree/
+struct InsertIntoABinarySearchTree {
+    
+    //Time: O(h) where h is the height of the tree
+    //Space: O(h)
+    //binary tree
+    //binary search tree
+    //
+    //Solution Description:
+    //As a BST has an order, when inserting a new value into the tree we are able to make choices at each node as to if we
+    //should go left or right - left if `val` is less than the current node's value or right if `val` is greater than the
+    //current nodes value. As a subtree has the same properties as tree we can use a recursive approach to find where in the
+    //tree `val` should go. We recursively search the tree until we find a node without a child on the side that we want to
+    //go down, at this moment we have found the position that `val` should be inserted into and we do so.
+    static func insertIntoBST(_ root: TreeNode?, _ val: Int) -> TreeNode? {
+        guard let root = root else {
+            return TreeNode(val)
+        }
+        
+        insert(root, val)
+        
+        return root
+    }
+    
+    private static func insert(_ root: TreeNode?, _ val: Int) {
+        guard let root = root else {
+            return
+        }
+        
+        if val < root.val {
+            if let left = root.left {
+                insert(left, val)
+            } else {
+                root.left = TreeNode(val)
+            }
+        } else {
+            if let right = root.right {
+                insert(right, val)
+            } else {
+                root.right = TreeNode(val)
+            }
+        }
+    }
+}
---
+++ LeetCode/LeetCodeTests/Tests/InsertIntoABinarySearchTreeTests.swift
@@ -0,0 +1,37 @@
+//
+//  InsertIntoABinarySearchTreeTests.swift
+//  LeetCodeTests
+//
+//  Created by William Boles on 15/11/2023.
+//
+
+import XCTest
+
+@testable import LeetCode
+
+final class InsertIntoABinarySearchTreeTests: XCTestCase {
+
+    //MARK: - Tests
+    
+    func test_A() {
+        let val = 5
+        let data = [4,2,7,1,3]
+        let root = TreeNode.deserialize(data)
+        
+        let result = InsertIntoABinarySearchTree.insertIntoBST(root, val)
+        let values = TreeNode.serialize(result)
+        
+        XCTAssertEqual(values, [4,2,7,1,3,5])
+    }
+    
+    func test_B() {
+        let val = 25
+        let data = [40,20,60,10,30,50,70]
+        let root = TreeNode.deserialize(data)
+        
+        let result = InsertIntoABinarySearchTree.insertIntoBST(root, val)
+        let values = TreeNode.serialize(result)
+        
+        XCTAssertEqual(values, [40,20,60,10,30,50,70,nil,nil,25])
+    }
+}
