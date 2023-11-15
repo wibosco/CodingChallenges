a2b0e1efa2e27976cb9633f440c4046cba78f4e3
William Boles <wibosco@users.noreply.github.com>
2023-11-15 21:18:43 +0000

    Implemented 'Delete Leaves With a Given Value' challenge

---
+++ LeetCode/LeetCode/Challenges/DeleteLeavesWithAGivenValue.swift
@@ -0,0 +1,49 @@
+//
+//  DeleteLeavesWithAGivenValue.swift
+//  LeetCode
+//
+//  Created by William Boles on 15/11/2023.
+//
+
+import Foundation
+
+//https://leetcode.com/problems/delete-leaves-with-a-given-value/
+struct DeleteLeavesWithAGivenValue {
+    
+    //Time: O(n) where n is the number of nodes in the tree
+    //Space: O(h) where h is the height of the tree
+    //binary tree
+    //DFS
+    //recursive
+    //bottom-up
+    //
+    //Solution Description:
+    //Using DFS we traverse to the leaf nodes of each branch and then pass back if the parent node should delete that child. This
+    //ensures that if a parent node becomes a leaf node and it's value matches `target` it will also be deleted.
+    static func removeLeafNodes(_ root: TreeNode?, _ target: Int) -> TreeNode? {
+        if dfs(root, target) {
+            return nil
+        }
+        
+        return root
+    }
+    
+    private static func dfs(_ node: TreeNode?, _ target: Int) -> Bool {
+        guard let node = node else {
+            return true
+        }
+        
+        let canDeleteLeft = dfs(node.left, target)
+        let canDeleteRight = dfs(node.right, target)
+        
+        if canDeleteLeft {
+            node.left = nil
+        }
+        
+        if canDeleteRight {
+            node.right = nil
+        }
+        
+        return canDeleteLeft && canDeleteRight && target == node.val
+    }
+}
---
+++ LeetCode/LeetCodeTests/Tests/DeleteLeavesWithAGivenValueTests.swift
@@ -0,0 +1,59 @@
+//
+//  DeleteLeavesWithAGivenValueTests.swift
+//  LeetCodeTests
+//
+//  Created by William Boles on 15/11/2023.
+//
+
+import XCTest
+
+@testable import LeetCode
+
+final class DeleteLeavesWithAGivenValueTests: XCTestCase {
+
+    //MARK: - Tests
+    
+    func test_A() {
+        let target = 2
+        let data = [1,2,3,2,nil,2,4]
+        let root = TreeNode.deserialize(data)
+        
+        let result = DeleteLeavesWithAGivenValue.removeLeafNodes(root, target)
+        let values = TreeNode.serialize(result)
+        
+        XCTAssertEqual(values, [1,nil,3,nil,4])
+    }
+    
+    func test_B() {
+        let target = 3
+        let data = [1,3,3,3,2]
+        let root = TreeNode.deserialize(data)
+        
+        let result = DeleteLeavesWithAGivenValue.removeLeafNodes(root, target)
+        let values = TreeNode.serialize(result)
+        
+        XCTAssertEqual(values, [1,3,nil,nil,2])
+    }
+    
+    func test_C() {
+        let target = 2
+        let data = [1,2,nil,2,nil,2]
+        let root = TreeNode.deserialize(data)
+        
+        let result = DeleteLeavesWithAGivenValue.removeLeafNodes(root, target)
+        let values = TreeNode.serialize(result)
+        
+        XCTAssertEqual(values, [1])
+    }
+    
+    func test_D() {
+        let target = 2
+        let data = [2,2,2]
+        let root = TreeNode.deserialize(data)
+        
+        let result = DeleteLeavesWithAGivenValue.removeLeafNodes(root, target)
+        let values = TreeNode.serialize(result)
+        
+        XCTAssertEqual(values, [Int]())
+    }
+}
