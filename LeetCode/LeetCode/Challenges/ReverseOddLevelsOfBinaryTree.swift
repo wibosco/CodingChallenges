709ffa2a3bf7d6517858095743bb6cd6893e63fd
William Boles <wibosco@users.noreply.github.com>
2023-11-15 15:43:10 +0000

    Implemented 'Reverse Odd Levels of Binary Tree' challenge

---
+++ LeetCode/LeetCode/Challenges/ReverseOddLevelsOfBinaryTree.swift
@@ -0,0 +1,62 @@
+//
+//  ReverseOddLevelsOfBinaryTree.swift
+//  LeetCode
+//
+//  Created by William Boles on 15/11/2023.
+//
+
+import Foundation
+
+//https://leetcode.com/problems/reverse-odd-levels-of-binary-tree/
+struct ReverseOddLevelsOfBinaryTree {
+    
+    //Time: O(n) where n is the number of nodes in the tree
+    //Space: O(m) where m is the number of nodes in the widest level of the tree
+    //binary tree
+    //BFS
+    //iterative
+    //level traversal
+    //
+    //Solution Description:
+    //Using BFS we traverse the tree level-by-level. At each level we check if that level is odd, if it is we swap the current node's
+    //value with its mirrored node's value - note how we don't swap the actual nodes just their values, this saves us having to
+    //re-associate any child nodes. We repeat this process for all nodes in the tree.
+    static func reverseOddLevels(_ root: TreeNode?) -> TreeNode? {
+        guard let root = root else {
+            return nil
+        }
+        
+        var level = 0
+        var queue = [TreeNode]()
+        queue.append(root)
+        
+        while !queue.isEmpty {
+            var nextIteration = [TreeNode]()
+            
+            for i in 0..<queue.count {
+                let node = queue[i]
+                
+                if let left = node.left {
+                    nextIteration.append(left)
+                }
+                
+                if let right = node.right {
+                    nextIteration.append(right)
+                }
+                
+                if level % 2 == 1 && i < (queue.count / 2) { //odd
+                    let mirrored = queue[((queue.count - 1) - i)]
+                        
+                    let tmp = node.val //don't want to lose this value when we override to mirrored.val
+                    node.val = mirrored.val
+                    mirrored.val = tmp
+                }
+            }
+            
+            level += 1
+            queue = nextIteration
+        }
+        
+        return root
+    }
+}
---
+++ LeetCode/LeetCodeTests/Tests/ReverseOddLevelsOfBinaryTreeTests.swift
@@ -0,0 +1,45 @@
+//
+//  ReverseOddLevelsOfBinaryTreeTests.swift
+//  LeetCodeTests
+//
+//  Created by William Boles on 15/11/2023.
+//
+
+import XCTest
+
+@testable import LeetCode
+
+final class ReverseOddLevelsOfBinaryTreeTests: XCTestCase {
+
+    //MARK: - Tests
+    
+    func test_A() {
+        let data = [2,3,5,8,13,21,34]
+        let root = TreeNode.deserialize(data)
+        
+        let result = ReverseOddLevelsOfBinaryTree.reverseOddLevels(root)
+        let values = TreeNode.serialize(result)
+        
+        XCTAssertEqual(values, [2,5,3,8,13,21,34])
+    }
+    
+    func test_B() {
+        let data = [7,13,11]
+        let root = TreeNode.deserialize(data)
+        
+        let result = ReverseOddLevelsOfBinaryTree.reverseOddLevels(root)
+        let values = TreeNode.serialize(result)
+        
+        XCTAssertEqual(values, [7,11,13])
+    }
+    
+    func test_C() {
+        let data = [0,1,2,0,0,0,0,1,1,1,1,2,2,2,2]
+        let root = TreeNode.deserialize(data)
+        
+        let result = ReverseOddLevelsOfBinaryTree.reverseOddLevels(root)
+        let values = TreeNode.serialize(result)
+        
+        XCTAssertEqual(values, [0,2,1,0,0,0,0,2,2,2,2,1,1,1,1])
+    }
+}
