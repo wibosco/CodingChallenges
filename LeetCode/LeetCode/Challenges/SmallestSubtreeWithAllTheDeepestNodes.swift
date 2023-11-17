0ebafaed14172494115c5491ff20321aa9c69ab8
William Boles <wibosco@users.noreply.github.com>
2023-11-17 14:38:35 +0000

    Implemented 'Smallest Subtree with all the Deepest Nodes' challenge

--- LeetCode/LeetCode/Challenges/SmallestSubtreeWithAllTheDeepestNodes.swift
+++ LeetCode/LeetCode/Challenges/SmallestSubtreeWithAllTheDeepestNodes.swift
@@ -1,14 +1,14 @@
 //
-//  LowestCommonAncestorOfDeepestLeaves.swift
+//  SmallestSubtreeWithAllTheDeepestNodes.swift
 //  LeetCode
 //
-//  Created by William Boles on 16/11/2023.
+//  Created by William Boles on 17/11/2023.
 //
 
 import Foundation
 
-//https://leetcode.com/problems/lowest-common-ancestor-of-deepest-leaves/
-struct LowestCommonAncestorOfDeepestLeaves {
+//https://leetcode.com/problems/smallest-subtree-with-all-the-deepest-nodes/
+struct SmallestSubtreeWithAllTheDeepestNodes {
     
     //Time: O(n) where n is the number of nodes in the tree
     //Space: O(h) where h is the height of the tree
@@ -27,25 +27,25 @@ struct LowestCommonAncestorOfDeepestLeaves {
     //other depth values, this way we ensure we are finding the `lca` of the deepest leaves. We repeat this process, until all
     //branches have been compared and can return the `lca`.
     //
-    //Same as: https://leetcode.com/problems/smallest-subtree-with-all-the-deepest-nodes/
-    static func lcaDeepestLeaves(_ root: TreeNode?) -> TreeNode? {
+    //Same as: https://leetcode.com/problems/lowest-common-ancestor-of-deepest-leaves/
+    static func subtreeWithAllDeepest(_ root: TreeNode?) -> TreeNode? {
         return dfs(root, 0).0
     }
     
-    private static func dfs(_ node: TreeNode?, _ depth: Int) -> (lca: TreeNode?, maxDeepFound: Int) {
+    private static func dfs(_ node: TreeNode?, _ depth: Int) -> (TreeNode?, Int) {
         guard let node = node else {
-            return (nil, (depth - 1)) //found this branches max depth
+            return (nil, (depth - 1))
         }
         
-        let left = dfs(node.left, (depth + 1))
-        let right = dfs(node.right, (depth + 1))
-        
-        if left.maxDeepFound == right.maxDeepFound { //found new LCA of these two branches
-            return (node, left.maxDeepFound)
-        } else if left.maxDeepFound < right.maxDeepFound { //right is deeper
-            return right
-        } else { //left is deeper
-            return left
+        let (leftNode, leftDepth) = dfs(node.left, (depth + 1))
+        let (rightNode, rightDepth) = dfs(node.right, (depth + 1))
+            
+        if leftDepth == rightDepth { //same depth, found new lca
+            return (node, leftDepth)
+        } else if leftDepth > rightDepth { //left is deeper
+            return (leftNode, leftDepth)
+        } else {
+            return (rightNode, rightDepth) //right is deeper
         }
     }
 }
---
+++ LeetCode/LeetCodeTests/Tests/SmallestSubtreeWithAllTheDeepestNodesTests.swift
@@ -0,0 +1,45 @@
+//
+//  SmallestSubtreeWithAllTheDeepestNodesTests.swift
+//  LeetCodeTests
+//
+//  Created by William Boles on 17/11/2023.
+//
+
+import XCTest
+
+@testable import LeetCode
+
+final class SmallestSubtreeWithAllTheDeepestNodesTests: XCTestCase {
+
+    //MARK: - Tests
+    
+    func test_A() {
+        let data = [3,5,1,6,2,0,8,nil,nil,7,4]
+        let root = TreeNode.deserialize(data)
+        
+        let result = SmallestSubtreeWithAllTheDeepestNodes.subtreeWithAllDeepest(root)
+        let values = TreeNode.serialize(result)
+        
+        XCTAssertEqual(values, [2,7,4])
+    }
+    
+    func test_B() {
+        let data = [1]
+        let root = TreeNode.deserialize(data)
+        
+        let result = SmallestSubtreeWithAllTheDeepestNodes.subtreeWithAllDeepest(root)
+        let values = TreeNode.serialize(result)
+        
+        XCTAssertEqual(values, [1])
+    }
+    
+    func test_C() {
+        let data = [0,1,3,nil,2]
+        let root = TreeNode.deserialize(data)
+        
+        let result = SmallestSubtreeWithAllTheDeepestNodes.subtreeWithAllDeepest(root)
+        let values = TreeNode.serialize(result)
+        
+        XCTAssertEqual(values, [2])
+    }
+}
