af797d53d3aaa07cb6dbe29f07f839ccb58706e1
William Boles <wibosco@users.noreply.github.com>
2023-11-17 16:38:03 +0000

    Implemented 'Maximum Level Sum of a Binary Tree' challenge

---
+++ LeetCode/LeetCode/Challenges/MaximumLevelSumOfABinaryTree.swift
@@ -0,0 +1,63 @@
+//
+//  MaximumLevelSumOfABinaryTree.swift
+//  LeetCode
+//
+//  Created by William Boles on 17/11/2023.
+//
+
+import Foundation
+
+//https://leetcode.com/problems/maximum-level-sum-of-a-binary-tree/
+struct MaximumLevelSumOfABinaryTree {
+    
+    //Time: O(n) where n is the number of nodes in the tree
+    //Space: O(m) where m is the number of nodes in the widest level
+    //binary tree
+    //BFS
+    //iterative
+    //level traversal
+    //
+    //Solution Description:
+    //Using BFS we perform a level-by-level traversal of the tree. At each level we calculate the sum of all nodes in that level
+    //and then compare that local sum with the overall `maxSum`. If the local sum is larger then we replace `maxSum` with that
+    //local sum and update `levelWithMaxSum` to be the current level. We repeat this for all levels in the tree.
+    static func maxLevelSum(_ root: TreeNode?) -> Int {
+        guard let root = root else {
+            return -1
+        }
+        
+        var queue = [TreeNode]()
+        queue.append(root)
+        
+        var maxSum = Int.min
+        var levelWithMaxSum = 1
+        var level = 1
+        
+        while !queue.isEmpty {
+            var nextIteration = [TreeNode]()
+            var sum = 0
+            
+            for node in queue {
+                sum += node.val
+                
+                if let left = node.left {
+                    nextIteration.append(left)
+                }
+                
+                if let right = node.right {
+                    nextIteration.append(right)
+                }
+            }
+            
+            if maxSum < sum { //sum needs to be greater, equal isn't enough to update the level
+                levelWithMaxSum = level
+                maxSum = sum
+            }
+            
+            level += 1
+            queue = nextIteration
+        }
+        
+        return levelWithMaxSum
+    }
+}
---
+++ LeetCode/LeetCodeTests/Tests/MaximumLevelSumOfABinaryTreeTests.swift
@@ -0,0 +1,33 @@
+//
+//  MaximumLevelSumOfABinaryTreeTests.swift
+//  LeetCodeTests
+//
+//  Created by William Boles on 17/11/2023.
+//
+
+import XCTest
+
+@testable import LeetCode
+
+final class MaximumLevelSumOfABinaryTreeTests: XCTestCase {
+
+    //MARK: - Tests
+    
+    func test_A() {
+        let data = [1,7,0,7,-8,nil,nil]
+        let root = TreeNode.deserialize(data)
+        
+        let result = MaximumLevelSumOfABinaryTree.maxLevelSum(root)
+        
+        XCTAssertEqual(result, 2)
+    }
+    
+    func test_B() {
+        let data = [989,nil,10250,98693,-89388,nil,nil,nil,-32127]
+        let root = TreeNode.deserialize(data)
+        
+        let result = MaximumLevelSumOfABinaryTree.maxLevelSum(root)
+        
+        XCTAssertEqual(result, 2)
+    }
+}
