309836ac1287d90f97b054df24337d5a319ba412
William Boles <wibosco@users.noreply.github.com>
2023-11-18 22:35:57 +0000

    Implemented 'Flip Binary Tree To Match Preorder Traversal' challenge

---
+++ LeetCode/LeetCode/Challenges/FlipBinaryTreeToMatchPreorderTraversal.swift
@@ -0,0 +1,64 @@
+//
+//  FlipBinaryTreeToMatchPreorderTraversal.swift
+//  LeetCode
+//
+//  Created by William Boles on 18/11/2023.
+//
+
+import Foundation
+
+//https://leetcode.com/problems/flip-binary-tree-to-match-preorder-traversal/
+struct FlipBinaryTreeToMatchPreorderTraversal {
+    
+    //Time: O(n) where n is the number of nodes in the tree
+    //Space: O(n)
+    //binary tree
+    //DFS
+    //recursive
+    //top-down
+    //inout
+    //
+    //Solution Description:
+    //Using DFS we check the tree to determine if a pre-order traversal matches `voyage` - pre-order being current->left->right.
+    //If we encounter a mismatch between the tree and `voyage` we record the mismatch in `changes` and flip the node the sibling
+    //nodes, before continuing the check with the new structure. We prevent unlimited flips of the same level by nesting the
+    //flipped call - this means we don't need to pass a "canFlip" parameter into `dfs`. We repeat this process until we either
+    //get another mismatch after having already flipped or we visit all nodes in the tree.
+    static func flipMatchVoyage(_ root: TreeNode?, _ voyage: [Int]) -> [Int] {
+        guard let root = root else {
+            return [-1]
+        }
+        
+        var changes = [Int]()
+        var index = 0
+        
+        if !dfs(root, voyage, &index, &changes) {
+            return [-1]
+        }
+        
+        return changes
+    }
+    
+    private static func dfs(_ node: TreeNode?, _ voyage: [Int], _ index: inout Int, _ changes: inout [Int]) -> Bool {
+        guard let node = node else {
+            return true
+        }
+        
+        guard node.val == voyage[index] else {
+            return false
+        }
+    
+        index += 1
+
+        //in pre-order traversal we fully search the left nodes before the right nodes
+        if !(dfs(node.left, voyage, &index, &changes) && dfs(node.right, voyage, &index, &changes)) {
+            //mismatch
+            changes.append(node.val)
+            
+            //no need to actually flip the nodes instead we search right node first which has the same affect
+            return dfs(node.right, voyage, &index, &changes) && dfs(node.left, voyage, &index, &changes)
+        }
+        
+        return true
+    }
+}
---
+++ LeetCode/LeetCodeTests/Tests/FlipBinaryTreeToMatchPreorderTraversalTests.swift
@@ -0,0 +1,65 @@
+//
+//  FlipBinaryTreeToMatchPreorderTraversalTests.swift
+//  LeetCodeTests
+//
+//  Created by William Boles on 18/11/2023.
+//
+
+import XCTest
+
+@testable import LeetCode
+
+final class FlipBinaryTreeToMatchPreorderTraversalTests: XCTestCase {
+
+    //MARK: - Tests
+    
+    func test_A() {
+        let data = [1,2]
+        let root = TreeNode.deserialize(data)
+        let voyage = [2,1]
+        
+        let result = FlipBinaryTreeToMatchPreorderTraversal.flipMatchVoyage(root, voyage)
+        
+        XCTAssertEqual(result, [-1])
+    }
+    
+    func test_B() {
+        let data = [1,2,3]
+        let root = TreeNode.deserialize(data)
+        let voyage = [1,3,2]
+        
+        let result = FlipBinaryTreeToMatchPreorderTraversal.flipMatchVoyage(root, voyage)
+        
+        XCTAssertEqual(result, [1])
+    }
+    
+    func test_C() {
+        let data = [1,2,3]
+        let root = TreeNode.deserialize(data)
+        let voyage = [1,2,3]
+        
+        let result = FlipBinaryTreeToMatchPreorderTraversal.flipMatchVoyage(root, voyage)
+        
+        XCTAssertEqual(result, [])
+    }
+    
+    func test_D() {
+        let data = [2,1,4,5,nil,3]
+        let root = TreeNode.deserialize(data)
+        let voyage = [2,4,3,1,5]
+        
+        let result = FlipBinaryTreeToMatchPreorderTraversal.flipMatchVoyage(root, voyage)
+        
+        XCTAssertEqual(result, [2])
+    }
+    
+    func test_E() {
+        let data = [1,2,nil,3]
+        let root = TreeNode.deserialize(data)
+        let voyage = [1,3,2]
+        
+        let result = FlipBinaryTreeToMatchPreorderTraversal.flipMatchVoyage(root, voyage)
+        
+        XCTAssertEqual(result, [-1])
+    }
+}
