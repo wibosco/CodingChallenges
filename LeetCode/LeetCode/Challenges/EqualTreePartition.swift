72e26fcb2a7dbe90f3dc95cbaa3848eb9c4bc007
William Boles <wibosco@users.noreply.github.com>
2023-11-22 11:01:05 +0000

    Implemented 'Equal Tree Partition' challenge

---
+++ LeetCode/LeetCode/Challenges/EqualTreePartition.swift
@@ -0,0 +1,59 @@
+//
+//  EqualTreePartition.swift
+//  LeetCode
+//
+//  Created by William Boles on 22/11/2023.
+//
+
+import Foundation
+
+//https://leetcode.com/problems/equal-tree-partition/
+struct EqualTreePartition {
+    
+    //Time: O(n) where n is the number of nodes in the tree
+    //Space: O(h) where he is the height of the tree
+    //binary tree
+    //DFS
+    //recursive
+    //bottom-up
+    //top-down
+    //
+    //Solution Description:
+    //Using bottom-up DFS we produce a sum tree where every node holds the sum of it's subtree. We then perform a top-down DFS to
+    //determine if removing a edge will result in the two trees having the same value. We recusively attempt this with all nodes
+    //in the tree until we find a spliting that results in equal trees. 
+    static func checkEqualTree(_ root: TreeNode?) -> Bool {
+        guard let root = root else {
+            return false
+        }
+        
+        let total = buildSumTree(root)
+        
+        return canEqualise(root.left, total) || canEqualise(root.right, total)
+    }
+    
+    private static func buildSumTree(_ node: TreeNode?) -> Int {
+        guard let node = node else {
+            return 0
+        }
+        
+        let leftValue = buildSumTree(node.left)
+        let rightValue = buildSumTree(node.right)
+        
+        node.val = leftValue + rightValue + node.val
+            
+        return node.val
+    }
+    
+    private static func canEqualise(_ node: TreeNode?, _ total: Int) -> Bool {
+        guard let node = node else {
+            return false
+        }
+        
+        if total - node.val == node.val { //would removing this edge make the trees equal?
+            return true
+        }
+        
+        return canEqualise(node.left, total) || canEqualise(node.right, total)
+    }
+}
---
+++ LeetCode/LeetCodeTests/Tests/EqualTreePartitionTests.swift
@@ -0,0 +1,60 @@
+//
+//  EqualTreePartitionTests.swift
+//  LeetCodeTests
+//
+//  Created by William Boles on 22/11/2023.
+//
+
+import XCTest
+
+@testable import LeetCode
+
+final class EqualTreePartitionTests: XCTestCase {
+
+    //MARK: - Tests
+    
+    func test_A() {
+        let data = [5,10,10,nil,nil,2,3]
+        let root = TreeNode.deserialize(data)
+        
+        let result = EqualTreePartition.checkEqualTree(root)
+        
+        XCTAssertTrue(result)
+    }
+    
+    func test_B() {
+        let data = [1,2,10,nil,nil,2,20]
+        let root = TreeNode.deserialize(data)
+        
+        let result = EqualTreePartition.checkEqualTree(root)
+        
+        XCTAssertFalse(result)
+    }
+    
+    func test_C() {
+        let data = [0,-1,1]
+        let root = TreeNode.deserialize(data)
+        
+        let result = EqualTreePartition.checkEqualTree(root)
+        
+        XCTAssertFalse(result)
+    }
+    
+    func test_D() {
+        let data = [2,nil,3,nil,4,nil,5,nil,6]
+        let root = TreeNode.deserialize(data)
+        
+        let result = EqualTreePartition.checkEqualTree(root)
+        
+        XCTAssertFalse(result)
+    }
+    
+    func test_E() {
+        let data = [2,1,3,0,2,nil,nil,nil,nil,2,nil,1,nil,nil,1]
+        let root = TreeNode.deserialize(data)
+        
+        let result = EqualTreePartition.checkEqualTree(root)
+        
+        XCTAssertTrue(result)
+    }
+}
