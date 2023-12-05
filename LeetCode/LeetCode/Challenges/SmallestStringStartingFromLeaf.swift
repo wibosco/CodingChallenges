4ec44920d1058d9934e98f46aa1e1ce4789ee8a3
William Boles <wibosco@users.noreply.github.com>
2023-12-05 16:40:15 +0000

    Implemented 'Smallest String Starting From Leaf' challenge

---
+++ LeetCode/LeetCode/Challenges/SmallestStringStartingFromLeaf.swift
@@ -0,0 +1,76 @@
+//
+//  SmallestStringStartingFromLeaf.swift
+//  LeetCode
+//
+//  Created by William Boles on 05/12/2023.
+//
+
+import Foundation
+
+//https://leetcode.com/problems/smallest-string-starting-from-leaf/
+struct SmallestStringStartingFromLeaf {
+    
+    //Time: O(n^2) where n is the numbser of nodes in the tree.
+    //Space: O(n)
+    //binary tree
+    //string
+    //sorting
+    //DFS
+    //recursive
+    //two pointers
+    //top-down
+    //bottom-up
+    //
+    //Solution Description:
+    //Using DFS we build the path from the the root to each leaf node then as the callstack unwinds we compare the paths 
+    //returned from left and right child nodes and take the lexicographically smaller path and return that path as the
+    //smallest on this branch. We repeat this process for each branch in the tree until we return to the root - we then
+    //convert the smallest array into a string and return that string.
+    static func smallestFromLeaf(_ root: TreeNode?) -> String {
+        let smallestPath = dfs(root, [])
+        
+        let startingValue = Int(UnicodeScalar("a").value)
+        let smallest = smallestPath.reduce("") { $0 + String(Unicode.Scalar($1 + startingValue)!) }
+                
+        return smallest
+    }
+    
+    private static func dfs(_ node: TreeNode?, _ path: [Int]) -> [Int] {
+        guard let node = node else {
+            return []
+        }
+        
+        var path = path
+        path.append(node.val)
+        
+        if node.left == nil && node.right == nil { //leaf node?
+            return Array(path.reversed())
+        }
+        
+        let left = dfs(node.left, path)
+        let right = dfs(node.right, path)
+        
+        guard left.count > 0, right.count > 0 else {
+            return left.count == 0 ? right : left
+        }
+        
+        return smallest(left, right)
+    }
+    
+    private static func smallest(_ a: [Int], _ b: [Int]) -> [Int] {
+        var p1 = 0
+                
+        while p1 < a.count && p1 < b.count {
+            if a[p1] == b[p1] {
+                p1 += 1
+            } else if a[p1] < b[p1] {
+                return a
+            } else {
+                return b
+            }
+        }
+        
+        //can only get there is `a` and `b` have the same prefix so return the smaller of the two
+        return a.count > b.count ? b : a
+    }
+}
---
+++ LeetCode/LeetCodeTests/Tests/SmallestStringStartingFromLeafTests.swift
@@ -0,0 +1,61 @@
+//
+//  SmallestStringStartingFromLeafTests.swift
+//  LeetCodeTests
+//
+//  Created by William Boles on 05/12/2023.
+//
+
+import XCTest
+
+@testable import LeetCode
+
+final class SmallestStringStartingFromLeafTests: XCTestCase {
+
+    //MARK: - Tests
+    
+    func test_A() {
+        let data = [0,1,2,3,4,3,4]
+        let root = TreeNode.deserialize(data)
+        
+        let result = SmallestStringStartingFromLeaf.smallestFromLeaf(root)
+        
+        XCTAssertEqual(result, "dba")
+    }
+    
+    func test_B() {
+        let data = [25,1,3,1,3,0,2]
+        let root = TreeNode.deserialize(data)
+        
+        let result = SmallestStringStartingFromLeaf.smallestFromLeaf(root)
+        
+        XCTAssertEqual(result, "adz")
+    }
+    
+    func test_C() {
+        let data = [2,2,1,nil,1,0,nil,0]
+        let root = TreeNode.deserialize(data)
+        
+        let result = SmallestStringStartingFromLeaf.smallestFromLeaf(root)
+        
+        XCTAssertEqual(result, "abc")
+    }
+    
+    func test_D() {
+        let data = [0,nil,1]
+        let root = TreeNode.deserialize(data)
+        
+        let result = SmallestStringStartingFromLeaf.smallestFromLeaf(root)
+        
+        XCTAssertEqual(result, "ba")
+    }
+    
+    
+    func test_E() {
+        let data = [25,1,nil,0,0,1,nil,nil,nil,0]
+        let root = TreeNode.deserialize(data)
+        
+        let result = SmallestStringStartingFromLeaf.smallestFromLeaf(root)
+        
+        XCTAssertEqual(result, "ababz")
+    }
+}
