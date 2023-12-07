a69e26c028002257ea16f85a135fd64ccd75f505
William Boles <wibosco@users.noreply.github.com>
2023-12-07 16:00:55 +0000

    Implemented 'Maximum Average Subtree' challenge

---
+++ LeetCode/LeetCode/Challenges/MaximumAverageSubtree.swift
@@ -0,0 +1,50 @@
+//
+//  MaximumAverageSubtree.swift
+//  LeetCode
+//
+//  Created by William Boles on 07/12/2023.
+//
+
+import Foundation
+
+//https://leetcode.com/problems/maximum-average-subtree/
+struct MaximumAverageSubtree {
+    
+    //Time: O(n) where n is the number of nodes in the tree
+    //Space: O(h) where h is the height of the tree
+    //binary tree
+    //DFS
+    //recursive
+    //bottom-up
+    //
+    //Solution Description:
+    //Using DFS we traverse to the left nodes in the tree and as the recursive stack unwinds we calculate the sum and average
+    //for that node. At each level we take the left and right child sums and counts and combine them with the current node to
+    //calculate that subtrees average - this average is then compared against `maxAverage` and `maxAverage` is updated if
+    //necessary. When then pass the `count` and `sum` up the next level to repeat this process. Once all averages have been
+    //calculcate we return `maxAverage`.
+    static func maximumAverageSubtree(_ root: TreeNode?) -> Double {
+        var maxAverage = 0.0
+        
+        _ = dfs(root, &maxAverage)
+        
+        return maxAverage
+    }
+    
+    private static func dfs(_ node: TreeNode?, _ maxAverage: inout Double) -> (Int, Int) { //count, sum
+        guard let node = node else {
+            return (0, 0) //basecase
+        }
+        
+        let (leftCount, leftSum) = dfs(node.left, &maxAverage)
+        let (rightCount, rightSum) = dfs(node.right, &maxAverage)
+        
+        let sum = node.val + leftSum + rightSum
+        let count = leftCount + rightCount + 1 //+1 for current node
+        
+        let average: Double = Double(sum) / Double(count)
+        maxAverage = max(maxAverage, average)
+        
+        return (count, sum)
+    }
+}
---
+++ LeetCode/LeetCodeTests/Tests/MaximumAverageSubtreeTests.swift
@@ -0,0 +1,33 @@
+//
+//  MaximumAverageSubtreeTests.swift
+//  LeetCodeTests
+//
+//  Created by William Boles on 07/12/2023.
+//
+
+import XCTest
+
+@testable import LeetCode
+
+final class MaximumAverageSubtreeTests: XCTestCase {
+
+    //MARK: - Tests
+    
+    func test_A() {
+        let data = [5,6,1]
+        let root = TreeNode.deserialize(data)
+        
+        let result = MaximumAverageSubtree.maximumAverageSubtree(root)
+        
+        XCTAssertEqual(result, 6.0, accuracy: 0.00001)
+    }
+    
+    func test_B() {
+        let data = [0,nil,1]
+        let root = TreeNode.deserialize(data)
+        
+        let result = MaximumAverageSubtree.maximumAverageSubtree(root)
+        
+        XCTAssertEqual(result, 1.0, accuracy: 0.00001)
+    }
+}
