5d2f8385d562d6af8dabd4039b4d7f613b5ee193
William Boles <wibosco@users.noreply.github.com>
2023-12-07 15:14:34 +0000

    Implemented 'Pseudo-Palindromic Paths in a Binary Tree' challenge

---
+++ LeetCode/LeetCode/Challenges/PseudoPalindromicPathsInABinaryTree.swift
@@ -0,0 +1,64 @@
+//
+//  PseudoPalindromicPathsInABinaryTree.swift
+//  LeetCode
+//
+//  Created by William Boles on 07/12/2023.
+//
+
+import Foundation
+
+//https://leetcode.com/problems/pseudo-palindromic-paths-in-a-binary-tree/
+struct PseudoPalindromicPathsInABinaryTree {
+    
+    //Time: O(n) where n is the number of nodes in the tree
+    //Space: O(n)
+    //binary tree
+    //DFS
+    //recursive
+    //dictionary
+    //inout
+    //top-down
+    //backtracking
+    //
+    //Solution Description:
+    //A palindromic can have 0 or at most 1 odd frequency occurance of a value, any more and it's not possible to form a 
+    //palindromic. Using this information we traverse the tree in a DFS manner and add each value we encounter into the `freq`
+    //dictionary. Once we reach a leaf node we check the `freq` for odd counts - if we end up with an `offCount` of 0 or 1
+    //then this path can be reconstructed into a palindromic and we increment `count`, otherwise we skip over. We repeat this
+    //for each path in the tree. Once all paths have been checked we return `count`.
+    static func pseudoPalindromicPaths (_ root: TreeNode?) -> Int {
+        var count = 0
+        var freq = [Int: Int]()
+        
+        dfs(root, &freq, &count)
+        
+        return count
+    }
+    
+    private static func dfs(_ node: TreeNode?, _ freq: inout [Int: Int], _ count: inout Int) {
+        guard let node = node else {
+            return
+        }
+
+        freq[node.val, default: 0] += 1
+        
+        //leaf node
+        if node.left == nil && node.right == nil {
+            var oddCount = 0
+            for value in freq.values {
+                if value % 2 != 0 {
+                    oddCount += 1
+                }
+            }
+            
+            if oddCount == 0 || oddCount == 1 {
+                count += 1
+            }
+        }
+        
+        dfs(node.left, &freq, &count)
+        dfs(node.right, &freq, &count)
+        
+        freq[node.val, default: 0] -= 1 //backtracking
+    }
+}
---
+++ LeetCode/LeetCodeTests/Tests/PseudoPalindromicPathsInABinaryTreeTests.swift
@@ -0,0 +1,42 @@
+//
+//  PseudoPalindromicPathsInABinaryTreeTests.swift
+//  LeetCodeTests
+//
+//  Created by William Boles on 07/12/2023.
+//
+
+import XCTest
+
+@testable import LeetCode
+
+final class PseudoPalindromicPathsInABinaryTreeTests: XCTestCase {
+
+    //MARK: - Tests
+    
+    func test_A() {
+        let data = [2,3,1,3,1,nil,1]
+        let root = TreeNode.deserialize(data)
+        
+        let result = PseudoPalindromicPathsInABinaryTree.pseudoPalindromicPaths(root)
+        
+        XCTAssertEqual(result, 2)
+    }
+    
+    func test_B() {
+        let data = [2,1,1,1,3,nil,nil,nil,nil,nil,1]
+        let root = TreeNode.deserialize(data)
+        
+        let result = PseudoPalindromicPathsInABinaryTree.pseudoPalindromicPaths(root)
+        
+        XCTAssertEqual(result, 1)
+    }
+    
+    func test_C() {
+        let data = [9]
+        let root = TreeNode.deserialize(data)
+        
+        let result = PseudoPalindromicPathsInABinaryTree.pseudoPalindromicPaths(root)
+        
+        XCTAssertEqual(result, 1)
+    }
+}
