dda2269c6630daef492a74fe0513ad5e92fe7215
William Boles <wibosco@users.noreply.github.com>
2023-12-13 12:43:37 +0000

    Implemented 'Two Sum BSTs' challenge

---
+++ LeetCode/LeetCode/Challenges/TwoSumBSTs.swift
@@ -0,0 +1,58 @@
+//
+//  TwoSumBSTs.swift
+//  LeetCode
+//
+//  Created by William Boles on 10/12/2023.
+//
+
+import Foundation
+
+//https://leetcode.com/problems/two-sum-bsts/
+struct TwoSumBSTs {
+    
+    //Time: O(n+m) where n is the number of nodes in `root1`
+    //             where m is the number of nodes in `root2`
+    //Space: O(n+m)
+    //binary tree
+    //DFS
+    //recursive
+    //set
+    //
+    //Solution Description:
+    //Using DFS we first convert the tree `root1` into a set - we use a set as we don't care how many times a value appears in
+    //the tree only that it does appear. Then using DFS again we search through the tree `root2` and attempt to find if the delta
+    //of `target` and the current node value is in `root1Values` - if it is present then we return true else we continue the left
+    //and right branches of the tree. 
+    //
+    //Similar to: https://leetcode.com/problems/two-sum/
+    static func twoSumBSTs(_ root1: TreeNode?, _ root2: TreeNode?, _ target: Int) -> Bool {
+        var root1Values = Set<Int>()
+        convertToSet(root1, &root1Values)
+        
+        return findTarget(root2, root1Values, target)
+    }
+    
+    private static func convertToSet(_ node: TreeNode?, _ values: inout Set<Int>) {
+        guard let node = node else {
+            return
+        }
+        
+        values.insert(node.val)
+        
+        convertToSet(node.left, &values)
+        convertToSet(node.right, &values)
+    }
+    
+    private static func findTarget(_ node: TreeNode?, _ values: Set<Int>, _ target: Int) -> Bool {
+        guard let node = node else {
+            return false
+        }
+        
+        let delta = target - node.val
+        if values.contains(delta) {
+            return true
+        }
+        
+        return findTarget(node.left, values, target) || findTarget(node.right, values, target)
+    }
+}
---
+++ LeetCode/LeetCodeTests/Tests/TwoSumBSTsTests.swift
@@ -0,0 +1,39 @@
+//
+//  ATwoSumBSTsTests.swift
+//  LeetCodeTests
+//
+//  Created by William Boles on 10/12/2023.
+//
+
+import XCTest
+
+@testable import LeetCode
+
+final class TwoSumBSTsTests: XCTestCase {
+
+    //MARK: - Tests
+    
+    func test_A() {
+        let target = 5
+        let data1 = [2,1,4]
+        let root1 = TreeNode.deserialize(data1)
+        let data2 = [1,0,3]
+        let root2 = TreeNode.deserialize(data2)
+        
+        let result = TwoSumBSTs.twoSumBSTs(root1, root2, target)
+        
+        XCTAssertTrue(result)
+    }
+    
+    func test_B() {
+        let target = 18
+        let data1 = [0,-10,10]
+        let root1 = TreeNode.deserialize(data1)
+        let data2 = [5,1,7,0,2]
+        let root2 = TreeNode.deserialize(data2)
+        
+        let result = TwoSumBSTs.twoSumBSTs(root1, root2, target)
+        
+        XCTAssertFalse(result)
+    }
+}
