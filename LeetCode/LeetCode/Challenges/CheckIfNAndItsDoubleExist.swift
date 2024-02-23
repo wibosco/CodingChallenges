c1c7c8fac04cd7c12a6f36d16c376b0fc25668f5
William Boles <wibosco@users.noreply.github.com>
2024-02-23 22:03:22 +0000

    Implemented 'Check If N and Its Double Exist' challenge

---
+++ LeetCode/LeetCode/Challenges/CheckIfNAndItsDoubleExist.swift
@@ -0,0 +1,43 @@
+//
+//  CheckIfNAndItsDoubleExist.swift
+//  LeetCode
+//
+//  Created by William Boles on 23/02/2024.
+//
+
+import Foundation
+
+//https://leetcode.com/problems/check-if-n-and-its-double-exist/
+struct CheckIfNAndItsDoubleExist {
+    
+    //Time: O(n) where n is the number of elements in `arr`
+    //Space: O(n)
+    //array
+    //set
+    //
+    //Solution Description:
+    //To avoid looping through `arr` multiple time we can use the set `seen` to store the values we have already seen
+    //as we perform our one pass through `arr`. In that one pass we search for both double `value` and half `value`
+    //(where the current value is an even number). If either of those values exist in `seen` we return true else we add
+    //`value` to seen and move onto the next iteration of `arr`. If having iterated through `arr` without returning we
+    //return false as a double doesn't exist.
+    func checkIfExist(_ arr: [Int]) -> Bool {
+        var seen = Set<Int>()
+        
+        for (index, value) in arr.enumerated() {
+            //have we seen double this value already
+            if seen.contains((value * 2)) {
+                return true
+            } else if value % 2 == 0 {
+                //have we seen half of this value already i.e. this value is the double
+                if seen.contains((value / 2)) {
+                    return true
+                }
+            }
+
+            seen.insert(value)
+        }
+        
+        return false
+    }
+}
---
+++ LeetCode/LeetCodeTests/Tests/CheckIfNAndItsDoubleExistTests.swift
@@ -0,0 +1,31 @@
+//
+//  AACheckIfNAndItsDoubleExistTests.swift
+//  LeetCodeTests
+//
+//  Created by William Boles on 23/02/2024.
+//
+
+import XCTest
+
+@testable import LeetCode
+
+final class CheckIfNAndItsDoubleExistTests: XCTestCase {
+
+    //MARK: - Tests
+    
+    func test_A() {
+        let arr = [10,2,5,3]
+        
+        let result = CheckIfNAndItsDoubleExist().checkIfExist(arr)
+        
+        XCTAssertTrue(result)
+    }
+    
+    func test_B() {
+        let arr = [3,1,7,11]
+        
+        let result = CheckIfNAndItsDoubleExist().checkIfExist(arr)
+        
+        XCTAssertFalse(result)
+    }
+}
