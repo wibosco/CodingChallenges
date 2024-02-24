1fd5b7dbd86a1e647910dff83aea370a0209ec01
William Boles <wibosco@users.noreply.github.com>
2024-02-24 22:05:07 +0000

    Implemented 'Count Negative Numbers in a Sorted Matrix' challenge

---
+++ LeetCode/LeetCode/Challenges/CountNegativeNumbersInASortedMatrix.swift
@@ -0,0 +1,52 @@
+//
+//  CountNegativeNumbersInASortedMatrix.swift
+//  LeetCode
+//
+//  Created by William Boles on 24/02/2024.
+//
+
+import Foundation
+
+//https://leetcode.com/problems/count-negative-numbers-in-a-sorted-matrix/
+struct CountNegativeNumbersInASortedMatrix {
+    
+    //Time: O(n log m) where n is the number of rows in `grid`
+    //                 where m is the number of columns in `grid`
+    //Space: O(1)
+    //array
+    //matrix
+    //binary search
+    //
+    //Solution Description:
+    //As we know the rows are sorted, we can use binary search to find the index whihch is start of the elements that are less
+    //than 0. With this index we can then calculate how many elements in that row are less than 0 and update `count` with that
+    //value.
+    func countNegatives(_ grid: [[Int]]) -> Int {
+        var count = 0
+        
+        for row in grid {
+            let index = binarySearch(row, 0)
+            
+            count += row.count - index
+        }
+        
+        return count
+    }
+    
+    private func binarySearch(_ values: [Int], _ target: Int) -> Int {
+        var left = 0
+        var right = values.count - 1
+        
+        while left <= right {
+            let mid = left + (right - left) / 2 //to avoid overflow
+            
+            if values[mid] < target {
+                right = mid - 1
+            } else {
+                left = mid + 1
+            }
+        }
+        
+        return left
+    }
+}
---
+++ LeetCode/LeetCodeTests/Tests/CountNegativeNumbersInASortedMatrixTests.swift
@@ -0,0 +1,31 @@
+//
+//  CountNegativeNumbersInASortedMatrixTests.swift
+//  LeetCodeTests
+//
+//  Created by William Boles on 24/02/2024.
+//
+
+import XCTest
+
+@testable import LeetCode
+
+final class CountNegativeNumbersInASortedMatrixTests: XCTestCase {
+
+    //MARK: - Tests
+    
+    func test_A() {
+        let grid = [[4,3,2,-1],[3,2,1,-1],[1,1,-1,-2],[-1,-1,-2,-3]]
+        
+        let result = CountNegativeNumbersInASortedMatrix().countNegatives(grid)
+        
+        XCTAssertEqual(result, 8)
+    }
+    
+    func test_B() {
+        let grid = [[3,2],[1,0]]
+        
+        let result = CountNegativeNumbersInASortedMatrix().countNegatives(grid)
+        
+        XCTAssertEqual(result, 0)
+    }
+}
