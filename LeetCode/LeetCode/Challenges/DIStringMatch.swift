8b46b80603d4d3f97e558d21282680b9a9012306
William Boles <wibosco@users.noreply.github.com>
2024-06-05 14:09:31 +0100

    Implemented 'DI String Match' challenge

---
+++ LeetCode/LeetCode/Challenges/DIStringMatch.swift
@@ -0,0 +1,46 @@
+//
+//  DIStringMatch.swift
+//  LeetCode
+//
+//  Created by William Boles on 05/06/2024.
+//
+
+import Foundation
+
+struct DIStringMatch {
+    
+    //Time: O(
+    //Space: O(
+    //string
+    //array
+    //two pointers
+    //greedy
+    //
+    //Solution Description:
+    //Iterating through `s` we keep two values (pointers) - `low` and `high`, with `low` being 0 and `high` bring `n-1`.
+    //If we encounter "I" we append the `low` value to our `values` array and incremet `low`; if we encounter "D" then we
+    //append the `high` value to our `values` array and decrement `high`. As our `values` array should process the last
+    //element after iterating through all `chars` we append `high` (could have been `low` as they should both have the
+    //same value at this point) to `values` and return `values`.
+    func diStringMatch(_ s: String) -> [Int] {
+        let chars = Array(s)
+        
+        var low = 0
+        var high = chars.count
+        
+        var values = [Int]()
+        
+        for c in chars {
+            if c == "I" {
+                values.append(low)
+                low += 1
+            } else {
+                values.append(high)
+                high -= 1
+            }
+        }
+        values.append(high)
+        
+        return values
+    }
+}
---
+++ LeetCode/LeetCodeTests/Tests/DIStringMatchTests.swift
@@ -0,0 +1,39 @@
+//
+//  DIStringMatchTests.swift
+//  LeetCodeTests
+//
+//  Created by William Boles on 05/06/2024.
+//
+
+import XCTest
+
+@testable import LeetCode
+
+final class DIStringMatchTests: XCTestCase {
+
+    // MARK: - Tests
+    
+    func test_A() {
+        let s = "IDID"
+        
+        let result = DIStringMatch().diStringMatch(s)
+        
+        XCTAssertEqual(result, [0,4,1,3,2])
+    }
+    
+    func test_B() {
+        let s = "III"
+        
+        let result = DIStringMatch().diStringMatch(s)
+        
+        XCTAssertEqual(result, [0,1,2,3])
+    }
+    
+    func test_C() {
+        let s = "DDI"
+        
+        let result = DIStringMatch().diStringMatch(s)
+        
+        XCTAssertEqual(result, [3,2,0,1])
+    }
+}
