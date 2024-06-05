2c0a438ba69023b85750a321c4d53359c8478e74
William Boles <wibosco@users.noreply.github.com>
2024-06-05 12:09:01 +0100

    Implemented 'Lexicographically Smallest Palindrome' challenge

---
+++ LeetCode/LeetCode/Challenges/LexicographicallySmallestPalindrome.swift
@@ -0,0 +1,43 @@
+//
+//  LexicographicallySmallestPalindrome.swift
+//  LeetCode
+//
+//  Created by William Boles on 05/06/2024.
+//
+
+import Foundation
+
+//https://leetcode.com/problems/lexicographically-smallest-palindrome/
+struct LexicographicallySmallestPalindrome {
+    
+    //Time: O(n) where n is the number of characters in `s`
+    //Space: O(1)
+    //string
+    //array
+    //two pointers
+    //
+    //Solution Description:
+    //Using two pointers we work out-to-in, comparing the characters at each pointer. Where the characters don't match we replace the
+    //larger of the characters with the smaller so ensuring that we are producing the lexicographically smallest palindrome possible.
+    func makeSmallestPalindrome(_ s: String) -> String {
+        var characters = Array(s)
+        
+        var p1 = 0
+        var p2 = characters.count - 1
+        
+        while p1 < p2 {
+            if characters[p1] != characters[p2] {
+                if characters[p1] > characters[p2] {
+                    characters[p1] = characters[p2]
+                } else {
+                    characters[p2] = characters[p1]
+                }
+            }
+            
+            p1 += 1
+            p2 -= 1
+        }
+        
+        return String(characters)
+    }
+}
---
+++ LeetCode/LeetCodeTests/Tests/LexicographicallySmallestPalindromeTests.swift
@@ -0,0 +1,39 @@
+//
+//  LexicographicallySmallestPalindromeTests.swift
+//  LeetCodeTests
+//
+//  Created by William Boles on 05/06/2024.
+//
+
+import XCTest
+
+@testable import LeetCode
+
+final class LexicographicallySmallestPalindromeTests: XCTestCase {
+
+    // MARK: - Tests
+    
+    func test_A() {
+        let s = "egcfe"
+        
+        let result = LexicographicallySmallestPalindrome().makeSmallestPalindrome(s)
+        
+        XCTAssertEqual(result, "efcfe")
+    }
+    
+    func test_B() {
+        let s = "abcd"
+        
+        let result = LexicographicallySmallestPalindrome().makeSmallestPalindrome(s)
+        
+        XCTAssertEqual(result, "abba")
+    }
+    
+    func test_C() {
+        let s = "seven"
+        
+        let result = LexicographicallySmallestPalindrome().makeSmallestPalindrome(s)
+        
+        XCTAssertEqual(result, "neven")
+    }
+}
