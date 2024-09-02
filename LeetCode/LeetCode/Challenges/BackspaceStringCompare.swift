516fd34d7dc6f09732df42492ce82162fd1bdf6b
William Boles <wibosco@users.noreply.github.com>
2024-09-02 20:54:59 +0100

    Implemented 'Assign Cookies' challenge

---
+++ LeetCode/LeetCode/Challenges/BackspaceStringCompare.swift
@@ -0,0 +1,44 @@
+//
+//  BackspaceStringCompare.swift
+//  LeetCode
+//
+//  Created by William Boles on 02/09/2024.
+//
+
+import Foundation
+
+//https://leetcode.com/problems/backspace-string-compare/
+struct BackspaceStringCompare {
+    
+    //Time: O(n + m) where n is the number of characters in `s`
+    //               where m is the number of characters in `t`
+    //Space: O(n + m)
+    //string
+    //array
+    //stack
+    //
+    //Solution Description:
+    //Using two stacks we iterate through `s` and `t` - when we encounter a non "#" character we add it to the stack, when we 
+    //encounter a "#" character we pop the last element in the stack. Once all characters in `s` and `t` have been trimmed we
+    //compare the stacks to determine if they are the same.
+    func backspaceCompare(_ s: String, _ t: String) -> Bool {
+        let trimmedS = trim(s)
+        let trimmedT = trim(t)
+        
+        return trimmedS == trimmedT
+    }
+    
+    private func trim(_ s: String) -> [Character] {
+        var trimmed = [Character]()
+        
+        for c in s {
+            if c == "#" {
+                _ = trimmed.popLast()
+            } else {
+                trimmed.append(c)
+            }
+        }
+        
+        return trimmed
+    }
+}
---
+++ LeetCode/LeetCodeTests/Tests/BackspaceStringCompareTests.swift
@@ -0,0 +1,42 @@
+//
+//  BackspaceStringCompareTests.swift
+//  LeetCodeTests
+//
+//  Created by William Boles on 02/09/2024.
+//
+
+import XCTest
+
+@testable import LeetCode
+
+final class BackspaceStringCompareTests: XCTestCase {
+
+    // MARK: - Tests
+    
+    func test_A() {
+        let s = "ab#c"
+        let t = "ad#c"
+            
+        let result = BackspaceStringCompare().backspaceCompare(s, t)
+        
+        XCTAssertTrue(result)
+    }
+    
+    func test_B() {
+        let s = "ab##"
+        let t = "c#d#"
+            
+        let result = BackspaceStringCompare().backspaceCompare(s, t)
+        
+        XCTAssertTrue(result)
+    }
+    
+    func test_C() {
+        let s = "a#c"
+        let t = "b"
+            
+        let result = BackspaceStringCompare().backspaceCompare(s, t)
+        
+        XCTAssertFalse(result)
+    }
+}
