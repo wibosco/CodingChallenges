d39ccc75e88c0d1afa944ee43d2647f8d3a3b1dd
William Boles <wibosco@users.noreply.github.com>
2025-02-04 15:09:20 +0000

    Implemented 'Find the Lexicographically Smallest Valid Sequence' challenge

---
+++ LeetCode/LeetCode/Challenges/FindTheLexicographicallySmallestValidSequence.swift
@@ -0,0 +1,92 @@
+//
+//  AAFindTheLexicographicallySmallestValidSequence.swift
+//  LeetCode
+//
+//  Created by William Boles on 04/02/2025.
+//
+
+//
+import Foundation
+
+//https://leetcode.com/problems/find-the-lexicographically-smallest-valid-sequence/
+struct FindTheLexicographicallySmallestValidSequence {
+    
+    //Time: O(n * m) where n is the number of characters in `words1`
+    //               where m is the number of characters in `words2`
+    //Space: O(
+    //string
+    //array
+    //DFS
+    //recursive
+    //in-out
+    //backtracking
+    //memoization
+    //greedy
+    //two pointers
+    //
+    //Solution Description:
+    //Treating `word1` as a graph we can use DFS with backtracking to traverse it in the pursuit of finding a sequence of
+    //characters that match `words2`. At each node in `words1` we check if it is equal to the current search character of
+    //`words2` - if they are equal we "take" that node and add it to `currentPath`; if they are not equal we potentally
+    //follow two paths:
+    //
+    //1. If `changes` is greater than 0 we can change the node's value to match the current search character and can
+    //   continue traversing down that path.
+    //2. We skip that node and check the next node for the current search character.
+    //
+    //To improve the efficiency of our search we use memoization to store any failed paths we have already traversed. If
+    //we find a path through `word1` that matches `word2` we set that path to `foundPath` and return from our recursive
+    //stack.
+    func validSequence(_ word1: String, _ word2: String) -> [Int] {
+        let chars1 = Array(word1)
+        let chars2 = Array(word2)
+        
+        var memo = Set<[Int]>()
+        var currentPath = [Int]()
+        var foundPath = [Int]()
+        
+        dfs(chars1, chars2, 0, 0, 1, &currentPath, &foundPath, &memo)
+        
+        return foundPath
+    }
+    
+    private func dfs(_ chars1: [Character], _ chars2: [Character], _ p1: Int, _ p2: Int, _ changes: Int, _ currentPath: inout [Int], _ foundPath: inout [Int], _ memo: inout Set<[Int]>) {
+        guard foundPath.isEmpty else {
+            return
+        }
+        
+        guard changes >= 0 else {
+            return
+        }
+        
+        guard p2 < chars2.count else {
+            foundPath = currentPath
+            return
+        }
+        
+        guard p1 < chars1.count else {
+            return
+        }
+        
+        let key = [currentPath.count, p1, p2, changes]
+        guard !memo.contains(key) else {
+            return
+        }
+
+        if chars1[p1] == chars2[p2] {
+            currentPath.append(p1)
+            dfs(chars1, chars2, p1 + 1, p2 + 1, changes, &currentPath, &foundPath, &memo)
+            currentPath.removeLast()
+        } else {
+            currentPath.append(p1)
+            dfs(chars1, chars2, p1 + 1, p2 + 1, changes - 1, &currentPath, &foundPath, &memo)
+            currentPath.removeLast()
+            
+            if foundPath.isEmpty { //no path found so try skipping
+                dfs(chars1, chars2, p1 + 1, p2, changes, &currentPath, &foundPath, &memo)
+            }
+        }
+        
+        memo.insert(key)
+    }
+}
---
+++ LeetCode/LeetCodeTests/Tests/FindTheLexicographicallySmallestValidSequenceTests.swift
@@ -0,0 +1,69 @@
+//
+//  FindTheLexicographicallySmallestValidSequenceTests.swift
+//  LeetCodeTests
+//
+//  Created by William Boles on 04/02/2025.
+//
+
+import XCTest
+
+@testable import LeetCode
+
+final class FindTheLexicographicallySmallestValidSequenceTests: XCTestCase {
+    
+    // MARK: - Tests
+    
+    func test_A() {
+        let word1 = "vbcca"
+        let word2 = "abc"
+            
+        let result = FindTheLexicographicallySmallestValidSequence().validSequence(word1, word2)
+        
+        XCTAssertEqual(result, [0,1,2])
+    }
+    
+    func test_B() {
+        let word1 = "bacdc"
+        let word2 = "abc"
+            
+        let result = FindTheLexicographicallySmallestValidSequence().validSequence(word1, word2)
+        
+        XCTAssertEqual(result, [1,2,4])
+    }
+    
+    func test_C() {
+        let word1 = "aaaaaa"
+        let word2 = "aaabc"
+            
+        let result = FindTheLexicographicallySmallestValidSequence().validSequence(word1, word2)
+        
+        XCTAssertEqual(result, [])
+    }
+    
+    func test_D() {
+        let word1 = "abc"
+        let word2 = "ab"
+            
+        let result = FindTheLexicographicallySmallestValidSequence().validSequence(word1, word2)
+        
+        XCTAssertEqual(result, [0,1])
+    }
+    
+    func test_E() {
+        let word1 = "cdbbcdddddbdddbcdbdbbbccbbc"
+        let word2 = "dbc"
+            
+        let result = FindTheLexicographicallySmallestValidSequence().validSequence(word1, word2)
+        
+        XCTAssertEqual(result, [0,2,4])
+    }
+    
+    func test_F() {
+        let word1 = "gfgggfgfgfffggggggggfgfggfffgffggfffggggfgfggfggggfffg"
+        let word2 = "gggffgg"
+            
+        let result = FindTheLexicographicallySmallestValidSequence().validSequence(word1, word2)
+        
+        XCTAssertEqual(result, [0,1,2,5,7,8,12])
+    }
+}
