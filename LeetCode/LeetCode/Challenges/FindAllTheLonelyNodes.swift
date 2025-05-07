46f39ef933fdffcf7bdb97a5461f35d448c78adb
William Boles <wibosco@users.noreply.github.com>
2025-05-07 15:32:56 +0100

    Added additional solution for 'Find All The Lonely Nodes' challenge

--- LeetCode/LeetCode/Challenges/FindAllTheLonelyNodes.swift
+++ LeetCode/LeetCode/Challenges/FindAllTheLonelyNodes.swift
@@ -11,7 +11,7 @@ import Foundation
 struct FindAllTheLonelyNodes {
     
     //Time: O(n) where n is the number of nodes in the tree
-    //Space: O(h) where h is the height of the tree
+    //Space: O(n)
     //binary tree
     //DFS
     //recursive
@@ -22,6 +22,38 @@ struct FindAllTheLonelyNodes {
     //Using DFS we visit each node in the tree and check if that node only has one child, if so we add that child node to
     //`lonelyNodes` else we skip over it.
     func getLonelyNodes(_ root: TreeNode?) -> [Int] {
+        var lonelyNodes = [Int]()
+        
+        dfs(root, false, &lonelyNodes)
+        
+        return lonelyNodes
+    }
+    
+    private func dfs(_ node: TreeNode?, _ lonelyNode: Bool, _ lonelyNodes: inout [Int]) {
+        guard let node else {
+            return
+        }
+        
+        if lonelyNode {
+            lonelyNodes.append(node.val)
+        }
+        
+        dfs(node.left, (node.right == nil), &lonelyNodes)
+        dfs(node.right, (node.left == nil), &lonelyNodes)
+    }
+    
+    //Time: O(n) where n is the number of nodes in the tree
+    //Space: O(h) where h is the height of the tree
+    //binary tree
+    //DFS
+    //recursive
+    //top-down
+    //inout
+    //
+    //Solution Description:
+    //Using DFS we visit each node in the tree and check if that node only has one child, if so we add that child node to
+    //`lonelyNodes` else we skip over it.
+    func getLonelyNodes2(_ root: TreeNode?) -> [Int] {
         var lonelyNodes = [Int]()
         
         dfs(root, &lonelyNodes)
