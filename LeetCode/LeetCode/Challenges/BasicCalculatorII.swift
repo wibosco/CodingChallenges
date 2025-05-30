68bdfbf25e7bbd871605a0202d65e129bc615047
William Boles <wibosco@users.noreply.github.com>
2025-05-30 12:38:14 +0100

    Added additional solution for 'Basic Calculator II' challenge

--- LeetCode/LeetCode/Challenges/BasicCalculatorII.swift
+++ LeetCode/LeetCode/Challenges/BasicCalculatorII.swift
@@ -12,6 +12,76 @@ import Foundation
 struct BasicCalculatorII {
     
     //Time: O(n) where n is the number of characters in `s`
+    //Space: O(n)
+    //stack
+    //string
+    //array
+    //maths
+    //fast forward
+    //
+    //Solution Description:
+    //Using BODMAS (Brackets, Orders, Division, Multiplication, Addition, Subtraction) we know that division and multiplication
+    //should be processed before addition and subtraction. In order to process `s` correctly we need to "park" any addition and
+    //subtraction operations until after all division and multiplication operations are complete. We can use a stack to hold the
+    //values and a variable to know the last operation seen. As we iterate through `s` we check if the current character of that
+    //iteration is a number or a symbol. If we encounter a number we fast forward through characters that make up that number,
+    //once the full number has been found we check what the current operation is. If the current operation is a "+" or "-" we
+    //store the number in `stack` (with the subtraction operation value being stored a negative as adding a negative is the same
+    //as directly subtracting - we will see this later); if the current operation is a "*" or "/" we process those straight away
+    //by popping the last value from the stack - the result of that operation is then stored in the `stack` to be used later. If
+    //the current iteration is a symbol we update `symbol`. Once all characters in `s` have been processed we are left with the
+    //addition and substraction operations to be performed which we can do by iterating through the stack and adding ever found
+    //to `total`, `total` is then returned as the result.
+    func calculate(_ s: String) -> Int {
+        var stack = [Int]()
+
+        let chars = Array(s)
+        var p1 = 0
+
+        var symbol: Character = "+"
+        while p1 < chars.count {
+            guard chars[p1] != " " else {
+                p1 += 1
+                continue
+            }
+
+            if Int(String(chars[p1])) != nil {
+                var value = 0
+                while p1 < chars.count, let num = Int(String(chars[p1])) {
+                    value *= 10
+                    value += num
+                    p1 += 1
+                }
+
+                if symbol == "*" {
+                    let last = stack.removeLast()
+                    stack.append((last * value))
+                } else if symbol == "/" {
+                    let last = stack.removeLast()
+                    stack.append((last / value))
+                } else if symbol == "-" {
+                    stack.append((value * -1))
+                } else {
+                    stack.append(value)
+                }
+
+                continue
+            } else {
+                symbol = chars[p1]
+            }
+
+            p1 += 1
+        }
+
+        var total = 0
+        for n in stack {
+            total += n
+        }
+
+        return total
+    }
+    
+    //Time: O(n) where n is the number of characters in `s`
     //Space: O(n) where n is the number of characters in `s`
     //array
     //stack
@@ -32,7 +102,7 @@ struct BasicCalculatorII {
     //
     //NB. We are processing the operation after we "parse" the second value in the operation by processing the operation symbol
     //after we encounter the next one (or the end of `s`).
-    func calculate(_ s: String) -> Int {
+    func calculate2(_ s: String) -> Int {
         var stack = [Int]()
         
         var currentValue = 0
