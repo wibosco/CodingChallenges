2599300f743208eadf9bcb53f7433129e65c9205
William Boles <wibosco@users.noreply.github.com>
2025-07-30 18:50:55 +0100

    Implemented 'LRUCache' challenge

---
+++ LeetCode/LeetCode/Challenges/LRUCache.swift
@@ -0,0 +1,90 @@
+// Created 30/07/2025.
+
+import Foundation
+
+//https://leetcode.com/problems/lru-cache/
+//linked list
+//doubly linked list
+//sentinel head
+//sentinel tail
+//queue
+//dictionary
+class LRUCache {
+    class Node {
+        let key: Int
+        let value: Int
+        
+        var prev: Node?
+        var next: Node?
+        
+        init(key: Int, value: Int) {
+            self.key = key
+            self.value = value
+        }
+    }
+    
+    private var head = Node(key: -1, value: -1)
+    private var tail = Node(key: -1, value: -1)
+    
+    private var nodes = [Int: Node]()
+    private let capacity: Int
+    
+    // MARK: - Init
+    
+    init(_ capacity: Int) {
+        self.capacity = capacity
+        
+        head.next = tail
+        tail.prev = head
+    }
+    
+    // MARK: - Get
+    
+    func get(_ key: Int) -> Int {
+        guard let node = nodes[key] else {
+            return -1
+        }
+        
+        //just delete and re-add as the tail to move it the end of the deletion queue
+        delete(node)
+        add(node)
+        
+        return node.value
+    }
+    
+    // MARK: - Put
+    
+    func put(_ key: Int, _ value: Int) {
+        if let node = nodes[key] {
+            delete(node)
+        }
+        
+        let node = Node(key: key, value: value)
+        nodes[key] = node
+        add(node)
+        
+        if nodes.count > capacity {
+            if let nodeToBeDeleted = head.next {
+                delete(nodeToBeDeleted)
+                nodes[nodeToBeDeleted.key] = nil
+            }
+        }
+    }
+    
+    // MARK: - Delete
+    
+    private func delete(_ node: Node) {
+        //skip over node
+        node.prev?.next = node.next
+        node.next?.prev = node.prev
+    }
+    
+    // MARK: - Add
+    
+    private func add(_ node: Node) {
+        tail.prev?.next = node
+        node.prev = tail.prev
+        node.next = tail
+        tail.prev = node
+    }
+}
---
+++ LeetCode/LeetCodeTests/Tests/LRUCacheTests.swift
@@ -0,0 +1,43 @@
+// Created 30/07/2025.
+
+import Testing
+
+@testable import LeetCode
+
+struct LRUCacheTests {
+
+    // MARK: - Tests
+    
+    @Test
+    func test_A() {
+        let cache = LRUCache(2)
+        
+        cache.put(1, 1)
+        cache.put(2, 2)
+        
+        #expect(cache.get(1) == 1)
+        
+        cache.put(3, 3)
+        
+        #expect(cache.get(2) == -1)
+        
+        cache.put(4, 4)
+        
+        #expect(cache.get(1) == -1)
+        #expect(cache.get(3) == 3)
+        #expect(cache.get(4) == 4)
+    }
+    
+    @Test
+    func test_B() {
+        let cache = LRUCache(2)
+        
+        cache.put(2, 1)
+        cache.put(1, 1)
+        cache.put(2, 3)
+        cache.put(4, 1)
+        
+        #expect(cache.get(1) == -1)
+        #expect(cache.get(2) == 3)
+    }
+}
