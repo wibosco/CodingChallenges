befa9af92d0da22430c3a8c1ae4cb920304bce33
William Boles <wibosco@users.noreply.github.com>
2025-07-31 13:03:26 +0100

    Added additional solution for 'Merge Intervals' challenge

--- LeetCode/LeetCode/Challenges/MergeIntervals.swift
+++ LeetCode/LeetCode/Challenges/MergeIntervals.swift
@@ -16,7 +16,9 @@ struct MergeIntervals {
     //array
     //sorting
     //intervals
+    //fast forward
     //
+    //Solution Description:
     //Solution Description:
     //By sorting the intervals into ascending order we can reduce the amount of work required to find an overlapping interval
     //as any overlapping intervals will be neighbors. As it's possible for multiple intervals to share an overlap it is not
@@ -26,6 +28,47 @@ struct MergeIntervals {
     //or equal to the interval-to-be-merged lower boundary. If there isn't an overlap we add the interval to the
     //`mergedIntervals` array. Once all intervals have been compared we return the `mergedIntervals` array.
     func merge(_ intervals: [[Int]]) -> [[Int]] {
+        let sortedIntervals = intervals.sorted(by: { $0[0] < $1[0] })
+
+        var mergedIntervals = [[Int]]()
+        var p1 = 0
+
+        while p1 < sortedIntervals.count {
+            let start = sortedIntervals[p1][0]
+            var end = sortedIntervals[p1][1]
+
+            var p2 = (p1 + 1)
+            while p2 < sortedIntervals.count {
+                guard sortedIntervals[p2][0] <= end else {
+                    break
+                }
+
+                end = max(end, sortedIntervals[p2][1])
+                p2 += 1
+            }
+
+            mergedIntervals.append([start, end])
+            p1 = p2
+        }
+
+        return mergedIntervals
+    }
+    
+    //Time: O(n log n) where n is the number of intervals
+    //Space: O(n) where n is the number of intervals (we copy the `intervals` array to sort it)
+    //array
+    //sorting
+    //intervals
+    //
+    //Solution Description:
+    //By sorting the intervals into ascending order we can reduce the amount of work required to find an overlapping interval
+    //as any overlapping intervals will be neighbors. As it's possible for multiple intervals to share an overlap it is not
+    //enough to merely compare one neighbour with another neighbour instead we need to compare an interval against the last
+    //already merged interval. Where an overlap exists we take the max upper boundary of the two intervals that are to be
+    //merged and set that as the merged intervals upper boundary - the lower boundary never changes as it is either less than
+    //or equal to the interval-to-be-merged lower boundary. If there isn't an overlap we add the interval to the
+    //`mergedIntervals` array. Once all intervals have been compared we return the `mergedIntervals` array.
+    func merge2(_ intervals: [[Int]]) -> [[Int]] {
         guard intervals.count > 1 else {
             return intervals
         }
@@ -49,6 +92,6 @@ struct MergeIntervals {
             }
         }
         
-       return mergedIntervals
+        return mergedIntervals
     }
 }
--- LeetCode/LeetCodeTests/Tests/MergeIntervalsTests.swift
+++ LeetCode/LeetCodeTests/Tests/MergeIntervalsTests.swift
@@ -15,42 +15,50 @@ final class MergeIntervalsTests: XCTestCase {
     // MARK: - Tests
 
     func test_A() {
-        let intervals = [[1, 3], [2, 6], [8, 10], [15, 18]]
+        let intervals = [[1,3],[2,6],[8,10],[15,18]]
         
         let result = MergeIntervals().merge(intervals)
         
-        XCTAssertEqual(result, [[1, 6], [8, 10], [15, 18]])
+        XCTAssertEqual(result,[[1,6],[8,10],[15,18]])
     }
     
     func test_B() {
-        let intervals = [[1, 4], [4, 5]]
+        let intervals = [[1,4],[4,5]]
         
         let result = MergeIntervals().merge(intervals)
         
-        XCTAssertEqual(result, [[1, 5]])
+        XCTAssertEqual(result,[[1,5]])
     }
     
     func test_C() {
-        let intervals = [[1, 4], [0, 4]]
+        let intervals = [[1,4],[0,4]]
         
         let result = MergeIntervals().merge(intervals)
         
-        XCTAssertEqual(result, [[0, 4]])
+        XCTAssertEqual(result,[[0,4]])
     }
     
     func test_D() {
-        let intervals = [[4, 5], [1, 4], [0, 1]]
+        let intervals = [[4,5],[1,4],[0,1]]
         
         let result = MergeIntervals().merge(intervals)
         
-        XCTAssertEqual(result, [[0, 5]])
+        XCTAssertEqual(result,[[0,5]])
     }
     
     func test_E() {
-        let intervals = [[2, 3], [4, 5], [6, 7], [8, 9], [1, 10]]
+        let intervals = [[2,3],[4,5],[6,7],[8,9],[1,10]]
         
         let result = MergeIntervals().merge(intervals)
         
-        XCTAssertEqual(result, [[1, 10]])
+        XCTAssertEqual(result,[[1,10]])
+    }
+    
+    func test_F() {
+        let intervals = [[1,4],[2,3]]
+        
+        let result = MergeIntervals().merge(intervals)
+        
+        XCTAssertEqual(result,[[1,4]])
     }
 }
