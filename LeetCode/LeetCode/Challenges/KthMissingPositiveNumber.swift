c4e1b9ace740ff2c90e7bdbe3534a8359dcb7475
William Boles <wibosco@users.noreply.github.com>
2025-08-01 23:48:58 +0100

    Added additional solution for 'Kth Missing Positive Number' challenge

--- LeetCode/LeetCode/Challenges/KthMissingPositiveNumber.swift
+++ LeetCode/LeetCode/Challenges/KthMissingPositiveNumber.swift
@@ -10,72 +10,6 @@ import Foundation
 //https://leetcode.com/problems/kth-missing-positive-number/
 struct KthMissingPositiveNumber {
     
-    //Time: O(log n) where n is the number of elements in `arr`
-    //Space: O(1)
-    //array
-    //binary search
-    //
-    //Solution Description:
-    //As we have a sorted array we can use binary search to determine where to the `kth` value should be. First we "fill in" any
-    //gaps before searching by reducing `remaining` value to get us to the first element in `arr`. If that uses all of `k` then
-    //we return `k` as the `kth` value. If not we perform a binary search. During the binary we attempt to "fill in" any gaps
-    //up the index we are using as `mid`. We do this by keeping track of the most rightward already fully filled in indexes (to
-    //start with this will be index 0). If we can get to that `mid` index fully filled in with `remaining` > 0 then we know that
-    //the `kth` index is to the right, else if `remaining` < 0 then we know that the `kth` index is the left. As we move left
-    //and right we gradually find the index to the left of the `kth` index and so add any remaining `remaining` value to that
-    //indexes value.
-    //
-    //Similar to: https://leetcode.com/problems/missing-element-in-sorted-array/
-    func findKthPositive(_ arr: [Int], _ k: Int) -> Int {
-        var remaining = k
-        
-        if arr[0] > 1 {
-            remaining -= (arr[0] - 1)
-        }
-        
-        guard remaining > 0 else {
-            return k
-        }
-        
-        let result = binarySearch(arr, remaining)
-        
-        return result
-    }
-    
-    private func binarySearch(_ values: [Int], _ remaining: Int) -> Int {
-        var remaining = remaining
-        var gapsFilledUpToIndex = 0
-        
-        var left = 0
-        var right = values.count - 1
-        
-        while left <= right {
-            let mid = left + (right - left) / 2 //to avoid overflow
-
-            let deltaIndex = abs(gapsFilledUpToIndex - mid)
-            let deltaValue = abs(values[gapsFilledUpToIndex] - values[mid])
-            let gapsToBeFilled = abs(deltaIndex - deltaValue)
-            
-            if gapsToBeFilled == 0 {
-                //no missing values in between, move rightwards
-                left = mid + 1
-                gapsFilledUpToIndex = mid
-            } else {
-                if gapsToBeFilled >= remaining {
-                    //too many missing values to fill in, move leftwards
-                    right = mid - 1
-                } else if gapsToBeFilled < remaining {
-                    //can fill in missing values, move rightwards
-                    left = mid + 1
-                    remaining -= gapsToBeFilled
-                    gapsFilledUpToIndex = mid
-                }
-            }
-        }
-        
-        return values[right] + remaining
-    }
-    
     //Time: O(n) where n is the number of elements in `arr`
     //Space: O(1)
     //array
@@ -86,7 +20,7 @@ struct KthMissingPositiveNumber {
     //we move the array onto it's next value. If the `kth` value is at the start or in the middle of `arr` then `remaining` will
     //reach 0 and we can return the `value` we stopped on; if the `kth` value is after the end of `arr` then we add `remainng`
     //onto the final value in the array.
-    func findKthPositiveLooping(_ arr: [Int], _ k: Int) -> Int {
+    func findKthPositive(_ arr: [Int], _ k: Int) -> Int {
         var remaining = k
         var value = 0
         var arrIndex = 0
@@ -102,5 +36,61 @@ struct KthMissingPositiveNumber {
         }
         
         return value + remaining
+    }
+    
+    //Time: O(n) where n is the number of elements in `arr`
+    //Space: O(1)
+    //array
+    //
+    //Solution Description:
+    //Rather than fill in the gaps we use the value that are there to increase `k` to what it should be in `arr`. As we loop
+    //through 'arr', we check if the current value `num` is less than or equal to `k` if it then we increase `k` as this value
+    //doesn't need to be filled in; if it is not then we can't fill in to this value and the current value of `k` is that
+    //missing value and can be returned.
+    func findKthPositive2(_ arr: [Int], _ k: Int) -> Int {
+        var k = k
+        
+        for num in arr {
+            guard num <= k else {
+                break
+            }
+
+            k += 1
+        }
+
+        return k
+    }
+    
+    //Time: O(log n) where n is the number of elements in `arr`
+    //Space: O(1)
+    //array
+    //binary search
+    //
+    //Solution Description:
+    //As we have a sorted array we can use binary search to determine where to the `kth` value should be. If an index had no
+    //missing values the value of that index would be the index itself - we can use the calculated missing value count to
+    //determine which way to move the `left` or `right`. Once `left` crosses `right`, `left` will be the index that kth
+    //element should be but isn't so we add `k` to that index to the missing kth value.
+    //
+    //Similar to: https://leetcode.com/problems/missing-element-in-sorted-array/
+    func findKthPositive3(_ arr: [Int], _ k: Int) -> Int {
+        var left = 0
+        var right = arr.count - 1
+
+        while left <= right {
+            let mid = left + (right - left) / 2
+            let missingCount = arr[mid] - (mid + 1)
+
+            if missingCount >= k {
+                right = mid - 1
+            } else {
+                left = mid + 1
+            }
+        }
+
+        //the left index is what the value would be for that index if nothing was missing
+        //so adding k to it gives the kth missing value as that would the same as filling
+        //in those missing values
+        return left + k
     }
 }
