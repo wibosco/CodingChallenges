57d722fcab8f13d3e823127026230309beee7e5b
William Boles <wibosco@users.noreply.github.com>
2025-08-03 22:36:54 +0100

    Added additional solution for 'Find Pivot Index' challenge

--- LeetCode/LeetCode/Challenges/FindPivotIndex.swift
+++ LeetCode/LeetCode/Challenges/FindPivotIndex.swift
@@ -8,11 +8,11 @@
 import Foundation
 
 //https://leetcode.com/problems/find-pivot-index/
-//array
 struct FindPivotIndex {
     
     //Time: O(n) where n is the number of elements in `nums`
     //Space: O(1)
+    //array
     //prefix sum
     //
     //Solution Description:
@@ -39,6 +39,50 @@ struct FindPivotIndex {
             leftSum += num
         }
         
+        return -1
+    }
+    
+    //Time: O(n) where n is the number of elements in `nums`
+    //Space: O(n)
+    //array
+    //prefix sum
+    //
+    //Solution Description:
+    //First we build up a prefix sum array going from rightwards (0..<n) with the calculated sum at each index being store
+    //for the managing index. Next we repeat this process going leftwards ((n - 1)...0). Now that we have two prefix sum
+    //arrays we iterate through `nums` again, this time comparing the prefix sums from each array at either side of the
+    //current `nums` index so `forwardPrefix` is checked at `i - 1` and `backwardPrefix` is checked at `i + 1` as the
+    //values at those indexes show the pre or post sum from `i`. If those values match we have found the pivot; if those
+    //values don't match we move onto the next iteration and repeat the process. If having processed all elements in
+    //`nums` we haven't found a pivot, we return -1.
+    func pivotIndex2(_ nums: [Int]) -> Int {
+        var prefix = 0
+
+        var forwardPrefix = Array(repeating: 0, count: nums.count)
+        
+        for i in 0..<nums.count {
+            prefix += nums[i]
+            forwardPrefix[i] = prefix
+        }
+
+        prefix = 0
+
+        var backwardPrefix = Array(repeating: 0, count: nums.count)
+
+        for i in (0..<nums.count).reversed() {
+            prefix += nums[i]
+            backwardPrefix[i] = prefix
+        }
+
+        for i in 0..<nums.count {
+            let preSum = i == 0 ? 0 : forwardPrefix[(i - 1)]
+            let postSum = i == (nums.count - 1) ? 0 : backwardPrefix[(i + 1)]
+
+            if preSum == postSum {
+                return i
+            }
+        }
+
         return -1
     }
 }
