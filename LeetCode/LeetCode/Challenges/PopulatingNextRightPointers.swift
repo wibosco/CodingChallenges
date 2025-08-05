f2e0de106b00217271978c835694b2fd50550064
William Boles <wibosco@users.noreply.github.com>
2025-08-05 15:10:02 +0100

    Added additional solution for 'Populating Next Right Pointers in Each Node' challenge

--- LeetCode/LeetCode/Challenges/PopulatingNextRightPointers.swift
+++ LeetCode/LeetCode/Challenges/PopulatingNextRightPointers.swift
@@ -11,17 +11,89 @@ import Foundation
 //https://leetcode.com/problems/populating-next-right-pointers-in-each-node/
 struct PopulatingNextRightPointers {
     
-    //Time: O(n)
+    //Time: O(n) where n is the number of nodes in the tree
+    //Space: O(h) where h is the height of the tree
+    //binary tree - perfect
+    //DFS
+    //recursive
+    //
+    //Solution Description:
+    //Using DFS we traverse the tree and set the `next` property for each node's child nodes we encounter. To conenct
+    //subtrees we use the `next` property of the current node to get to it's `next` nodes child nodes.
+    func connect(_ root: TreeNodeNext?) -> TreeNodeNext? {
+        dfs(root)
+
+        return root
+    }
+
+    private func dfs(_ node: TreeNodeNext?) {
+        guard let node else {
+            return
+        }
+
+        node.left?.next = node.right
+        node.right?.next = node.next?.left
+
+        dfs(node.left)
+        dfs(node.right)
+    }
+    
+    
+    //Time: O(n) where n is the number nodes in the tree
+    //Space: O(m) where m is number of nodes at the last level (as it's a perfect tree)
+    //binary tree - perfect
+    //BFS
+    //iterative
+    //level traversal
+    //queue
+    //
+    //Solution Description:
+    //Using BFS we traverse the tree level-by-level. At each level we track the previous (left) node for the current
+    //node and then set the `next` property on the `prev` node to the current node. We repeat this process for each
+    //node in each level until all nodes have been processed. We then return `root`.
+    func connect2(_ root: TreeNodeNext?) -> TreeNodeNext? {
+        guard let root else {
+            return nil
+        }
+
+        var queue = [TreeNodeNext]()
+        queue.append(root)
+
+        while !queue.isEmpty {
+            var nextQueue = [TreeNodeNext]()
+            var prev: TreeNodeNext?
+
+            for node in queue {
+                prev?.next = node
+                prev = node
+
+                if let left = node.left {
+                    nextQueue.append(left)
+                }
+
+                if let right = node.right {
+                    nextQueue.append(right)
+                }
+            }
+
+            queue = nextQueue
+        }
+
+        return root
+    }
+    
+    //Time: O(n) where n is the number nodes in the tree
     //Space: O(1)
     //binary tree - perfect
     //BFS
+    //iterative
     //linked list - treat each level as a linked list
     //
     //Solution Description:
-    //1. BFS traversal
-    //2. Treat each level as a linked list
-    //3. Set N + 1 `next` property from level N
-    func connect(_ root: TreeNodeNext?) -> TreeNodeNext? {
+    //Using BFS we traverse the tree level-by-level and set the `next` property for each node's child nodes we
+    //encounter. To conenct subtrees we use the `next` property of the current node to get to it's `next` nodes
+    //child nodes.
+    func connect3(_ root: TreeNodeNext?) -> TreeNodeNext? {
         guard let root = root else {
             return nil
         }
@@ -42,49 +114,7 @@ struct PopulatingNextRightPointers {
                 head = head?.next //move onto next node on same level
             }
             
-            leftMostNode = leftMostNode?.left //move onto next level
-        }
-        
-        return root
-    }
-    
-    //Time: O(n)
-    //Space: O(n) where n is number of nodes at the last level (as it's a perfect tree)
-    //binary tree - perfect
-    //BFS
-    //queue - use queue count as level count
-    //pointer
-    //
-    //Solution Description:
-    //1. BFS traversal
-    //2. Queue nodes
-    //3. Use a `prev` to set the N - 1 nodes next property to the N node
-    func connectQueue(_ root: TreeNodeNext?) -> TreeNodeNext? {
-        guard let root = root else {
-            return nil
-        }
-        
-        var queue = [root]
-        
-        while !queue.isEmpty {
-            let count = queue.count //how many nodes are in this level
-            var prev: TreeNodeNext?
-            
-            for _ in 0..<count { //need to know how many times we remove from the queue for this level
-                let node = queue.removeFirst() //O(n)
-                
-                if let left = node.left {
-                    queue.append(left)
-                    prev?.next = left
-                    prev = left
-                }
-                
-                if let right = node.right {
-                    queue.append(right)
-                    prev?.next = right
-                    prev = right
-                }
-            }
+            leftMostNode = leftMostNode?.left //move onto left most node on the next level
         }
         
         return root
