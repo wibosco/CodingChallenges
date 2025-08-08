25c3cc51d03b09142e18cba001e74fba274a3526
William Boles <wibosco@users.noreply.github.com>
2025-08-08 22:02:21 +0100

    Added additional solution for 'Group Anagrams' challenge

--- LeetCode/LeetCode/Challenges/GroupAnagrams.swift
+++ LeetCode/LeetCode/Challenges/GroupAnagrams.swift
@@ -11,7 +11,7 @@ import Foundation
 struct GroupAnagrams {
     
     //Time: O(n m log m) where n is the number of elements in `strs`
-    //                   where m is max character length
+    //                   where m is max length of an element within `strs`
     //Space: O(n) where n is the number of elements in `strs`
     //string
     //sorting
@@ -35,5 +35,37 @@ struct GroupAnagrams {
         }
         
         return grouping.values.map { Array($0) }
+    }
+    
+    //Time: O(n * m) where n is the number of elements in `strs`
+    //               where m is max length of an element within `strs`
+    //Space: O(n) where n is the number of elements in `strs`
+    //string
+    //counting
+    //dictionary
+    //
+    //Solution Description:
+    //If one string is an anagram of another it will contain the exact same characters just in a different order. Rather go through each
+    //`strs` element and compare against each other one, we can instead count the characters in each `strs` element and place this count
+    //into a dictionary - this will result in anagrams having the the same count dictionary. This count dictionary can then be used as a
+    //key for an outer dictionary that contains the `strs` elements. Once all `strs` elements have been placed in `grouping` we can
+    //extract the value from each key, combine those values and return them.
+    func groupAnagrams2(_ strs: [String]) -> [[String]] {
+        var grouping = [[Character: Int]: [String]]()
+
+        for str in strs {
+            var key = [Character: Int]()
+            for c in str {
+                key[c, default: 0] += 1
+            }
+            grouping[key, default: [String]()].append(str)
+        }
+
+        var combined = [[String]]()
+        for value in grouping.values {
+            combined.append(value)
+        }
+
+        return combined
     }
 }
