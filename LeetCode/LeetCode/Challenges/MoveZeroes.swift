e79d06d5dce4f466fc38b5ead2ebeef83885c7dd
William Boles <wibosco@users.noreply.github.com>
2025-08-10 09:31:55 +0100

    Added additional solution for 'Move Zeroes' challenge

--- LeetCode/LeetCode/Challenges/MoveZeroes.swift
+++ LeetCode/LeetCode/Challenges/MoveZeroes.swift
@@ -47,12 +47,44 @@ struct MoveZeros {
     //Time: O(n) where n is the number of elements in `nums`
     //Space: O(1)
     //array
+    //two pointers
+    //fast forward
+    //
+    //Solution Description:
+    //Using two pointers we iterate through `nums`. When we encounter a zero, `p1` get left on that zero, `p2` continues on.
+    //When `p2` encounters a non-zero, we check if `nums[p1]` is zero. If `nums[p1]` is a zero we swap the values of `p1` and
+    //`p2` and then fast forward `p1` to either the next zero or to `p2` (as we know that `p2` is now zero). If `nums[p1]`
+    //does not equal zero then `nums[p2]` is already in it's final position. We repeat this process until all elements in
+    //`nums` have been process and can the exit.
+    func moveZeroes2(_ nums: inout [Int]) {
+        var p1 = 0
+        var p2 = 0
+
+        while p2 < nums.count {
+            if nums[p2] != 0 && nums[p1] == 0 {
+                nums.swapAt(p1, p2)
+
+                while p1 < p2, nums[p1] != 0 {
+                    //fast forward to next zero
+                    p1 += 1
+                }
+            } else if nums[p1] != 0 {
+                p1 += 1
+            }
+
+            p2 += 1
+        }
+    }
+    
+    //Time: O(n^2) where n is the number of elements in `nums`
+    //Space: O(1)
+    //array
     //removal
     //
     //Solution Description:
     //Remove zeros from `nums` as this are found while keeping a count of how many have been removed. Once all zeros have been
     //removed, add them to the end of the array.
-    func moveZeroesAppending(_ nums: inout [Int]) {
+    func moveZeroes3(_ nums: inout [Int]) {
         guard nums.count > 1 else {
             return
         }
