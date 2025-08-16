3ca201d522118cc9e65b655785b0b885eb50fb5d
William Boles <wibosco@users.noreply.github.com>
2025-08-16 21:20:43 +0100

    Added additional solution for 'Copy List with Random Pointer' challenge

--- LeetCode/LeetCode/Challenges/CopyListWithRandomPointer.swift
+++ LeetCode/LeetCode/Challenges/CopyListWithRandomPointer.swift
@@ -12,6 +12,50 @@ import Foundation
 struct CopyListWithRandomPointer {
     
     //Time: O(n) where n is the number of nodes in the list
+    //Space: O(n)
+    //linked list
+    //dictionary
+    //
+    //Solution Description:
+    //In a single pass we iterate through the list and create copies of the current original `next` and `random` nodes. These
+    //copies are then set on the `next` and `random` property of the current copy node. Both the original and copy nodes are
+    //then moved forward. To avoid making multiple copies of the same node we store each copy of a node we make in `map` to
+    //be retrieved when required. Once all original nodes have been copied we return the copied head node.
+    func copyRandomList(_ head: ListNodeRandom?) -> ListNodeRandom? {
+        guard let head else {
+            return nil
+        }
+        var tail: ListNodeRandom? = head
+
+        let copyHead = ListNodeRandom(head.val)
+        var copyTail: ListNodeRandom? = copyHead
+
+        var map = [ListNodeRandom: ListNodeRandom]()
+        map[head] = copyHead
+        
+        while let t = tail {
+            if let next = t.next {
+                let copyNext = map[next] ?? ListNodeRandom(next.val)
+                copyTail?.next = copyNext
+
+                map[next] = copyNext //can't rely on a defaulted dictionary value here as it doesn't save it
+            }
+
+            if let random = t.random {
+                let copyRandom = map[random] ?? ListNodeRandom(random.val)
+                copyTail?.random = copyRandom
+
+                map[random] = copyRandom //can't rely on a defaulted dictionary value here as it doesn't save it
+            }
+
+            tail = t.next
+            copyTail = copyTail?.next
+        }
+        
+        return copyHead
+    }
+    
+    //Time: O(n) where n is the number of nodes in the list
     //Space: O(n) where n is the number of nodes in the list
     //linked list
     //recursive
@@ -24,7 +68,7 @@ struct CopyListWithRandomPointer {
     //the original node, if we have we return it else we create a copy and attempt to fill its `next` and `random` properties
     //before return it - this is a depth first approach. Once all copies have been made we use the `head` node to return its
     //copy
-    func copyRandomList(_ head: ListNodeRandom?) -> ListNodeRandom? {
+    func copyRandomList2(_ head: ListNodeRandom?) -> ListNodeRandom? {
         guard let head = head else {
             return nil
         }
@@ -64,7 +108,7 @@ struct CopyListWithRandomPointer {
     //key, if this isn't the first time through we add `originalCopy` as the `next` property of the previous copy, if the
     //original node has a random node then we also create this node and add it to the dictionary. Once all copies have been
     //made we use the `head` node to return its copy
-    func copyRandomList2(_ head: ListNodeRandom?) -> ListNodeRandom? {
+    func copyRandomList3(_ head: ListNodeRandom?) -> ListNodeRandom? {
         guard let head = head else {
             return nil
         }
@@ -104,7 +148,7 @@ struct CopyListWithRandomPointer {
     //as the key (without attempting to set the `next` or `random` properties). Next we iterate through the original list
     //again, this time setting the `next` and `random` properties. Once all copies have been made we use the `head` node to
     //return its copy.
-    func copyRandomList3(_ head: ListNodeRandom?) -> ListNodeRandom? {
+    func copyRandomList4(_ head: ListNodeRandom?) -> ListNodeRandom? {
         guard let head = head else {
             return nil
         }
