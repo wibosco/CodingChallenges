ad0ae8448dbdee45435e65794a098d4e7733e4b0
William Boles <wibosco@users.noreply.github.com>
2025-08-18 18:19:13 +0100

    Added additional solution for 'Longest Common Prefix' challenge

--- LeetCode/LeetCode/Challenges/LongestCommonPrefix.swift
+++ LeetCode/LeetCode/Challenges/LongestCommonPrefix.swift
@@ -11,8 +11,9 @@ import Foundation
 //https://leetcode.com/problems/longest-common-prefix/
 struct LongestCommonPrefix {
     
-    //Time: O(n^2) where n is the number of characters in `strs`
-    //Space: O(c) where `c` is the number of characters in the smallest element in `strs`
+    //Time: O(n * m) where n is the number of elements in `strs`
+    //               where m is the number of characters in the smallest element in `strs`
+    //Space: O(m)
     //string
     //array
     //vertical scanning
@@ -44,5 +45,85 @@ struct LongestCommonPrefix {
         }
         
         return String(prefix)
+    }
+    
+    //Time: O(n * m) where n is the number of elements in `strs`
+    //               where m is the number of characters in the smallest element in `strs`
+    //Space: O(m)
+    //string
+    //array
+    //
+    //Solution Description:
+    //First we convert each `strs` element into a character array to allow for easier index accessing. As we are coverting to
+    //character arrays we also find which `strs` element is the shortest as that will be the upper bounds for the prefix. Using
+    //that upper bounds we iterate through the elements in `chars` and compare each character at a given index. If any character
+    //is different from the rest we abort. If all characters at that index are the same we add that character to `prefix` and
+    //repeat the process. If we iterate through all indexes up to the upper bounds and only find matches then we can return
+    //`prefix`.
+    func longestCommonPrefix2(_ strs: [String]) -> String {
+        var list = [[Character]]()
+        var maxPrefix = Int.max
+
+        for str in strs {
+            list.append(Array(str))
+
+            maxPrefix = min(maxPrefix, str.count)
+        }
+
+        var p1 = 0
+        var prefix = [Character]()
+
+        while p1 < maxPrefix {
+            let c = list[0][p1]
+            for chars in list[1...] {
+                guard c == chars[p1] else {
+                    return String(prefix)
+                }
+            }
+
+            prefix.append(c)
+            p1 += 1
+        }
+
+        return String(prefix)
+    }
+    
+    //Time: O(n * m) where n is the number of elements in `strs`
+    //               where m is the number of characters in the smallest element in `strs`
+    //Space: O(1)
+    //string
+    //array
+    //
+    //Solution Description:
+    //First we convert each `strs` element into a character array to allow for easier index accessing. As we are coverting to
+    //character arrays we also find which `strs` element is the shortest as that will be the upper bounds for the prefix. Using
+    //that upper bounds we iterate through the elements in `chars` and compare each character at a given index. If any character
+    //is different from the rest we abort and return the prefix we have found so far. If all characters at that index are the
+    //same increment `p1` and repeat the process. If we iterate through all indexes up to the upper bounds and only find matches
+    //then we can return the prefix.
+    func longestCommonPrefix3(_ strs: [String]) -> String {
+        var list = [[Character]]()
+        var maxPrefix = Int.max
+
+        for str in strs {
+            list.append(Array(str))
+
+            maxPrefix = min(maxPrefix, str.count)
+        }
+
+        var p1 = 0
+
+        while p1 < maxPrefix {
+            let c = list[0][p1]
+            for chars in list[1...] {
+                guard c == chars[p1] else {
+                    return p1 == 0 ? "" : String(list[0][0..<p1])
+                }
+            }
+
+            p1 += 1
+        }
+
+        return String(list[0][0..<p1])
     }
 }
