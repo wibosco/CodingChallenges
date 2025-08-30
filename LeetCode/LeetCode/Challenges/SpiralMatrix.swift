f44b61f113685162e456a0e62cceaaf72750c520
William Boles <wibosco@users.noreply.github.com>
2025-08-30 14:09:42 +0100

    Added additional solution for 'Spiral Matrix' challenge

--- LeetCode/LeetCode/Challenges/SpiralMatrix.swift
+++ LeetCode/LeetCode/Challenges/SpiralMatrix.swift
@@ -23,6 +23,7 @@ struct SpiralMatrix {
     //graph
     //visited
     //relative indexing
+    //inout
     //
     //Solution Description:
     //A spiral will go right -> down -> left -> up, until all cells have been visited, this means that if we hit a boundary
@@ -92,6 +93,67 @@ struct SpiralMatrix {
             } else {
                 return nextIndex
             }
+        }
+    }
+    
+    //Time: O(n * m) where n is the number of rows in `matrix`
+    //               where m is the number of columns in `matrix`
+    //Space: O(n * m)
+    //matrix
+    //graph
+    //visited
+    //relative indexing
+    //inout
+    //
+    //Solution Description:
+    //A spiral will go right -> down -> left -> up, until all cells have been visited, this means that if we hit a boundary
+    //we move onto the next direction e.g. from travelling `right` to `down`. Treating the matrix as multiple circuits we can
+    //gradually contract the boundaries of the matrix to force the matrix traversal onto different circuits. Each circuit is
+    //performed using DFS. Within each circuit when we encounter a boundary we switch direction. To ensure that we don't
+    //enter an infinite cycle we use a visited set to know when we reach the start of the ciruite again. When a circuit
+    //completes we move the boundaries of the next inwards. When `order` has a count that matches the matrix count then we
+    //can return `order`.
+    func spiralOrder2(_ matrix: [[Int]]) -> [Int] {
+        var order = [Int]()
+        var visited = Set<[Int]>()
+
+        var leftEdge = 0
+        var rightEdge = matrix[0].count
+        var topEdge = 0
+        var bottomEdge = matrix.count
+
+        while order.count < (matrix.count * matrix[0].count) {
+            dfs(matrix, leftEdge, topEdge, leftEdge, rightEdge, topEdge, bottomEdge, &order, &visited)
+
+            leftEdge += 1
+            rightEdge -= 1
+            topEdge += 1
+            bottomEdge -= 1
+        }
+
+        return order
+    }
+
+    private func dfs(_ matrix: [[Int]], _ r: Int, _ c: Int, _ leftEdge: Int, _ rightEdge: Int, _ topEdge: Int, _ bottomEdge: Int, _ order: inout [Int], _ visited: inout Set<[Int]>) {
+        guard r >= topEdge, r < bottomEdge, c >= leftEdge, c < rightEdge else {
+            return
+        }
+
+        guard !visited.contains([r, c]) else {
+            return
+        }
+        visited.insert([r, c])
+        order.append(matrix[r][c])
+
+        //order of checking if important to ensure that the movements are made in the correct order
+        if r == topEdge && c != (rightEdge - 1) {
+            dfs(matrix, r, c + 1, leftEdge, rightEdge, topEdge, bottomEdge, &order, &visited)
+        } else if c == (rightEdge - 1) && r != (bottomEdge - 1) {
+            dfs(matrix, r + 1, c, leftEdge, rightEdge, topEdge, bottomEdge, &order, &visited)
+        } else if r == (bottomEdge - 1) && c != leftEdge {
+            dfs(matrix, r, c - 1, leftEdge, rightEdge, topEdge, bottomEdge, &order, &visited)
+        } else if c == leftEdge && r != topEdge {
+            dfs(matrix, r - 1, c, leftEdge, rightEdge, topEdge, bottomEdge, &order, &visited)
         }
     }
 }
