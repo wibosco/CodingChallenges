71b68eb4b078b12c9d509c464c080f21f1ebf8dc
William Boles <wibosco@users.noreply.github.com>
2025-08-31 12:42:05 +0100

    Added additional solution for 'Isomorphic Strings' challenge

--- LeetCode/LeetCode/Challenges/IsomorphicStrings.swift
+++ LeetCode/LeetCode/Challenges/IsomorphicStrings.swift
@@ -17,12 +17,18 @@ struct IsomorphicStrings {
     //
     //Solution Description:
     //Using two dictionaries we can ensure that the characters in `s` are consistently mapped to the same characters in `t`
-    //and vice-versa. Using one pointer, `p`, we iterate through both `s` and `t` and use the characters we find at that
-    //index to compare against against any previous times we have encountered those character. If we have previously
-    //encountered those characters we add them to `mappingSToT` and `mappingTToS` with the correct key and value
-    //configuration; if we have previously encountered either of those characters we check to make sure it was in the
-    //presence of both the characters, if either mapping is to a different character we return false. After iterating
-    //through both strings we return true.
+    //and vice-versa. It is neccessarily to have to seperate mappings from `s` to `t` and `t` to `s` as a character in `s`
+    //might map to a different character in `t`, than the same character in `t` maps to in `s` e.g.
+    //
+    // s = "paper", t = "title"
+    //
+    //In the above example, the `e` in `s` maps to `l` in `t` however the `e` in `t` maps to `r` in `s`.
+    //
+    //Using one pointer, `p`, we iterate through both `s` and `t` and use the characters we find at each index to compare
+    //against any previous times we have encountered those character. If we have previously encountered those characters we
+    //add them to `mappingSToT` and `mappingTToS` with the correct key and value configuration; if we have previously
+    //encountered either of those characters we check to make sure it was in the presence of both the characters, if either
+    //mapping is to a different character we return false. After iterating through both strings we return true.
     func isIsomorphic(_ s: String, _ t: String) -> Bool {
         guard s.count == t.count else {
             return false
@@ -56,6 +62,54 @@ struct IsomorphicStrings {
             p += 1
         }
         
+        return true
+    }
+    
+    //Time: O(n) where n is length of either `s` or `t`
+    //Space: O(n)
+    //string
+    //dictionary
+    //
+    //Solution Description:
+    //Using a dictionary we can ensure that the characters in `s` are consistently mapped to the same characters in `t` and
+    //vice-versa. It is neccessarily to have to seperate mappings from `s` to `t` and `t` to `s` as a character in `s` might
+    //map to a different character in `t`, than the same character in `t` maps to in `s` e.g.
+    //
+    // s = "paper", t = "title"
+    //
+    //In the above example, the `e` in `s` maps to `l` in `t` however the `e` in `t` maps to `r` in `s`.
+    //
+    //First we check that mapping from `s` to `t` is valid. We do this by iterating through both `s` and `t` and use the
+    //characters we find at each index to compare against against any previous mapping that may exist. If we haven't
+    //previously encountered that mapping we add it to `mapping`; if we have previously encountered that mapping we check to
+    //make sure it the previous mapping matches the current mapping, if the current mapping is to a different we return false.
+    //We now repeat this process for mapping from `t` to `s`. If both mappings are valid, we return true else we return false.
+    func isIsomorphic2(_ s: String, _ t: String) -> Bool {
+        var mapping = [Character: Character]()
+
+        let s = Array(s)
+        let t = Array(t)
+
+        return canMap(s, t) && canMap(t, s)
+    }
+
+    private func canMap(_ a: [Character], _ b: [Character]) -> Bool {
+        var mapping = [Character: Character]()
+
+        var p1 = 0
+
+        while p1 < a.count {
+            if let mappedTo = mapping[a[p1]] {
+                if mappedTo != b[p1] {
+                    return false
+                }
+            } else {
+                mapping[a[p1]] = b[p1]
+            }
+
+            p1 += 1
+        }
+
         return true
     }
 }
