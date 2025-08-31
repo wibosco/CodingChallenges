e99be2b180297f6882365f7f8d0c7bbf26ee203b
William Boles <wibosco@users.noreply.github.com>
2025-08-31 21:36:47 +0100

    Added additional solution for 'Merge Sorted Array' challenge

--- LeetCode/LeetCode/Challenges/MergeSortedArray.swift
+++ LeetCode/LeetCode/Challenges/MergeSortedArray.swift
@@ -21,15 +21,15 @@ struct MergeSortedArray {
     //
     //Solution Description:
     //Using three pointers we iterate back through `nums1` and `nums2` in reverse. `p1` iterates backwards through the elements
-    //in `nums2` that needed to be sorted, `p2` iterates backwards through `nums2`, and `p3` iterates background all elements of
-    //`nums1`. By iterating backwards we can merge the arrays in non-ascending order which when iterated will be non-descending.
-    //This allows the suffix `0` elelments of `nums1` be overwritten with the correct element wihtout needing extra storage. So
-    //iterating from the final end of `nums3` and moving backwards we check which of `nums1` and `nums2` should be overwrite
-    //the `p3` element of `nums1`. Which ever arrays elements is choosen the respective pointer is decrement. `p2` is
+    //in `nums1` that need to be sorted, `p2` iterates backwards through `nums2`, and `p3` iterates backwards through all
+    //elements of `nums1`. By iterating backwards we can merge the arrays in non-ascending order which when iterated will be
+    //non-descending. This allows the suffix `0` elements of `nums1` be overwritten with the correct element wihtout needing
+    //extra storage. We check which element of `nums1` and `nums2` at their respective indexes is larger and use that larger
+    //value to overwrite the `p3` element of `nums1`. Whichever element is choosen the respective pointer is decrement. `p3` is
     //decremented each iteration. We repeat this process of overwriting for all elements in `nums1`. Special care must be taken
-    //when one array runs out of elements before the other one as from that point on until `p3` == -1, we can only move
-    //elements from one array to the `p3` location of `nums1`. This is  what the overwriting looks like for nums1: [1 3 4 0 0 0]
-    //and nums2: [2 5 6]:
+    //when one array runs out of elements .
+    //
+    //This is  what the overwriting looks like for nums1: [1 3 4 0 0 0] and nums2: [2 5 6]:
     //
     // [1 3 4 0 0 6]
     // [1 3 4 0 5 6]
@@ -37,7 +37,59 @@ struct MergeSortedArray {
     // [1 3 3 4 5 6]
     // [1 2 3 4 5 6]
     //
+    //Once `p3` reaches -1 we exit the loop.
     func merge(_ nums1: inout [Int], _ m: Int, _ nums2: [Int], _ n: Int) {
+        var p1 = m - 1
+        var p2 = n - 1
+        var p3 = nums1.count - 1
+
+        while p3 >= 0 {
+            //by using `Int.min`, when one array runs out of element we ensure the other arrays elements are always choosen
+            let n1 = p1 >= 0 ? nums1[p1] : Int.min
+            let n2 = p2 >= 0 ? nums2[p2] : Int.min
+
+            if n1 > n2 {
+                nums1[p3] = n1
+
+                p1 -= 1
+            } else {
+                nums1[p3] = n2
+
+                p2 -= 1
+            }
+
+            p3 -= 1
+        }
+    }
+    
+    //Time: O(m + n) where m is the number of elements in `nums1`
+    //               where n is the number of elements in `nums2`
+    //Space: O(1)
+    //three pointers
+    //sorting
+    //in-line
+    //array
+    //
+    //Solution Description:
+    //Using three pointers we iterate back through `nums1` and `nums2` in reverse. `p1` iterates backwards through the elements
+    //in `nums1` that need to be sorted, `p2` iterates backwards through `nums2`, and `p3` iterates backwards through all
+    //elements of `nums1`. By iterating backwards we can merge the arrays in non-ascending order which when iterated will be
+    //non-descending. This allows the suffix `0` elements of `nums1` be overwritten with the correct element wihtout needing
+    //extra storage. We check which element of `nums1` and `nums2` at their respective indexes is larger and use that larger
+    //value to overwrite the `p3` element of `nums1`. Whichever element is choosen the respective pointer is decrement. `p3` is
+    //decremented each iteration. We repeat this process of overwriting for all elements in `nums1`. Special care must be taken
+    //when one array runs out of elements .
+    //
+    //This is  what the overwriting looks like for nums1: [1 3 4 0 0 0] and nums2: [2 5 6]:
+    //
+    // [1 3 4 0 0 6]
+    // [1 3 4 0 5 6]
+    // [1 3 4 4 5 6]
+    // [1 3 3 4 5 6]
+    // [1 2 3 4 5 6]
+    //
+    //Once `p3` reaches -1 we exit the loop.
+    func merge2(_ nums1: inout [Int], _ m: Int, _ nums2: [Int], _ n: Int) {
         var p1 = m - 1
         var p2 = n - 1
         var p3 = n + m - 1
@@ -90,7 +142,7 @@ struct MergeSortedArray {
     // [1 2 3 4 5 6]
     //
     //We treat everything after `p` as final and anything before as open to change
-    func merge2(_ nums1: inout [Int], _ m: Int, _ nums2: [Int], _ n: Int) {
+    func merge3(_ nums1: inout [Int], _ m: Int, _ nums2: [Int], _ n: Int) {
         var p1 = m - 1
         var p2 = n - 1
         
@@ -122,7 +174,7 @@ struct MergeSortedArray {
     //`nums2` - if we find that the current `nums2` element is smaller than the current `nums1` element we iterate through
     //`nums2` until we find a `nums2` element that is larger or we exhaust `nums2`. If comparing `nums1` against `nums2` we
     //still have values in `nums2` we add these as-is to the end of the `combined` array
-    func merge3(_ nums1: inout [Int], _ m: Int, _ nums2: [Int], _ n: Int) {
+    func merge4(_ nums1: inout [Int], _ m: Int, _ nums2: [Int], _ n: Int) {
         var combined = [Int]()
         
         var p1 = 0
