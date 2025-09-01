92e0fdf713ba536a2258a26e039b083a15c17604
William Boles <wibosco@users.noreply.github.com>
2025-09-01 13:27:24 +0100

    Added additional solution for 'Nested List Weight Sum II' challenge

--- LeetCode/LeetCode/Challenges/NestedListWeightSumII.swift
+++ LeetCode/LeetCode/Challenges/NestedListWeightSumII.swift
@@ -11,18 +11,18 @@ import Foundation
 //https://leetcode.com/problems/nested-list-weight-sum-ii/
 struct NestedListWeightSumII {
     
-    //Time: O(n)
+    //Time: O(n) where n is the number of elements in `nestedList`
     //Space O(n)
     //graph theory
+    //n-ary tree
     //DFS
     //recursive
     //inout
     //
     //Solution Description:
-    //Treat `nestedList` as a graph. Using DFS traverse through the `nestedList`, at each level checking if we
-    //have an integer or list. If we have an integer we add it and the depth to the `values` array; if we have
-    //a list we go deeper. We also track the max depth as we traverse. When our DFS traversal ends we calculate
-    //the sum and return it
+    //Treat `nestedList` as a tree. Using DFS traverse through the `nestedList`, at each level checking if we have an
+    //integer or list. If we have an integer we add it and the depth to the `values` array; if we have a list we go deeper.
+    //We also track the max depth as we traverse. When our DFS traversal ends we calculate the sum and return it.
     func depthSumInverse(_ nestedList: [NestedInteger]) -> Int {
         var values = [(integer: Int, depth: Int)]()
         var maxDepth = 1
@@ -54,5 +54,49 @@ struct NestedListWeightSumII {
                 dfs(list, (depth + 1), &maxDepth, &values)
             }
         }
+    }
+    
+    //Time: O(n) where n is the number of elements in `nestedList`
+    //Space O(n)
+    //graph theory
+    //n-ary tree
+    //DFS
+    //recursive
+    //
+    //Solution Description:
+    //Treating `nestedList` as a tree. We make two passes using DFS, the first pass calculates the max depth of the tree, the
+    //second calculates the sum of the tree using the max depth. In the first DFS we are only interested in the depth for we
+    //ignore the integer values. In the second DFS we use the integer value to gradually build up the sum. Once all ndoes have
+    //been processed we return `sum`.
+    func depthSumInverse2(_ nestedList: [NestedInteger]) -> Int {
+        let md = maxDepth(nestedList, 1)
+
+        return dfs(nestedList, 1, md)
+    }
+
+    private func dfs(_ list: [NestedInteger], _ depth: Int, _ maxDepth: Int) -> Int {
+        var sum = 0
+        for n in list {
+            if n.isInteger() {
+                let num = n.getInteger()
+                sum += (num * ((maxDepth - depth) + 1))
+            } else {
+                sum += dfs(n.getList(), (depth + 1), maxDepth)
+            }
+        }
+
+        return sum
+    }
+
+    private func maxDepth(_ list: [NestedInteger], _ depth: Int) -> Int {
+        var md = depth
+        for n in list {
+            if !n.isInteger() {
+                let d = maxDepth(n.getList(), (depth + 1))
+                md = max(md, d)
+            }
+        }
+
+        return md
     }
 }
