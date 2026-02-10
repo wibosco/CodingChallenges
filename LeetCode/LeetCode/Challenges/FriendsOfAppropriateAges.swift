a4482dce6e83a32860eaf693c79baaefa0e4f44d
William Boles <wibosco@users.noreply.github.com>
2026-02-10 19:30:23 +0000

    Added additional solution for 'Friends Of Appropriate Ages' challenge

--- LeetCode/LeetCode/Challenges/FriendsOfAppropriateAges.swift
+++ LeetCode/LeetCode/Challenges/FriendsOfAppropriateAges.swift
@@ -12,6 +12,74 @@ struct FriendsOfAppropriateAges {
     //binary search
     //
     //Solution Description:
+    //First we sort `ages`, to allow us to use binary search to determine how many friend requests any given person can send.
+    //With a sorted `ages` array we first find the lower then the upper bounds of the valid friend request group - as `ages`
+    //is sorted everyone in between is valid. We calculate the number of friends by subtracting the upper bound from the
+    //lower bounds and add that sum to `count`. We repeat this process for every element in `ages` and return `count`.
+    func numFriendRequests(_ ages: [Int]) -> Int {
+        let ages = ages.sorted()
+        var count = 0
+        
+        var i = 0
+        while i < ages.count {
+            let age = ages[i]
+            let minValid = age / 2 + 8  //y > 0.5 * age + 7
+            
+            if minValid <= age {  //valid range exists
+                let left = lowerBound(ages, minValid) //first index >= minValid
+                let right = upperBound(ages, age) //first index > age
+                
+                let range = right - left
+                count += range - 1  //everyone in range except self
+            }
+            
+            i += 1
+        }
+        
+        return count
+    }
+    
+    //first index where ages[index] >= target
+    private func lowerBound(_ ages: [Int], _ target: Int) -> Int {
+        var left = 0
+        var right = ages.count
+        
+        while left < right {
+            let mid = left + (right - left) / 2
+            if ages[mid] < target { //note the <
+                left = mid + 1
+            } else {
+                right = mid
+            }
+        }
+
+        return left
+    }
+    
+    //first index where ages[index] > target
+    private func upperBound(_ ages: [Int], _ target: Int) -> Int {
+        var left = 0
+        var right = ages.count
+        
+        while left < right {
+            let mid = left + (right - left) / 2
+            if ages[mid] <= target { //note the <=
+                left = mid + 1
+            } else {
+                right = mid
+            }
+        }
+
+        return left
+    }
+    
+    //Time: O(n log n) where n is the number of elements in `ages`
+    //Space: O(n)
+    //array
+    //sorting
+    //binary search
+    //
+    //Solution Description:
     //By sorting `ages` in descending order we can apply binary search to find the right-most element of `ages` that the
     //current element of `ages` can find a friend request to. With that right-most element we know that all elements between
     //the current `ages` element and that right-most element are also valid we can add the difference between the indexes of
@@ -20,7 +88,7 @@ struct FriendsOfAppropriateAges {
     //reach the end of that duplicated sequence and add those friend requests to `count`, we also need to account for the
     //duplicates after the binary search as each duplicate can send the same friend requests. Once we check all elements
     //in `ages` we can return `count`.
-    func numFriendRequests(_ ages: [Int]) -> Int {
+    func numFriendRequests2(_ ages: [Int]) -> Int {
         var count = 0
         let ages = ages.sorted { $0 > $1 }
 
